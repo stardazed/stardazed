19993a9acfa45e57413adc13a2eb958f0d6d41f4
Arthur Langereis <puurtuur@me.com>
2015-09-18 00:06:30 +0200

    d.ts files are in repo

---
+++ build/libzm3d.d.ts
@@ -0,0 +1,46 @@
+/// <reference path="../defs/gl-matrix.d.ts" />
+/// <reference path="../defs/webgl-ext.d.ts" />
+/// <reference path="libzm.d.ts" />
+/// <reference path="libzmgame.d.ts" />
+declare var gl: WebGLRenderingContext;
+interface ZMBasicGLProgram extends WebGLProgram {
+    vertexPositionAttribute: number;
+    vertexNormalAttribute: number;
+    vertexColorAttribute: number;
+    vertexUVAttribute: number;
+    projMatrixUniform?: WebGLUniformLocation;
+    mvMatrixUniform?: WebGLUniformLocation;
+    normalMatrixUniform?: WebGLUniformLocation;
+    textureUniform?: WebGLUniformLocation;
+    timeUniform?: WebGLUniformLocation;
+}
+declare class TriMesh {
+    vertexBuffer: WebGLBuffer;
+    normalBuffer: WebGLBuffer;
+    colorBuffer: WebGLBuffer;
+    uvBuffer: WebGLBuffer;
+    elementCount: number;
+    constructor(vertexArray: ArrayOfNumber, normalArray?: ArrayOfNumber, colorArray?: ArrayOfNumber, uvArray?: ArrayOfNumber);
+    draw(program: ZMBasicGLProgram, texture?: WebGLTexture): void;
+}
+interface Material {
+    ambientColor?: ArrayOfNumber;
+    diffuseColor?: ArrayOfNumber;
+    specularColor?: ArrayOfNumber;
+}
+declare type MaterialSet = {
+    [matName: string]: Material;
+};
+interface TriangleSoup {
+    elementCount: number;
+    vertexes: ArrayOfNumber;
+    normals?: ArrayOfNumber;
+    uvs?: ArrayOfNumber;
+}
+declare function parseLWMaterialSource(text: string): MaterialSet;
+interface LWObjectData extends TriangleSoup {
+    mtlFileName: string;
+}
+declare function parseLWObjectSource(text: string): LWObjectData;
+declare function loadLWMaterialFile(filePath: string): Promise<MaterialSet>;
+declare function loadLWObjectFile(filePath: string): Promise<TriangleSoup>;
