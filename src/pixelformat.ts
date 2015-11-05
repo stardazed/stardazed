67d67916edacd1101f77bbd28da18e835c30dced
Arthur Langereis <puurtuur@me.com>
2015-11-05 15:13:30 +0100

    pixel format enum and traits

---
+++ src/pixelformat.ts
@@ -0,0 +1,106 @@
+// pixelformat - pixel formats and traits
+// Part of Stardazed TX
+// (c) 2015 by Arthur Langereis - @zenmumbler
+
+namespace sd.render {
+
+	export const enum PixelFormat {
+		None,
+	
+		// 8-bit component
+		Alpha,
+		RGB8,
+		RGBA8,
+
+		// 16-bit component
+		RGB_5_6_5,
+		RGBA_4_4_4_4,
+		RGBA_5_5_5_1,
+
+		// 32-bit component
+		RGB32F,
+		RGBA32F,
+
+		// S3TC (desktop only)
+		DXT1,
+		DXT3,
+		DXT5,
+
+		// Depth / Stencil
+		Depth16I,
+		Depth32I,
+		Depth32F,
+		DepthShadow = Depth32F,
+
+		Stencil8,
+
+		Depth24_Stencil8
+	}
+
+
+	export function pixelFormatIsCompressed(format: PixelFormat) {
+		return format == PixelFormat.DXT1 ||
+			   format == PixelFormat.DXT3 ||
+			   format == PixelFormat.DXT5;
+	}
+
+
+	export function pixelFormatIsDepthFormat(format: PixelFormat) {
+		return format == PixelFormat.Depth16I ||
+			   format == PixelFormat.Depth32I ||
+			   format == PixelFormat.Depth32F;
+	}
+
+
+	export function pixelFormatIsStencilFormat(format: PixelFormat) {
+		return format == PixelFormat.Stencil8;
+	}
+
+
+	export function pixelFormatIsDepthStencilFormat(format: PixelFormat) {
+		return format == PixelFormat.Depth24_Stencil8;
+	}
+
+
+	export function pixelFormatBytesPerElement(format: PixelFormat) {
+		// Element means a pixel for non-compressed formats
+		// and a block for compressed formats
+	
+		switch (format) {
+			case PixelFormat.Alpha:
+			case PixelFormat.Stencil8:
+				return 1;
+
+			case PixelFormat.Depth16I:
+				return 2;
+
+			case PixelFormat.RGB8:
+				return 3;
+
+			case PixelFormat.RGBA8:
+			case PixelFormat.Depth32I:
+			case PixelFormat.Depth32F:
+			case PixelFormat.Depth24_Stencil8:
+				return 4;
+
+			case PixelFormat.RGB32F:
+				return 12;
+
+			case PixelFormat.RGBA32F:
+				return 16;
+
+			// -- compressed formats
+			case PixelFormat.DXT1:
+				return 8;
+
+			case PixelFormat.DXT3:
+			case PixelFormat.DXT5:
+				return 16;
+
+			default:
+				assert(!"unhandled pixel buffer format");
+				return 0;
+		}
+	}
+
+} // ns sd.render
