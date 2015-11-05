84afddd75ba76e02a8a67ba5c5f970a893f1e323
Arthur Langereis <puurtuur@me.com>
2015-11-05 16:39:58 +0100

    texture descriptor structs and make functions

---
+++ src/texture-desc.ts
@@ -0,0 +1,136 @@
+// texture-desc - texture descriptors
+// Part of Stardazed TX
+// (c) 2015 by Arthur Langereis - @zenmumbler
+
+/// <reference path="pixelformat.ts"/>
+
+namespace sd.render {
+
+	export const enum TextureClass {
+		Tex2D,
+		TexCube
+	}
+
+
+	export const enum TextureUsageHint {
+		Normal,
+		RenderTargetOnly
+	}
+
+
+	export const enum UseMipMaps {
+		No = 0,
+		Yes = 1
+	}
+
+
+	// the numerical constant for each face is also the layer index in the cubemap
+	export const enum CubeMapFace {
+		PosX,
+		NegX,
+		PosY,
+		NegY,
+		PosZ,
+		NegZ
+	}
+
+
+	export interface MipMapRange {
+		baseLevel: number;
+		numLevels: number;
+	}
+
+
+	export const enum TextureRepeatMode {
+		Repeat,
+		MirroredRepeat,
+		ClampToEdge,
+		ClampToConstColour
+	}
+
+
+	export const enum TextureSizingFilter {
+		Nearest,
+		Linear
+	}
+
+
+	export const enum TextureMipFilter {
+		None,
+		Nearest,
+		Linear
+	}
+
+
+	export interface SamplerDescriptor {
+		repeatS: TextureRepeatMode;
+		repeatT: TextureRepeatMode;
+
+		minFilter: TextureSizingFilter;
+		magFilter: TextureSizingFilter;
+		mipFilter: TextureMipFilter;
+
+		maxAnisotropy: number;
+	}
+	
+
+	export interface TextureDescriptor {
+		textureClass: TextureClass;
+		pixelFormat: PixelFormat;
+		usageHint: TextureUsageHint;
+		sampling: SamplerDescriptor;
+		dim: PixelDimensions;
+		mipmaps: number;
+	}
+
+
+	export function makeMipMapRange(baseLevel: number, numLevels: number): MipMapRange {
+		return { baseLevel: baseLevel, numLevels: numLevels };
+	}
+
+
+	export function makeSamplerDescriptor(): SamplerDescriptor {
+		return {
+			repeatS: TextureRepeatMode.Repeat,
+			repeatT: TextureRepeatMode.Repeat,
+
+			minFilter: TextureSizingFilter.Linear,
+			magFilter: TextureSizingFilter.Linear,
+			mipFilter: TextureMipFilter.Nearest,
+
+			maxAnisotropy: 1
+		}
+	}
+
+
+	export function maxMipLevelsForDimension(dim: number) {
+		return 1 + Math.floor(Math.log(dim | 0) / Math.LOG2E);
+	}
+
+
+	export function makeTexDesc2D(pixelFormat: PixelFormat, width: number, height: number, mipmapped: UseMipMaps): TextureDescriptor {
+		var maxDim = Math.max(width, height);
+
+		return {
+			textureClass: TextureClass.Tex2D,
+			pixelFormat: pixelFormat,
+			usageHint: TextureUsageHint.Normal,
+			sampling: makeSamplerDescriptor(),
+			dim: makePixelDimensions(width, height),
+			mipmaps: (mipmapped == UseMipMaps.Yes) ? maxMipLevelsForDimension(maxDim) : 1
+		}
+	}
+
+
+	export function makeTexDescCube(pixelFormat: PixelFormat, dimension: number, mipmapped: UseMipMaps): TextureDescriptor {
+		return {
+			textureClass: TextureClass.TexCube,
+			pixelFormat: pixelFormat,
+			usageHint: TextureUsageHint.Normal,
+			sampling: makeSamplerDescriptor(),
+			dim: makePixelDimensions(dimension, dimension),
+			mipmaps: (mipmapped == UseMipMaps.Yes) ? maxMipLevelsForDimension(dimension) : 1
+		};
+	}
+
+} // ns sd.render
