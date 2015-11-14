57b6e1bd3b8ae0835ac884e003d4c8c2a7cb8aed
Arthur Langereis <puurtuur@me.com>
2015-11-14 12:17:14 +0100

    expose 2 container util functions

--- src/container.ts
+++ src/container.ts
@@ -35,9 +35,16 @@ if (! ArrayBuffer.transfer) {
 
 namespace sd.container {
 
-	function copyElementRange(src: TypedArray, srcOffset: number, srcCount: number, dest: TypedArray, destOffset: number) {
+	export function copyElementRange<T>(src: ArrayLike<T>, srcOffset: number, srcCount: number, dest: ArrayLike<T>, destOffset: number) {
 		for (var ix = 0; ix < srcCount; ++ix) {
 			dest[destOffset++] = src[srcOffset++];
+		}
+	}
+
+
+	export function fill<T>(dest: ArrayLike<T>, value: T, count: number, offset: number = 0) {
+		for (var ix = 0; ix < count; ++ix) {
+			dest[ix + offset] = value;
 		}
 	}
 
