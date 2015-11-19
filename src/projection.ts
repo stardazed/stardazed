3748ff947b1f884b8cdb9b13145cb18d59b65f92
Arthur Langereis <puurtuur@me.com>
2015-11-19 18:30:28 +0100

    extract out projection interface, may be folded into something else later again

---
+++ src/projection.ts
@@ -0,0 +1,14 @@
+// projection - simple projection utils
+// Part of Stardazed TX
+// (c) 2015 by Arthur Langereis - @zenmumbler
+
+/// <reference path="math.ts" />
+
+namespace sd.world {
+
+	export interface ProjectionSetup {
+		projectionMatrix: ArrayOfNumber;
+		viewMatrix: ArrayOfNumber;
+	}
+
+} // sd.world
