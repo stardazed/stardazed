b4c64ac99eaab42d3275443cc4bd492805092dd8
Arthur Langereis <puurtuur@me.com>
2015-11-24 16:23:41 +0100

    Transform: true support for hierarchical transforms

--- src/transform.ts
+++ src/transform.ts
@@ -14,45 +14,67 @@ namespace sd.world {
 
 	export interface TransformDescriptor {
 		position: ArrayOfNumber;
-		rotation: ArrayOfNumber;
-		scale: ArrayOfNumber;
+		rotation?: ArrayOfNumber;
+		scale?: ArrayOfNumber;
 	}
 
 
 	export class TransformManager {
 		private instanceData_: container.MultiArrayBuffer;
 
+		private entityBase_: TypedArray;
+
 		private parentBase_: TypedArray;
+		private firstChildBase_: TypedArray;
+		private prevSiblingBase_: TypedArray;
+		private nextSiblingBase_: TypedArray;
+
 		private positionBase_: TypedArray;
 		private rotationBase_: TypedArray;
 		private scaleBase_: TypedArray;
-		private modelMatrixBase_: TypedArray;
-
-		static root: TransformInstance = 0;
+		private localMatrixBase_: TypedArray;
+		private worldMatrixBase_: TypedArray;
 
 		constructor() {
 			var instanceFields: container.MABField[] = [
+				{ type: SInt32, count: 1 }, // entity
+
 				{ type: SInt32, count: 1 }, // parentInstance
+				{ type: SInt32, count: 1 }, // firstChild
+				{ type: SInt32, count: 1 }, // prevSibling
+				{ type: SInt32, count: 1 }, // nextSibling
+
 				{ type: Float, count: 3 },  // position
 				{ type: Float, count: 4 },  // rotation
 				{ type: Float, count: 3 },  // scale
-				{ type: Float, count: 16 }  // modelMatrix
+
+				{ type: Float, count: 16 }, // localMatrix
+				{ type: Float, count: 16 }  // worldMatrix
 			];
-			this.instanceData_ = new container.MultiArrayBuffer(512, instanceFields);
+			this.instanceData_ = new container.MultiArrayBuffer(2048, instanceFields); // 376 KiB
 
 			this.rebase();
 		}
 
 		rebase() {
-			this.parentBase_ = this.instanceData_.indexedFieldView(0);
-			this.positionBase_ = this.instanceData_.indexedFieldView(1);
-			this.rotationBase_ = this.instanceData_.indexedFieldView(2);
-			this.scaleBase_ = this.instanceData_.indexedFieldView(3);
-			this.modelMatrixBase_ = this.instanceData_.indexedFieldView(4);
+			this.entityBase_ = this.instanceData_.indexedFieldView(0);
+
+			this.parentBase_ = this.instanceData_.indexedFieldView(1);
+			this.firstChildBase_ = this.instanceData_.indexedFieldView(2);
+			this.prevSiblingBase_ = this.instanceData_.indexedFieldView(3);
+			this.nextSiblingBase_ = this.instanceData_.indexedFieldView(4);
+
+			this.positionBase_ = this.instanceData_.indexedFieldView(5);
+			this.rotationBase_ = this.instanceData_.indexedFieldView(6);
+			this.scaleBase_ = this.instanceData_.indexedFieldView(7);
+
+			this.localMatrixBase_ = this.instanceData_.indexedFieldView(8);
+			this.worldMatrixBase_ = this.instanceData_.indexedFieldView(9);
 		}
 
 		// -- array access
 		get count() { return this.instanceData_.count; }
+
 
 		create(linkedEntity: Entity, parent?: TransformInstance): TransformInstance;
 		create(linkedEntity: Entity, desc: TransformDescriptor, parent?: TransformInstance): TransformInstance;
@@ -65,84 +87,175 @@ namespace sd.world {
 				}
 			}
 
-			var inst = entIndex;
+			var thisInstance = entIndex;
+			var parentInstance = 0;
+			var descriptor: TransformDescriptor = null;
 
-			if (descOrParent && ("position" in descOrParent)) {
-				let desc = <TransformDescriptor>descOrParent;
+			if (descOrParent) {
+				if (typeof descOrParent == "number") {
+					parentInstance = <number>descOrParent;
+				}
+				else {
+					descriptor = <TransformDescriptor>descOrParent;
+					parentInstance = <number>parent; // can be null
+				}
+			}
 
-				if (parent)
-					this.parentBase_[inst] = <number>parent;
-				this.positionBase_.set(desc.position, inst * math.Vec3.elementCount);
-				this.rotationBase_.set(desc.rotation, inst * math.Quat.elementCount);
-				this.scaleBase_.set(desc.scale, inst * math.Vec3.elementCount);
+			if (parentInstance) {
+				this.parentBase_[thisInstance] = parentInstance;
+				var myPrevSibling = this.firstChildBase_[parentInstance];
 
-				var modelMat = math.vectorArrayItem(this.modelMatrixBase_, math.Mat4, inst);
-				mat4.fromRotationTranslationScale(modelMat, desc.rotation, desc.position, desc.scale);
+				if (myPrevSibling) {
+					assert(this.prevSiblingBase_[myPrevSibling] == 0, "firstChild cannot have prev siblings");
+
+					// find end of child chain
+					while (this.nextSiblingBase_[myPrevSibling] != 0) {
+						myPrevSibling = this.nextSiblingBase_[myPrevSibling];
+					}
+
+					// append self to parent's child list
+					this.nextSiblingBase_[myPrevSibling] = thisInstance;
+					this.prevSiblingBase_[thisInstance] = myPrevSibling;
+				}
+				else {
+					this.firstChildBase_[parentInstance] = thisInstance;
+					this.prevSiblingBase_[thisInstance] = 0;
+					this.nextSiblingBase_[thisInstance] = 0;
+				}
 			}
 			else {
-				let par = <TransformInstance>descOrParent;
-				if (par)
-					this.parentBase_[inst] = <number>par;
-
-				this.rotationBase_.set(math.Quat.identity, inst * math.Quat.elementCount);
-				this.scaleBase_.set(math.Vec3.one, inst * math.Vec3.elementCount);
-				this.modelMatrixBase_.set(math.Mat4.identity, inst * math.Mat4.elementCount);
+				this.parentBase_[thisInstance] = 0;
+				this.prevSiblingBase_[thisInstance] = 0;
+				this.nextSiblingBase_[thisInstance] = 0;
 			}
 
-			return inst;
-		}
 
-		// -- single instance data access
-		parent(inst: TransformInstance): TransformInstance { return this.parentBase_[<number>inst]; }
-		position(inst: TransformInstance) { return math.vectorArrayItem(this.positionBase_, math.Vec3, <number>inst); }
-		rotation(inst: TransformInstance) { return math.vectorArrayItem(this.rotationBase_, math.Quat, <number>inst); }
-		scale(inst: TransformInstance) { return math.vectorArrayItem(this.scaleBase_, math.Vec3, <number>inst); }
-		modelMatrix(inst: TransformInstance) { return math.vectorArrayItem(this.modelMatrixBase_, math.Mat4, <number>inst); }
+			if (descriptor) {
+				// optional descriptor fields
+				let rotation = descriptor.rotation || math.Quat.identity;
+				let scale = descriptor.scale || math.Vec3.one;
 
-		setParent(inst: TransformInstance, newParent: TransformInstance) {
-			this.parentBase_[<number>inst] = <number>newParent;
-		}
+				this.positionBase_.set(descriptor.position, thisInstance * math.Vec3.elementCount);
+				this.rotationBase_.set(rotation, thisInstance * math.Quat.elementCount);
+				this.scaleBase_.set(scale, thisInstance * math.Vec3.elementCount);
 
-		setPosition(inst: TransformInstance, newPosition: ArrayOfNumber) {
-			var index = <number>inst;
-			this.positionBase_.set(newPosition, index * math.Vec3.elementCount);
+				this.setLocalMatrix(thisInstance, rotation, descriptor.position, scale);
+			}
+			else {
+				this.positionBase_.set(math.Vec3.zero, thisInstance * math.Quat.elementCount);
+				this.rotationBase_.set(math.Quat.identity, thisInstance * math.Quat.elementCount);
+				this.scaleBase_.set(math.Vec3.one, thisInstance * math.Vec3.elementCount);
 
-			var modelMat = math.vectorArrayItem(this.modelMatrixBase_, math.Mat4, index);
-			mat4.fromRotationTranslationScale(modelMat, this.rotation(inst), newPosition, this.scale(inst));
-		}
+				this.localMatrixBase_.set(math.Mat4.identity, thisInstance * math.Mat4.elementCount);
+				this.worldMatrixBase_.set(math.Mat4.identity, thisInstance * math.Mat4.elementCount);
+			}
 
-		setRotation(inst: TransformInstance, newRotation: ArrayOfNumber) {
-			var index = <number>inst;
-			this.rotationBase_.set(newRotation, index * math.Quat.elementCount);
-
-			var modelMat = math.vectorArrayItem(this.modelMatrixBase_, math.Mat4, index);
-			mat4.fromRotationTranslationScale(modelMat, newRotation, this.position(inst), this.scale(inst));
-		}
-
-		setPositionAndRotation(inst: TransformInstance, newPosition: ArrayOfNumber, newRotation: ArrayOfNumber) {
-			var index = <number>inst;
-			this.positionBase_.set(newPosition, index * math.Vec3.elementCount);
-			this.rotationBase_.set(newRotation, index * math.Quat.elementCount);
-
-			var modelMat = math.vectorArrayItem(this.modelMatrixBase_, math.Mat4, index);
-			mat4.fromRotationTranslationScale(modelMat, newRotation, newPosition, this.scale(inst));
-		}
-
-		setScale(inst: TransformInstance, newScale: ArrayOfNumber) {
-			var index = <number>inst;
-			this.scaleBase_.set(newScale, index * math.Vec3.elementCount);
-
-			var modelMat = math.vectorArrayItem(this.modelMatrixBase_, math.Mat4, index);
-			mat4.fromRotationTranslationScale(modelMat, this.rotation(inst), this.position(inst), newScale);
+			return thisInstance;
 		}
 
 
-		// -- Entity -> TransformInstance mapping
+		// Entity -> TransformInstance mapping
 		forEntity(ent: Entity): TransformInstance {
 			var index = entityIndex(ent);
 			if (index > 0 && index <= this.instanceData_.count)
 				return <number>ent;
 			assert(false, "No transform for entity " + index);
+		}
+
+
+		// -- single instance getters
+		entity(inst: TransformInstance): Entity { return this.entityBase_[<number>inst]; }
+
+		parent(inst: TransformInstance): TransformInstance { return this.parentBase_[<number>inst]; }
+		firstChild(inst: TransformInstance): TransformInstance { return this.firstChildBase_[<number>inst]; }
+		prevSibling(inst: TransformInstance): TransformInstance { return this.prevSiblingBase_[<number>inst]; }
+		nextSibling(inst: TransformInstance): TransformInstance { return this.nextSiblingBase_[<number>inst]; }
+
+		localPosition(inst: TransformInstance) { return math.vectorArrayItem(this.positionBase_, math.Vec3, <number>inst); }
+		localRotation(inst: TransformInstance) { return math.vectorArrayItem(this.rotationBase_, math.Quat, <number>inst); }
+		localScale(inst: TransformInstance) { return math.vectorArrayItem(this.scaleBase_, math.Vec3, <number>inst); }
+
+		worldPosition(inst: TransformInstance): ArrayOfNumber {
+			var worldMat = math.vectorArrayItem(this.worldMatrixBase_, math.Mat4, <number>inst);
+			return [worldMat[12], worldMat[13], worldMat[14]];
+		}
+
+		localMatrix(inst: TransformInstance) { return math.vectorArrayItem(this.localMatrixBase_, math.Mat4, <number>inst); }
+		worldMatrix(inst: TransformInstance) { return math.vectorArrayItem(this.worldMatrixBase_, math.Mat4, <number>inst); }
+
+
+		// update the world matrices of inst and all of its children
+		private applyParentTransform(parentMatrix: ArrayOfNumber, inst: TransformInstance) {
+			var localMat = this.localMatrix(inst);
+			var worldMat = this.worldMatrix(inst);
+			mat4.multiply(worldMat, parentMatrix, localMat);
+
+			var child = this.firstChildBase_[<number>inst];
+			while (child != 0) {
+				this.applyParentTransform(worldMat, child);
+				child = this.nextSiblingBase_[child];
+			}
+		}
+
+
+		// two overloads: one with new matrix, one with tranform components
+		setLocalMatrix(inst: TransformInstance, newLocalMatrix: ArrayOfNumber): void;
+		setLocalMatrix(inst: TransformInstance, newRotation: ArrayOfNumber, newPosition: ArrayOfNumber, newScale: ArrayOfNumber): void;
+		setLocalMatrix(inst: TransformInstance, localMatOrRot: ArrayOfNumber, newPosition?: ArrayOfNumber, newScale?: ArrayOfNumber) {
+			var localMat = math.vectorArrayItem(this.localMatrixBase_, math.Mat4, <number>inst);
+			if (arguments.length == 4) {
+				mat4.fromRotationTranslationScale(localMat, localMatOrRot, newPosition, newScale);
+			}
+			else {
+				localMat.set(localMatOrRot); // 4x4 mat
+			}
+
+			var parent = this.parentBase_[<number>inst];
+			var firstChild = this.firstChildBase_[<number>inst];
+
+			// -- optimization for root-level, childless entities (of which I have seen there are many, but this may/will change)
+			if (parent || firstChild) {
+				var parentWorldMat = (parent == 0) ? math.Mat4.identity : this.worldMatrix(parent);
+				this.applyParentTransform(parentWorldMat, inst);
+			}
+			else {
+				mat4.copy(this.worldMatrix(inst), localMat);
+			}
+		}
+
+
+		setParent(inst: TransformInstance, newParent: TransformInstance) {
+			this.parentBase_[<number>inst] = <number>newParent;
+		}
+
+
+		setPosition(inst: TransformInstance, newPosition: ArrayOfNumber) {
+			this.positionBase_.set(newPosition, <number>inst * math.Vec3.elementCount);
+			this.setLocalMatrix(inst, this.localRotation(inst), newPosition, this.localScale(inst));
+		}
+
+		setRotation(inst: TransformInstance, newRotation: ArrayOfNumber) {
+			this.rotationBase_.set(newRotation, <number>inst * math.Quat.elementCount);
+			this.setLocalMatrix(inst, newRotation, this.localPosition(inst), this.localScale(inst));
+		}
+
+		setPositionAndRotation(inst: TransformInstance, newPosition: ArrayOfNumber, newRotation: ArrayOfNumber) {
+			this.positionBase_.set(newPosition, <number>inst * math.Vec3.elementCount);
+			this.rotationBase_.set(newRotation, <number>inst * math.Quat.elementCount);
+			this.setLocalMatrix(inst, newRotation, newPosition, this.localScale(inst));
+		}
+
+		setScale(inst: TransformInstance, newScale: ArrayOfNumber) {
+			this.scaleBase_.set(newScale, <number>inst * math.Vec3.elementCount);
+			this.setLocalMatrix(inst, this.localRotation(inst), this.localPosition(inst), newScale);
+		}
+
+
+		// -- relative transform helpers
+
+		translate(inst: TransformInstance, localDelta: ArrayOfNumber) {
+			var pos = this.localPosition(inst);
+			this.setPosition(inst, [pos[0] + localDelta[0], pos[1] + localDelta[1], pos[2] + localDelta[2]]);
 		}
 	}
 
