2b8eca8c7df815fbae525eb79889bfb1a36cf2bb
Arthur Langereis <puurtuur@me.com>
2015-12-07 15:14:36 +0100

    some quick texture loading util functions (from GZ)

---
+++ src/texture-util.ts
@@ -0,0 +1,70 @@
+// texture-util - texture creation utilities
+// Part of Stardazed TX
+// (c) 2015 by Arthur Langereis - @zenmumbler
+
+namespace sd.render {
+
+	export function loadSimpleTexture(rc: RenderContext, filePath: string, mipmaps = false): Promise<render.Texture> {
+		return new Promise<render.Texture>(function(resolve, reject) {
+			var image = new Image();
+
+			image.onload = function() {
+				var td = render.makeTexDesc2DFromImageSource(image, render.useMipMaps(mipmaps));
+				var texture = new render.Texture(rc, td);
+				resolve(texture);
+			};
+			image.onerror = function() {
+				assert(false, "Image " + filePath + "does not exist");
+				reject();
+			};
+
+			image.src = filePath;
+		});
+	}
+
+
+	export function loadCubeTexture(rc: RenderContext, filePaths: string[]): Promise<render.Texture> {
+		assert(filePaths.length == 6, "must have 6 paths for cube tex");
+
+		return new Promise<render.Texture>(function(resolve, reject) {
+			var images: HTMLImageElement[] = [];
+			var loaded = 0;
+
+			for (var k = 0; k < 6; ++k) {
+				(function(face: number) {
+					var image = new Image;
+
+					image.onload = function() {
+						images[face] = image;
+						++loaded;
+
+						if (loaded == 6) {
+							var td = render.makeTexDescCubeFromImageSources(images);
+							var texture = new render.Texture(rc, td);
+							resolve(texture);
+						}
+					};
+					image.onerror = function() {
+						assert(false, "Image " + filePaths[face] + "does not exist");
+						reject();
+					};
+
+					image.src = filePaths[face];
+				} (k));
+			}
+		});
+	}
+
+
+	export function makeCubeMapPaths(basePath: string, extension: string): string[] {
+		return [
+			basePath + "posx" + extension,
+			basePath + "negx" + extension,
+			basePath + "posy" + extension,
+			basePath + "negy" + extension,
+			basePath + "posz" + extension,
+			basePath + "negz" + extension
+		];
+	}
+
+} // ns sd.render
