178504135ee04c316e038913cce2659271e36c92
Arthur Langereis <puurtuur@me.com>
2015-12-10 18:52:44 +0100

    add quat.fromEuler extension fn

--- src/math.ts
+++ src/math.ts
@@ -10,6 +10,30 @@ interface Math {
 }
 
 
+interface quat {
+	fromEuler(yaw: number, pitch: number, roll: number): Float32Array;
+}
+
+quat.fromEuler = function(yaw: number, pitch: number, roll: number) {
+	var y = yaw * 0.5;
+	var p = pitch * 0.5;
+	var r = roll * 0.5;
+
+	var siny = Math.sin(y), cosy = Math.cos(y);
+	var sinp = Math.sin(p), cosp = Math.cos(p);
+	var sinr = Math.sin(r), cosr = Math.cos(r);
+
+	// evaluated form of 3 Quat multiplications (of yaw, pitch and roll)
+	return <Float32Array>quat.normalize(new Float32Array(4), [
+		sinr * cosp * cosy - cosr * sinp * siny,
+		cosr * sinp * cosy + sinr * cosp * siny,
+		cosr * cosp * siny - sinr * sinp * cosy,
+		cosr * cosp * cosy + sinr * sinp * siny
+	]);
+}
+
+
+
 namespace sd.math {
 
 	export function intRandom(maximum: number): number {
