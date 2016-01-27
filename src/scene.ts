ad31fcd305ec3bb641b03919ccef3be734bb7e49
Arthur Langereis <puurtuur@me.com>
2016-01-27 14:33:02 +0100

    Scene.makeEntity method to create entity and optional components

--- src/scene.ts
+++ src/scene.ts
@@ -1,16 +1,36 @@
-// scene - manages a set of related components
+// scene - manages a set of related components and provides convenience functions
 // Part of Stardazed TX
 // (c) 2015-6 by Arthur Langereis - @zenmumbler
 
 namespace sd.world {
 
+	export interface EntityDescriptor {
+		transform?: TransformDescriptor;
+		model?: StdModelDescriptor;
+		rigidBody?: RigidBodyDescriptor;
+		collider?: ColliderDescriptor;
+		light?: LightDescriptor;
+	}
+
+
+	export interface EntityInfo {
+		entity: Entity;
+		transform: TransformInstance;
+		model?: StdModelInstance;
+		rigidBody?: RigidBodyInstance;
+		collider?: ColliderInstance;
+		light?: LightInstance;
+	}
+
+
 	export class Scene {
+		stdMaterialMgr: StdMaterialManager;
+		physMatMgr: PhysicsMaterialManager;
+
 		entityMgr: EntityManager;
 		transformMgr: TransformManager;
 		lightMgr: LightManager;
-		stdMaterialMgr: StdMaterialManager;
 		stdModelMgr: StdModelManager;
-		physMatMgr: PhysicsMaterialManager;
 		rigidBodyMgr: RigidBodyManager;
 		colliderMgr: ColliderManager;
 
@@ -26,6 +46,20 @@ namespace sd.world {
 
 			this.colliderMgr = new ColliderManager(this.physMatMgr);
 			this.rigidBodyMgr = new RigidBodyManager(this.transformMgr, this.colliderMgr);
+		}
+
+
+		makeEntity(desc?: EntityDescriptor): EntityInfo {
+			var ent = this.entityMgr.create();
+
+			return {
+				entity: ent,
+				transform: this.transformMgr.create(ent, desc && desc.transform),
+				model: desc && desc.model ? this.stdModelMgr.create(ent, desc.model) : null,
+				rigidBody: desc && desc.rigidBody ? this.rigidBodyMgr.create(ent, desc.rigidBody) : null,
+				collider: desc && desc.collider ? this.colliderMgr.create(ent, desc.collider) : null,
+				light: desc && desc.light ? this.lightMgr.create(ent, desc.light) : null
+			};
 		}
 	}
 
