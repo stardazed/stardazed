36c125c968f315d4553739d01b2c2c1c371d32b7
Arthur Langereis <puurtuur@me.com>
2016-02-03 16:04:16 +0100

    rename SoundManager to audio.AudioContext and change to struct + functions setup

---
+++ src/audiocontext.ts
@@ -0,0 +1,52 @@
+// audiocontext - web audio interfaces
+// Part of Stardazed TX
+// (c) 2016 by Arthur Langereis - @zenmumbler
+
+declare var webkitAudioContext: {
+    prototype: AudioContext;
+    new (): AudioContext;
+}
+
+interface Window {
+	webkitAudioContext?: typeof AudioContext;
+	AudioContext?: typeof AudioContext;
+}
+
+type NativeAudioContext = AudioContext;
+
+
+namespace sd.audio {
+
+	export interface AudioContext {
+		ctx: NativeAudioContext;
+	}
+
+
+	export function makeAudioBufferFromData(ac: AudioContext, data: ArrayBuffer): Promise<AudioBuffer> {
+		return new Promise<AudioBuffer>((resolve, reject) => {
+			ac.ctx.decodeAudioData(data,
+				(audioData: AudioBuffer) => {
+					resolve(audioData);
+				},
+				() => {
+					reject("invalid audio data");
+				}
+			);
+		});
+	}
+
+
+	export function makeAudioContext(): audio.AudioContext {
+		var ac = window.AudioContext ? new (window.AudioContext)() : (window.webkitAudioContext ? new webkitAudioContext() : null);
+
+		if (ac) {
+			return {
+				ctx: ac
+			};
+		}
+		else {
+			return null;
+		}
+	}
+
+} // ns sd.audio
