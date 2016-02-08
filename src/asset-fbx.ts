cf4b026b21dcb9a0e1adac7ae2ec0a1fd491d98c
Arthur Langereis <puurtuur@me.com>
2016-02-08 17:27:26 +0100

    Redo fbx interpreter again by creating a supported object graph.

--- src/asset-fbx.ts
+++ src/asset-fbx.ts
@@ -16,8 +16,8 @@ namespace sd.asset {
 				Skip
 			}
 
-			export const enum FBXValueType {
-				Invalid,
+			export const enum FBXPropertyType {
+				Unknown,
 
 				Int,
 				Double,
@@ -25,59 +25,69 @@ namespace sd.asset {
 				Time,
 				String,
 				Vector3D,
-				Object,
-
-				Compound
+				Vector4D,
+				Object
 			}
 
-			const fbxTypeNameMapping: { [type: string]: FBXValueType } = {
-				"enum": FBXValueType.Int,
-				"int": FBXValueType.Int,
-				"integer": FBXValueType.Int,
+			const fbxTypeNameMapping: { [type: string]: FBXPropertyType } = {
+				"enum": FBXPropertyType.Int,
+				"int": FBXPropertyType.Int,
+				"integer": FBXPropertyType.Int,
 
-				"double": FBXValueType.Double,
-				"number": FBXValueType.Double,
-				"ulonglong": FBXValueType.Double,
+				"float": FBXPropertyType.Double,
+				"double": FBXPropertyType.Double,
+				"number": FBXPropertyType.Double,
+				"ulonglong": FBXPropertyType.Double,
+				"fieldofview": FBXPropertyType.Double,
+				"fieldofviewx": FBXPropertyType.Double,
+				"fieldofviewy": FBXPropertyType.Double,
+				"roll": FBXPropertyType.Double,
+				"opticalcenterx": FBXPropertyType.Double,
+				"opticalcentery": FBXPropertyType.Double,
 
-				"bool": FBXValueType.Bool,
-				"visibility": FBXValueType.Bool,
-				"visibility inheritance": FBXValueType.Bool,
+				"bool": FBXPropertyType.Bool,
+				"visibility": FBXPropertyType.Bool,
+				"visibility inheritance": FBXPropertyType.Bool,
 
-				"ktime": FBXValueType.Time,
+				"ktime": FBXPropertyType.Time,
 
-				"kstring": FBXValueType.String,
-				"datetime": FBXValueType.String,
+				"kstring": FBXPropertyType.String,
+				"datetime": FBXPropertyType.String,
 
-				"vector3d": FBXValueType.Vector3D,
-				"vector": FBXValueType.Vector3D,
-				"color": FBXValueType.Vector3D,
-				"colorrgb": FBXValueType.Vector3D,
-				"lcl translation": FBXValueType.Vector3D,
-				"lcl rotation": FBXValueType.Vector3D,
-				"lcl scaling": FBXValueType.Vector3D,
+				"vector3d": FBXPropertyType.Vector3D,
+				"vector": FBXPropertyType.Vector3D,
+				"color": FBXPropertyType.Vector3D,
+				"colorrgb": FBXPropertyType.Vector3D,
+				"lcl translation": FBXPropertyType.Vector3D,
+				"lcl rotation": FBXPropertyType.Vector3D,
+				"lcl scaling": FBXPropertyType.Vector3D,
 
-				"object": FBXValueType.Object,
-				"compound": FBXValueType.Compound
+				"colorandalpha": FBXPropertyType.Vector4D,
+
+				"object": FBXPropertyType.Object
 			};
 
 			export interface FBXProp70Prop {
 				name: string;
 				typeName: string;
-				type: FBXValueType;
+				type: FBXPropertyType;
 				values: FBXValue[];
 			}
 
 			export function interpretProp70P(pValues: FBXValue[]) {
 				assert(pValues.length >= 4, "A P must have 4 or more values.");
+				var typeName = <string>pValues[1];
 
 				var result: FBXProp70Prop = {
 					name: <string>pValues[0],
-					typeName: <string>pValues[1],
-					type: fbxTypeNameMapping[(<string>pValues[1]).toLowerCase()] || FBXValueType.Invalid,
+					typeName: typeName,
+					type: fbxTypeNameMapping[typeName.toLowerCase()] || FBXPropertyType.Unknown,
 					values: pValues.slice(4)
 				};
 
-				assert(result.type != FBXValueType.Invalid, "Unknown Prop70 type: " + result.typeName);
+				if (result.type == FBXPropertyType.Unknown) {
+					console.warn("Unknown typed prop typename: " + typeName);
+				}
 				return result;
 			}
 
@@ -86,9 +96,10 @@ namespace sd.asset {
 				endBlock(): void;
 
 				property(name: string, values: FBXValue[]): void;
-				typedProperty(name: string, type: FBXValueType, typeName: string, values: FBXValue[]): void;
+				typedProperty(name: string, type: FBXPropertyType, typeName: string, values: FBXValue[]): void;
 
 				error(msg: string, offset: number, token?: string): void;
+				completed(): void;
 			}
 
 			export interface FBXParser {
@@ -99,309 +110,178 @@ namespace sd.asset {
 		} // ns parse
 
 
-		// -- FBX Document data
-
-		export const enum FBXObjectType {
-			None,
-
-			Geometry,
-			Model,
-			Material,
-			Video,
-			Texture,
-			NodeAttribute
-		}
-
-		function fbxObjectTypeForName(typeName: string) {
-			switch (typeName) {
-				case "Geometry": return FBXObjectType.Geometry;
-				case "Model": return FBXObjectType.Model;
-				case "Material": return FBXObjectType.Material;
-				case "Video": return FBXObjectType.Video;
-				case "Texture": return FBXObjectType.Texture;
-				case "NodeAttribute": return FBXObjectType.NodeAttribute;
-			}
-
-			return FBXObjectType.None;
-		}
-
-		export interface FBXObject {
-			type: FBXObjectType;
-			id: number;
-			name: string;
-			class: string;
-			subClass: string;
-		}
-
-		export const enum FBXGeometryStreamType {
-			Normal = 1,
-			UV,
-			MaterialIndex,
-			TextureIndex
-		}
-
-		export const enum FBXGeometryStreamMappingType {
-			PerVertex = 1,
-			PerPolygonvertex,
-			PerPolygon,
-			AllSame
-		}
-
-		export const enum FBXGeometryStreamReferenceType {
-			Direct = 1,
-			IndexToDirect = 2
-		}
-
-		export interface FBXGeometryStream {
-			name: string;
-			index: number;
-			type: FBXGeometryStreamMappingType;
-			mappingType: FBXGeometryStreamMappingType;
-			referenceType: FBXGeometryStreamReferenceType;
-			dataArray?: TypedArray;
-			indexArray?: TypedArray;
-		}
-
-		export interface FBXGeometry extends FBXObject {
-			vertexCount: number;
-			triangleCount: number;
-
-			positions: TypedArray;
-			triangleIndexes: TypedArray;
-
-			streams: FBXGeometryStream[];
-		}
-
-		export interface FBXModel extends FBXObject {
-			localPosition: Float3;
-			localRotation: Float4;
-			localScaling: Float3;
-		}
-
-		export interface FBXMeshModel extends FBXModel {
-			geometry: FBXGeometry;
-			materials: Material[];
-		}
-
-		export interface FBXMaterial extends FBXObject {
-			material: Material;
-		}
-
-		export interface FBXLightAttr extends FBXObject {
-			colour: Float3;
-			intensity: number;
-		}
-
-		export interface FBXLightModel extends FBXModel {
-			light: FBXLightAttr;
-		}
-
-		export interface FBXCameraAttr extends FBXObject {
-			clearColour: Float3;
-			fov: number;
-			nearZ: number;
-			farZ: number;
-		}
-
-		export interface FBXCameraModel extends FBXModel {
-			camera: FBXCameraAttr;
-		}
-
-		export interface FBXMarkerAttr extends FBXObject {
-			colour: Float3;
-		}
-
-		export interface FBXMarkerModel extends FBXModel {
-			marker: FBXMarkerAttr;
-		}
-
-		export interface FBXVideo extends FBXObject {
-			useMipMap: boolean;
-			filePath: string;
-			texture?: FBXTexture;
-			textureData?: ArrayBuffer;
-		}
-
-		export interface FBXTexture extends FBXObject {
-			uvStreamName: string;
-			uvScale: Float2;
-			uvOffset: Float2;
-		}
-
-		export interface FBXDocument {
-			geometries: FBXGeometry[];
-			lightAttrs: FBXLightAttr[];
-			cameraAttrs: FBXCameraAttr[];
-			markerAttrs: FBXMarkerAttr[];
-
-			meshes: FBXMeshModel[];
-			lights: FBXLightModel[];
-			cameras: FBXCameraModel[];
-			markers: FBXMarkerModel[];
-
-			materials: FBXMaterial[];
-			clips: FBXVideo[];
-			textures: FBXTexture[];
-		}
-
-
-		function makeFBXObject(name: string, values: parse.FBXValue[]): FBXObject {
-			assert(values.length == 3 &&
-				typeof values[0] == "number" &&
-				typeof values[1] == "string" &&
-				typeof values[2] == "string");
-
-			var classAndName = (<string>values[1]).split("::");
-			assert(values.length == 2);
-
-			var o: FBXObject = {
-				type: fbxObjectTypeForName(name),
-				id: <number>values[0],
-				name: classAndName[1],
-				"class": classAndName[0],
-				subClass: <string>values[2]
-			};
-
-			switch (o.type) {
-				case FBXObjectType.Geometry:
-					{
-						let g = <FBXGeometry>(o);
-						g.vertexCount = 0;
-						g.triangleCount = 0;
-						g.positions = null;
-						g.triangleIndexes = null;
-						g.streams = [];
-					}
-					break;
-				case FBXObjectType.Material:
-					{
-						let m = <FBXMaterial>(o);
-						m.material = makeMaterial();
-					}
-					break;
-				case FBXObjectType.Model:
-					{
-						let m = <FBXModel>(o);
-						m.localPosition = [0, 0, 0];
-						m.localRotation = [0, 0, 0];
-						m.localScaling = [1, 1, 1];
-
-						if (o.subClass == "Mesh") {
-							let mm = <FBXMeshModel>(m);
-							mm.geometry = null;
-							mm.materials = [];
-						}
-						else if (o.subClass == "Light") {
-							let lm = <FBXLightModel>(m);
-							lm.light = null;
-						}
-						else if (o.subClass == "Camera") {
-							let cm = <FBXCameraModel>(m);
-							cm.camera = null;
-						}
-						else if (o.subClass == "Marker") {
-							let mm = <FBXMarkerModel>(o);
-							mm.marker = null;
-						}
-					}
-					break;
-				case FBXObjectType.NodeAttribute:
-					{
-						if (o.subClass == "Light") {
-							let la = <FBXLightAttr>(o);
-							la.colour = [0, 0, 0];
-							la.intensity = 0;
-						}
-						else if (o.subClass == "Camera") {
-							let ca = <FBXCameraAttr>(o);
-							ca.clearColour = [0, 0, 0];
-							ca.nearZ = 1;
-							ca.farZ = 1000;
-							ca.fov = 45;
-						}
-						else if (o.subClass == "Marker") {
-							let ma = <FBXMarkerAttr>(o);
-							ma.colour = [0, 0, 0];
-						}
-					}
-					break;
-				case FBXObjectType.Texture:
-					{
-						let t = <FBXTexture>(o);
-						t.uvOffset = [0, 0];
-						t.uvScale = [1, 1];
-						t.uvStreamName = "";
-					}
-					break;
-				case FBXObjectType.Video:
-					{
-						let v = <FBXVideo>(o);
-						v.filePath = "";
-						v.texture = null;
-						v.textureData = null;
-					}
-					break;
-			}
-
-			return o;
-		}
-
-
 		// -- Document builder
 
-		const enum Section {
-			Root,
+		function i2i(values: parse.FBXValue[]) {
+			var str = convertBytesToString(new Uint8Array(<ArrayBuffer>values[0]));
+			var b64 = btoa(str);
+			str = "data:image/png;base64," + b64;
+			var img = new Image();
+			img.src = str;
+		}
 
+
+		class Node {
+			name: string;
+			type: parse.FBXPropertyType;
+			typeName: string;
+			values: parse.FBXValue[];
+			children: Node[];
+			parent: Node;
+
+			connectionsIn: Connection[];
+			connectionsOut: Connection[];
+
+			constructor(name: string, values: parse.FBXValue[], type: parse.FBXPropertyType = parse.FBXPropertyType.Unknown, typeName: string = "") {
+				this.name = name;
+				this.values = values;
+				this.type = type;
+				this.typeName = typeName;
+
+				this.children = [];
+				this.parent = null;
+				this.connectionsIn = [];
+				this.connectionsOut = [];
+			}
+
+			appendChild(node: Node) {
+				assert(node.parent == null, "Can't re-parent a Node");
+				node.parent = this;
+				this.children.push(node);
+			}
+		}
+
+
+		type ObjectSet = { [id: number]: Node };
+
+
+		interface Connection {
+			fromID: number;
+			fromNode?: Node;
+			toID: number;
+			toNode?: Node;
+			propName?: string;
+		}
+
+
+		export interface FBXDocument {
+		}
+
+
+		class FBXDocumentBuilder {
+			private globals: Node[];
+
+			private allObjects: ObjectSet; 
+			private geometryNodes: ObjectSet;
+			private videoNodes: ObjectSet;
+			private textureNodes: ObjectSet;
+			private materialNodes: ObjectSet;
+			private modelNodes: ObjectSet;
+
+			private connections: Connection[];
+			private rootNode: Node;
+
+			constructor() {
+				this.globals = [];
+
+				this.allObjects = {};
+				this.geometryNodes = {};
+				this.videoNodes = {};
+				this.textureNodes = {};
+				this.materialNodes = {};
+				this.modelNodes = {};
+
+				this.connections = [];
+
+				this.rootNode = new Node("RootNode", [0, "Model::RootNode", "RootNode"])
+				this.allObjects[0] = this.rootNode;
+			}
+
+			globalSetting(node: Node) {
+				this.globals.push(node);
+			}
+
+			addObject(node: Node) {
+				var typeSetMap: { [name: string]: ObjectSet } = {
+					"Geometry": this.geometryNodes,
+					"Video": this.videoNodes,
+					"Texture": this.textureNodes,
+					"Material": this.materialNodes,
+					"Model": this.modelNodes
+				};
+
+				var id = <number>node.values[0];
+				var set = typeSetMap[node.name];
+				assert(set != null, "Unknown object class " + node.name);
+				set[id] = node;
+				this.allObjects[id] = node;
+			}
+
+			addConnection(conn: Connection) {
+				conn.fromNode = this.allObjects[conn.fromID];
+				conn.toNode = this.allObjects[conn.toID];
+				
+				if (conn.fromNode && conn.toNode) {
+					conn.fromNode.connectionsOut.push(conn);
+					conn.toNode.connectionsIn.push(conn);
+					this.connections.push(conn);
+				}
+			}
+		}
+
+
+		const enum BuilderState {
+			Root,
 			GlobalSettings,
 			Objects,
+			Object,
 			Connections
 		}
 
 
-		export class FBX7DocumentBuilder implements parse.FBXParserDelegate {
-			private doc: FBXDocument;
-			private section = Section.Root;
-			private object: FBXObject = null;
+		export class FBX7DocumentParser implements parse.FBXParserDelegate {
+			private doc: FBXDocumentBuilder;
+			private state = BuilderState.Root;
+
 			private depth = 0;
+			private curObject: Node = null;
+			private curNodeParent: Node = null;
+
+			private knownObjects: Set<string>;
 
 			constructor() {
-				this.doc = {
-					geometries: [],
-					lightAttrs: [],
-					cameraAttrs: [],
-					markerAttrs: [],
-
-					meshes: [],
-					lights: [],
-					cameras: [],
-					markers: [],
-
-					materials: [],
-					clips: [],
-					textures: []
-				};
+				this.doc = new FBXDocumentBuilder();
+				this.knownObjects = new Set<string>(["Geometry", "Video", "Texture", "Material", "Model"]);
 			}
 
 
 			block(name: string, values: parse.FBXValue[]): parse.FBXBlockAction {
-				console.info("BLK: " + name, values);
 				var skip = false;
-				if (this.section == Section.Root) {
+
+				if (this.state == BuilderState.Root) {
 					if (name == "GlobalSettings")
-						this.section = Section.GlobalSettings;
+						this.state = BuilderState.GlobalSettings;
 					else if (name == "Objects")
-						this.section = Section.Objects;
+						this.state = BuilderState.Objects;
 					else if (name == "Connections")
-						this.section = Section.Connections;
+						this.state = BuilderState.Connections;
 					else
 						skip = true;
 				}
-				else if (this.section == Section.Objects) {
-					if (this.depth == 1) {
-						if (name == "Geometry") {
-							this.object = makeFBXObject(name, values);
-						}
+				else if (this.state == BuilderState.Objects) {
+					if (this.knownObjects.has(name)) {
+						this.state = BuilderState.Object;
+						this.curObject = new Node(name, values);
+						this.curNodeParent = this.curObject;
 					}
+					else {
+						skip = true;
+					}
+				}
+				else if (this.curNodeParent) {
+					var node = new Node(name, values);
+					this.curNodeParent.appendChild(node);
+					this.curNodeParent = node;
 				}
 
 				if (! skip) {
@@ -413,28 +293,61 @@ namespace sd.asset {
 
 
 			endBlock() {
-				console.info("/BLK");
 				this.depth--;
-				if (this.depth == 0) {
-					this.section = Section.Root;
+				if (this.depth == 1) {
+					if (this.state = BuilderState.Object) {
+						this.doc.addObject(this.curObject);
+
+						this.curObject = null;
+						this.curNodeParent = null;
+						this.state = BuilderState.Objects;
+					}
+				}
+				else if (this.depth == 0) {
+					this.state = BuilderState.Root;
+				}
+				else if (this.curNodeParent) {
+					this.curNodeParent = this.curNodeParent.parent;
+					assert(this.curNodeParent != null);
 				}
 			}
 
 
 			property(name: string, values: parse.FBXValue[]) {
-				console.info("Prop: " + name, values);
-				if (name == "Content") {
-					var str = convertBytesToString(new Uint8Array(<ArrayBuffer>values[0]));
-					var b64 = btoa(str);
-					str = "data:image/png;base64," + b64;
-					var img = new Image();
-					img.src = str;
+				this.typedProperty(name, parse.FBXPropertyType.Unknown, "", values);
+			}
+
+
+			typedProperty(name: string, type: parse.FBXPropertyType, typeName: string, values: parse.FBXValue[]) {
+				var node = new Node(name, values, type, typeName);
+
+				if (this.state == BuilderState.GlobalSettings) {
+					this.doc.globalSetting(node);
+				}
+				else if (this.state == BuilderState.Object) {
+					this.curNodeParent.appendChild(node);
+				}
+				else if (this.state == BuilderState.Connections) {
+					assert(name == "C", "Only C properties are allowed inside Connections");
+					var binding = <string>node.values[0];
+					var fromID = <number>node.values[1];
+					var toID = <number>node.values[2];
+
+					if (binding == "OO") {
+						this.doc.addConnection({ fromID: fromID, toID: toID });
+					}
+					else if (binding == "OP") {
+						this.doc.addConnection({ fromID: fromID, toID: toID, propName: <string>node.values[3] });
+					}
+					else {
+						console.warn("Don't know what to do with connection: ", node.values);
+					}
 				}
 			}
 
 
-			typedProperty(name: string, type: parse.FBXValueType, typeName: string, values: parse.FBXValue[]) {
-				console.info("Typed Prop: " + name + " " + typeName + " (" + type + ")", values);
+			completed() {
+				console.info(this.doc);
 			}
 
 
@@ -444,7 +357,7 @@ namespace sd.asset {
 
 
 			get document(): FBXDocument {
-				return this.doc;
+				return null;
 			}
 		}
 
@@ -452,7 +365,8 @@ namespace sd.asset {
 
 
 	function parseFBXSource(source: string | ArrayBuffer): fbx.FBXDocument {
- 		var del = new fbx.FBX7DocumentBuilder();
+		var t0 = performance.now();
+ 		var del = new fbx.FBX7DocumentParser();
 		var parser: fbx.parse.FBXParser;
 		if (typeof source === "string") {
 			parser = new fbx.parse.FBXTextParser(source, del);
@@ -461,6 +375,7 @@ namespace sd.asset {
 			parser = new fbx.parse.FBXBinaryParser(source, del);
 		}
 		parser.parse();
+		console.info("took: " + (performance.now() - t0).toFixed(1) + "ms");
 		return del.document;
 	}
 
