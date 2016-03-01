cf2cb0602a0e67df44be44304cf0f722886cbbed
Arthur Langereis <puurtuur@me.com>
2016-03-01 17:47:45 +0100

    add quick build script using closure-compiler

---
+++ build/minify.sh
@@ -0,0 +1,1 @@
+closure-compiler --language_in ECMASCRIPT5_STRICT --language_out ECMASCRIPT5_STRICT --env BROWSER --js_output_file stardazed-tx-min.js ../ext/gl-matrix.js ../ext/inflate.js stardazed-tx.js
