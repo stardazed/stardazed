c0cfa88971676e21b69458c03b567bfb75babb7a
Arthur Langereis <puurtuur@me.com>
2016-03-04 18:51:12 +0100

    data setup for SkeletonMgr

--- src/skeleton.ts
+++ src/skeleton.ts
@@ -12,7 +12,7 @@ namespace sd.world {
 
 
 	export interface SkelAnimX {
-		anim: asset.SkeletonAnimation;
+		animation: asset.SkeletonAnimation;
 		looping: boolean;
 		movementScale?: number;
 		speedMultiplier?: number;
@@ -21,18 +21,28 @@ namespace sd.world {
 
 	export class SkeletonManager implements ComponentManager<SkeletonManager> {
 		private instanceData_: container.MultiArrayBuffer;
+		private entityBase_: EntityArrayView;
+		private transformBase_: TransformArrayView;
+		private jointOffsetBase_: Int32Array;
+		private jointCountBase_: Int32Array;
+
+		private jointData_: Float32Array;
 		private jointDataTex_: render.Texture;
-		private nextFreeJointIndex_ = 0;
 
 		constructor(private rc: render.RenderContext, private transformMgr_: TransformManager) {
-			var fields: container.MABField[] = [
-				
+			var instFields: container.MABField[] = [
+				{ type: SInt32, count: 1 }, // entity
+				{ type: SInt32, count: 1 }, // transform
+				{ type: SInt32, count: 1 }, // jointOffset
+				{ type: SInt32, count: 1 }, // jointCount
 			];
+			this.instanceData_ = new container.MultiArrayBuffer(128, instFields);
+			this.rebase();
 
-			// 
-			var texData = new Float32Array(256 * 256 * 4);
+			// -- the compiled joint data texture and client copy
+			this.jointData_ = new Float32Array(256 * 256 * 4);
 			var td = render.makeTexDesc2D(render.PixelFormat.RGBA32F, 256, 256, render.UseMipMaps.No);
-			td.pixelData = [texData];
+			td.pixelData = [this.jointData_];
 			td.sampling.magFilter = render.TextureSizingFilter.Nearest;
 			td.sampling.minFilter = render.TextureSizingFilter.Nearest;
 			td.sampling.mipFilter = render.TextureMipFilter.None;
@@ -42,12 +52,22 @@ namespace sd.world {
 		}
 
 
-		registerSkeleton(name: string, bindPose: asset.Model) {
-
+		private rebase() {
+			this.entityBase_ = this.instanceData_.indexedFieldView(0);
+			this.transformBase_ = this.instanceData_.indexedFieldView(1);
+			this.jointOffsetBase_ = <Int32Array>this.instanceData_.indexedFieldView(2);
+			this.jointCountBase_ = <Int32Array>this.instanceData_.indexedFieldView(3);
 		}
 
 
-		create(rootTX: TransformInstance) {
+		create(rootEntity: Entity) {
+			if (this.instanceData_.extend() == container.InvalidatePointers.Yes) {
+				this.rebase();
+			}
+			var ix = this.instanceData_.count;
+
+			this.entityBase_[ix] = <number>rootEntity;
+			this.transformBase_[ix] = <number>this.transformMgr_.forEntity(rootEntity);
 
 		}
 
@@ -80,13 +100,56 @@ namespace sd.world {
 		// ----
 
 
+		private updateJointData(skel: world.TransformInstance[]) {
+			var count = skel.length;
+			var txm = this.transformMgr_;
+			var gl = this.rc.gl;
+			var texData = new Float32Array(256 * 4 * 4);
+
+			var parent = txm.parent(skel[0]);
+			var originWorldTransform = txm.worldMatrix(parent);
+			var invOriginWorldTransform = mat4.invert([], originWorldTransform);
+
+			for (var ji = 0; ji < count; ++ji) {
+				var j = skel[ji];
+				var texelBaseIndex = ji * 8;
+
+				var xform = txm.copyWorldMatrix(j);
+				mat4.multiply(xform, invOriginWorldTransform, xform);
+
+				// container.setIndexedVec4(texData, texelBaseIndex, [0,0,0,1]);
+				container.setIndexedVec4(texData, texelBaseIndex, quat.invert([], txm.localRotation(j)));
+				container.setIndexedMat4(texData, (ji * 2) + 1, xform);
+			}
+
+			// FIXME: need to add this to Texture
+			this.jointDataTex_.bind();
+			gl.texSubImage2D(this.jointDataTex_.target, 0, 0, 0, 256, 4, gl.RGBA, gl.FLOAT, texData);
+			this.jointDataTex_.unbind();
+		}
+
+
 		startAnimation(skel: SkeletonInstance, anim: asset.SkeletonAnimation) {
 			
 		}
 
 
 		update(dt: number) {
-			
+		/*
+			minJoint = Infinity, maxJoint = 0
+			for each animatingJoint
+				update minJoint, maxJoint from animatingJoint
+				for each animation on animatingJoint
+					init totalPos, totalRot, totalScale
+					totalPos += animation.blendFactor * joint.calculatedPos
+					totalRot += animation.blendFactor * joint.calculatedRot
+					totalScale += animation.blendFactor * joint.calculatedScale
+				end
+				set pos, rot, scale of animatingJoint
+			end
+
+			updateJointData(minJoint, maxJoint)
+		*/
 		}
 	}
 
