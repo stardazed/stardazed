f162a3ff7c44127118bd26a04121d2b6ef5e0ecb
Arthur Langereis <puurtuur@me.com>
2016-09-20 16:18:11 +0200

    meshx: bind and unbind

--- src/meshx.ts
+++ src/meshx.ts
@@ -110,13 +110,6 @@ namespace sd.world {
 	}
 
 
-	interface AttributeLocation {
-		attribute: meshdata.PositionedAttribute;
-		clientBuffer: meshdata.VertexBuffer;
-		buffer: render.Buffer;
-	}
-
-
 	const enum MeshFeatures {
 		VertexPositions = 1,
 		VertexNormals = 2,
@@ -125,6 +118,14 @@ namespace sd.world {
 		VertexColours = 16,
 		VertexWeights = 32,
 		Indexes = 64
+	}
+
+
+	interface MeshAttributeData {
+		buffer: WebGLBuffer;
+		vertexField: meshdata.VertexField;
+		offset: number;
+		stride: number;
 	}
 
 
@@ -143,6 +144,7 @@ namespace sd.world {
 
 	export class MeshManager implements ComponentManager<MeshManager> {
 		private instanceData_: container.MultiArrayBuffer;
+		private featuresBase_: ConstEnumArrayView<MeshFeatures>;
 		private primitiveTypeBase_: ConstEnumArrayView<meshdata.PrimitiveType>;
 		private glPrimitiveTypeBase_: Int32Array;
 		private glIndexElementTypeBase_: Int32Array;
@@ -155,26 +157,24 @@ namespace sd.world {
 		private bufGLBuffers_: (WebGLBuffer | null)[];
 
 		private attributeData_: container.MultiArrayBuffer;
+		private attrRoleBase_: ConstEnumArrayView<meshdata.VertexAttributeRole>;
 		private attrBufferIndexBase_: Int32Array;
 		private attrVertexFieldBase_: ConstEnumArrayView<meshdata.VertexField>;
 		private attrFieldOffsetBase_: Int32Array;
 		private attrStrideBase_: Int32Array;
 
 		private primGroupData_: container.MultiArrayBuffer;
-		private pgFeaturesBase_: Int32Array;
 		private pgFromPrimIxBase_: Int32Array;
 		private pgPrimCountBase_: Int32Array;
 		private pgMaterialBase_: Int32Array;
 
+		private pipelineVAOMaps_: WeakMap<render.Pipeline, WebGLVertexArrayObjectOES>[] | null = null;
 		private entityMap_: Map<Entity, MeshInstance>;
 
-/*
-		private pipelineVAOMaps_: WeakMap<Pipeline, WebGLVertexArrayObjectOES> | null = null;
-		private attributes_ = new Map<meshdata.VertexAttributeRole, AttributeLocation>();
-*/
 
 		constructor(private rctx_: render.RenderContext) {
 			var instanceFields: container.MABField[] = [
+				{ type: SInt32, count: 1 }, // features
 				{ type: SInt32, count: 1 }, // primitiveType
 				{ type: SInt32, count: 1 }, // glPrimitiveType
 				{ type: SInt32, count: 1 }, // glIndexElementType
@@ -193,6 +193,7 @@ namespace sd.world {
 			this.rebaseBuffers();
 
 			var attrFields: container.MABField[] = [
+				{ type: SInt32, count: 1 }, // role
 				{ type: SInt32, count: 1 }, // bufferIndex
 				{ type: SInt32, count: 1 }, // vertexField
 				{ type: SInt32, count: 1 }, // fieldOffset
@@ -202,7 +203,6 @@ namespace sd.world {
 			this.rebaseAttributes();
 
 			var pgFields: container.MABField[] = [
-				{ type: SInt32, count: 1 }, // features
 				{ type: SInt32, count: 1 }, // fromPrimIx
 				{ type: SInt32, count: 1 }, // primCount
 				{ type: SInt32, count: 1 }, // materialIx - mesh-local zero-based material indexes
@@ -211,16 +211,21 @@ namespace sd.world {
 			this.rebasePrimGroups();
 
 			this.entityMap_ = new Map<Entity, MeshInstance>();
+
+			if (rctx_.extVAO) {
+				this.pipelineVAOMaps_ = [];
+			}
 		}
 
 
 		rebaseInstances() {
-			this.primitiveTypeBase_ = this.instanceData_.indexedFieldView(0);
-			this.glPrimitiveTypeBase_ = this.instanceData_.indexedFieldView(1);
-			this.glIndexElementTypeBase_ = this.instanceData_.indexedFieldView(2);
-			this.indexElementSizeBytesBase_ = this.instanceData_.indexedFieldView(3);
-			this.buffersOffsetCountBase_ = this.instanceData_.indexedFieldView(4);
-			this.attrsOffsetCountBase_ = this.instanceData_.indexedFieldView(5);
+			this.featuresBase_ = this.instanceData_.indexedFieldView(0);
+			this.primitiveTypeBase_ = this.instanceData_.indexedFieldView(1);
+			this.glPrimitiveTypeBase_ = this.instanceData_.indexedFieldView(2);
+			this.glIndexElementTypeBase_ = this.instanceData_.indexedFieldView(3);
+			this.indexElementSizeBytesBase_ = this.instanceData_.indexedFieldView(4);
+			this.buffersOffsetCountBase_ = this.instanceData_.indexedFieldView(5);
+			this.attrsOffsetCountBase_ = this.instanceData_.indexedFieldView(6);
 		}
 
 
@@ -230,18 +235,18 @@ namespace sd.world {
 
 
 		rebaseAttributes() {
-			this.attrBufferIndexBase_ = this.attributeData_.indexedFieldView(0);
-			this.attrVertexFieldBase_ = this.attributeData_.indexedFieldView(1);
-			this.attrFieldOffsetBase_ = this.attributeData_.indexedFieldView(2);
-			this.attrStrideBase_ = this.attributeData_.indexedFieldView(3);
+			this.attrRoleBase_ = this.attributeData_.indexedFieldView(0);
+			this.attrBufferIndexBase_ = this.attributeData_.indexedFieldView(1);
+			this.attrVertexFieldBase_ = this.attributeData_.indexedFieldView(2);
+			this.attrFieldOffsetBase_ = this.attributeData_.indexedFieldView(3);
+			this.attrStrideBase_ = this.attributeData_.indexedFieldView(4);
 		}
 
 
 		rebasePrimGroups() {
-			this.pgFeaturesBase_ = this.primGroupData_.indexedFieldView(0);
-			this.pgFromPrimIxBase_ = this.primGroupData_.indexedFieldView(1);
-			this.pgPrimCountBase_ = this.primGroupData_.indexedFieldView(2);
-			this.pgMaterialBase_ = this.primGroupData_.indexedFieldView(3);
+			this.pgFromPrimIxBase_ = this.primGroupData_.indexedFieldView(0);
+			this.pgPrimCountBase_ = this.primGroupData_.indexedFieldView(1);
+			this.pgMaterialBase_ = this.primGroupData_.indexedFieldView(2);
 		}
 
 
@@ -253,6 +258,8 @@ namespace sd.world {
 				this.rebaseInstances();
 			}
 			const instance = this.instanceData_.count;
+
+			var meshFeatures: MeshFeatures = 0;
 
 			const bufferCount = meshData.vertexBuffers.length + (meshData.indexBuffer !== null ? 1 : 0);
 			var bufferIndex = this.bufferData_.count;
@@ -268,7 +275,7 @@ namespace sd.world {
 			}
 			container.setIndexedVec2(this.attrsOffsetCountBase_, instance, [attrIndex, attrCount]);
 
-
+			// -- allocate gpu vertex buffers and cache attribute mappings for fast binding
 			for (var vertexBuffer of meshData.vertexBuffers) {
 				let glBuf = gl.createBuffer(); // FIXME: could fail
 				gl.bindBuffer(gl.ARRAY_BUFFER, glBuf);
@@ -280,10 +287,22 @@ namespace sd.world {
 				for (let aix = 0; aix < vertexBuffer.attributeCount; ++aix) {
 					let attr = vertexBuffer.attrByIndex(aix)!;
 
+					this.attrRoleBase_[attrIndex] = attr.role;
 					this.attrBufferIndexBase_[attrIndex] = bufferIndex;
 					this.attrVertexFieldBase_[attrIndex] = attr.field;
 					this.attrFieldOffsetBase_[attrIndex] = attr.offset;
 					this.attrStrideBase_[attrIndex] = vertexBuffer.strideBytes;
+
+					// set the appropriate mesh feature flag based on the attribute role
+					switch (attr.role) {
+						case meshdata.VertexAttributeRole.Position: meshFeatures |= MeshFeatures.VertexPositions; break;
+						case meshdata.VertexAttributeRole.Normal: meshFeatures |= MeshFeatures.VertexNormals; break;
+						case meshdata.VertexAttributeRole.Tangent: meshFeatures |= MeshFeatures.VertexTangents; break;
+						case meshdata.VertexAttributeRole.UV0: meshFeatures |= MeshFeatures.VertexUVs; break; // UV1,2,3 can only occur alongside UV0
+						case meshdata.VertexAttributeRole.Colour: meshFeatures |= MeshFeatures.VertexColours; break;
+						case meshdata.VertexAttributeRole.WeightedPos0: meshFeatures |= MeshFeatures.VertexWeights; break;
+						default: break;
+					}
 
 					attrIndex += 1;
 				}
@@ -291,6 +310,7 @@ namespace sd.world {
 				bufferIndex += 1;
 			}
 
+			// -- allocate gpu index buffer if present and cache some index info as it is accessed frequently
 			if (meshData.indexBuffer) {
 				let glBuf = gl.createBuffer();
 				gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, glBuf);
@@ -298,10 +318,11 @@ namespace sd.world {
 				this.bufGLBuffers_[bufferIndex] = glBuf;
 				this.bufGLTargetBase_[bufferIndex] = gl.ELEMENT_ARRAY_BUFFER;
 			
-				// -- precompute some info required for draw calls
 				this.primitiveTypeBase_[instance] = meshData.indexBuffer.primitiveType;
 				this.glIndexElementTypeBase_[instance] = glTypeForIndexElementType(this.rctx_, meshData.indexBuffer.indexElementType);
 				this.indexElementSizeBytesBase_[instance] = meshData.indexBuffer.indexElementSizeBytes;
+
+				meshFeatures |= MeshFeatures.Indexes;
 
 				bufferIndex += 1;
 			}
@@ -312,7 +333,7 @@ namespace sd.world {
 			this.glPrimitiveTypeBase_[instance] = glTypeForPrimitiveType(this.rctx_, this.primitiveTypeBase_[instance]);
 
 
-			// -- primitive groups
+			// -- cache primitive groups and metadata
 			const primGroupCount = meshData.primitiveGroups.length;
 			assert(primGroupCount > 0, "No primitive groups present in meshData");
 			var primGroupIndex = this.primGroupData_.count;
@@ -321,7 +342,6 @@ namespace sd.world {
 			}
 
 			for (let pg of meshData.primitiveGroups) {
-				this.pgFeaturesBase_[primGroupIndex] = 0;
 				this.pgFromPrimIxBase_[primGroupIndex] = pg.fromPrimIx;
 				this.pgPrimCountBase_[primGroupIndex] = pg.primCount;
 				this.pgMaterialBase_[primGroupIndex] = pg.materialIx;
@@ -329,9 +349,13 @@ namespace sd.world {
 				primGroupIndex += 1;
 			}
 
-			// if (this.rctx_.extVAO) {
-			// 	this.pipelineVAOMap_ = new WeakMap<render.Pipeline, WebGLVertexArrayObjectOES>();
-			// }
+			// -- store mesh features accumulated during the creation process
+			this.featuresBase_[instance] = meshFeatures;
+
+			// -- we weakly link Pipelines to VAO mappings per mesh, see bind()
+			if (this.pipelineVAOMaps_) {
+				this.pipelineVAOMaps_[instance] = new WeakMap<render.Pipeline, WebGLVertexArrayObjectOES>();
+			}
 
 			return instance;
 		}
@@ -351,6 +375,8 @@ namespace sd.world {
 
 
 		destroy(inst: MeshInstance) {
+			// TODO: remove mesh from all instances (add a reverse map->entities map?)
+			// TODO: zero+free all array segments
 		}
 
 
@@ -373,7 +399,114 @@ namespace sd.world {
 		}
 
 
+		// -- actions
+
+		private bindSingleAttribute(attr: MeshAttributeData, toVAIndex: number) {
+			var elementCount = meshdata.vertexFieldElementCount(attr.vertexField);
+			var normalized = meshdata.vertexFieldIsNormalized(attr.vertexField);
+			var glElementType = glTypeForVertexField(this.rctx_, attr.vertexField);
+
+			this.rctx_.gl.enableVertexAttribArray(toVAIndex);
+			this.rctx_.gl.vertexAttribPointer(toVAIndex, elementCount, glElementType, normalized, attr.stride, attr.offset);
+		}
+
+
+		private meshAttributesData(inst: MeshInstance): Map<meshdata.VertexAttributeRole, MeshAttributeData> {
+			const attrs = new Map<meshdata.VertexAttributeRole, MeshAttributeData>();
+			const meshIx = <number>inst;
+			const offsetCount = container.copyIndexedVec2(this.attrsOffsetCountBase_, meshIx);
+
+			for (let aix = 0; aix < offsetCount[1]; ++aix) {
+				let attrOffset = aix + offsetCount[0];
+
+				attrs.set(this.attrRoleBase_[attrOffset], {
+					buffer: this.bufGLBuffers_[this.attrBufferIndexBase_[attrOffset]]!,
+					vertexField: this.attrVertexFieldBase_[attrOffset],
+					offset: this.attrFieldOffsetBase_[attrOffset],
+					stride: this.attrStrideBase_[attrOffset]
+				});
+			}
+
+			return attrs;
+		}
+
+
+		bind(inst: MeshInstance, toPipeline: render.Pipeline) {
+			const meshIx = <number>inst;
+			const gl = this.rctx_.gl;
+			var plVAO: WebGLVertexArrayObjectOES | undefined;
+			var needBinding = true;
+
+			if (this.pipelineVAOMaps_) {
+				// If we're using VAOs then each mesh has a VAO per Pipeline it is bound to.
+				// This approach is sadly necessary as attribute indexes can differ for the same attributes for every Pipeline.
+				// gl.bindAttribLocation can be used to bind named attributes but different shaders would have to still specify
+				// a list of all their attributes and desired bind points, here we then have to detect a shader type and create
+				// one VAO per separate shader, maybe later.
+				plVAO = this.pipelineVAOMaps_[meshIx].get(toPipeline);
+				if (plVAO) {
+					needBinding = false;
+				}
+				else {
+					plVAO = this.rctx_.extVAO.createVertexArrayOES();
+					this.pipelineVAOMaps_[meshIx].set(toPipeline, plVAO);
+				}
+
+				this.rctx_.extVAO.bindVertexArrayOES(plVAO || null);
+			}
+
+			if (needBinding) {
+				let roleIndexes = toPipeline.attributePairs();
+				let pair = roleIndexes.next();
+				let attributes = this.meshAttributesData(inst);
+
+				while (! pair.done) {
+					var attrRole = pair.value![0];
+					var attrIndex = pair.value![1];
+
+					var meshAttr = attributes.get(attrRole);
+					if (meshAttr) {
+						gl.bindBuffer(gl.ARRAY_BUFFER, meshAttr.buffer);
+						this.bindSingleAttribute(meshAttr, attrIndex);
+					}
+					else {
+						console.warn("Mesh does not have Pipeline attr for index " + attrIndex + " of role " + attrRole);
+						gl.disableVertexAttribArray(attrIndex);
+					}
+
+					pair = roleIndexes.next();
+				}
+
+				if (this.featuresBase_[meshIx] & MeshFeatures.Indexes) {
+					// the index buffer, when present, is the last buffer in the list
+					let bufOC = container.copyIndexedVec2(this.buffersOffsetCountBase_, meshIx);
+					let indexBuffer = this.bufGLBuffers_[bufOC[0] + bufOC[1] - 1];
+					gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+				}
+			}
+		}
+
+
+		unbind(fromPipeline: render.Pipeline) {
+			if (this.pipelineVAOMaps_) {
+				this.rctx_.extVAO.bindVertexArrayOES(null);
+			}
+			else {
+				// -- explicitly disable all attributes specified in the pipeline
+				var roleIndexes = fromPipeline.attributePairs();
+				var pair = roleIndexes.next();
+
+				while (! pair.done) {
+					var attrIndex = pair.value![1];
+					this.rctx_.gl.disableVertexAttribArray(attrIndex);
+					pair = roleIndexes.next();
+				}
+			}
+		}
+
+
 		// -- single instance getters
+
 	}
 
 } // ns sd.world
