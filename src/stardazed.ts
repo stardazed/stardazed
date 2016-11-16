4bd45c00d54e5ed178bf0dabc8f025f5be307472
Arthur Langereis <puurtuur@me.com>
2016-11-16 14:09:50 +0100

    refactor(core): add experimental top-level umbrella module

---
+++ src/stardazed.ts
@@ -0,0 +1,21 @@
+// stardazed.ts - main library entry point file
+// Part of Stardazed TX
+// (c) 2015-2016 by Arthur Langereis - @zenmumbler
+// https://github.com/stardazed/stardazed-tx
+
+export * from "core/util";
+export * from "core/array";
+export * from "core/typedarray";
+export * from "core/numeric";
+
+export * from "core/dom";
+
+export * from "core/deque";
+export * from "core/multiarraybuffer";
+
+export * from "core/perftimer";
+export * from "core/runloop";
+
+// testing how to approach submodules
+import * as input from "input/input";
+export { input };
