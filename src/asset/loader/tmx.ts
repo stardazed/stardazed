b93bd8088614692b93eac9869c64253c780b33e2
Arthur Langereis <puurtuur@me.com>
2016-11-22 18:17:37 +0100

    refactor(asset): modularise loader/tmx

--- src/asset/loader/tmx.ts
+++ src/asset/loader/tmx.ts
@@ -1,124 +1,125 @@
-// asset-tmx - Tiled (mapeditor.org) TMX files and types
+// asset/loader/tmx - Tiled (mapeditor.org) TMX files and types
 // Part of Stardazed TX
 // (c) 2015-2016 by Arthur Langereis - @zenmumbler
 // https://github.com/stardazed/stardazed-tx
 
-namespace sd.asset {
+// FIXME: not an actual asset loader yet, needs to be converted, requires update to AssetGroup
 
-	// Minimal support for orthogonal right-down order, base64 encoded tile data
+// import * as registry from "asset/registry";
+import { FileLoadType, loadFile } from "asset/util";
 
-	export class TMXLayer {
-		width: number;
-		height: number;
-		tileData: Uint32Array;
+// Minimal support for orthogonal right-down order, base64 encoded tile data
 
-		constructor(layerNode: Node) {
-			const layerText = (layerNode.textContent || "").trim();
-			const byteView = new Uint8Array(atob(layerText).split("").map(c => { return c.charCodeAt(0); }));
-			this.tileData = new Uint32Array(byteView.buffer);
+export class TMXLayer {
+	width: number;
+	height: number;
+	tileData: Uint32Array;
 
-			for (const attr of [].slice.call(layerNode.attributes, 0)) {
-				if (attr.nodeName == "width") {
-					this.width = parseInt(attr.textContent || "0");
-				}
-				if (attr.nodeName == "height") {
-					this.height = parseInt(attr.textContent || "0");
-				}
+	constructor(layerNode: Node) {
+		const layerText = (layerNode.textContent || "").trim();
+		const byteView = new Uint8Array(atob(layerText).split("").map(c => { return c.charCodeAt(0); }));
+		this.tileData = new Uint32Array(byteView.buffer);
+
+		for (const attr of [].slice.call(layerNode.attributes, 0)) {
+			if (attr.nodeName == "width") {
+				this.width = parseInt(attr.textContent || "0");
 			}
-		}
-
-		tileAt(col: number, row: number) {
-			if (row < 0 || col < 0 || row >= this.height || col >= this.width) {
-				return -1;
-			}
-			return this.tileData[(row * this.width) + col];
-		}
-
-		setTileAt(col: number, row: number, tile: number) {
-			if (row < 0 || col < 0 || row >= this.height || col >= this.width) {
-				return;
-			}
-			this.tileData[(row * this.width) + col] = tile;
-		}
-
-		eachTile(callback: (row: number, col: number, tile: number) => void) {
-			let off = 0;
-			for (let row = 0; row < this.height; ++row) {
-				for (let col = 0; col < this.width; ++col) {
-					if (this.tileData[off]) {
-						callback(row, col, this.tileData[off]);
-					}
-					++off;
-				}
+			if (attr.nodeName == "height") {
+				this.height = parseInt(attr.textContent || "0");
 			}
 		}
 	}
 
+	tileAt(col: number, row: number) {
+		if (row < 0 || col < 0 || row >= this.height || col >= this.width) {
+			return -1;
+		}
+		return this.tileData[(row * this.width) + col];
+	}
 
-	export class TMXObjectGroup {
-		constructor(_groupNode: Node) {
-			// TODO: TBI
+	setTileAt(col: number, row: number, tile: number) {
+		if (row < 0 || col < 0 || row >= this.height || col >= this.width) {
+			return;
+		}
+		this.tileData[(row * this.width) + col] = tile;
+	}
+
+	eachTile(callback: (row: number, col: number, tile: number) => void) {
+		let off = 0;
+		for (let row = 0; row < this.height; ++row) {
+			for (let col = 0; col < this.width; ++col) {
+				if (this.tileData[off]) {
+					callback(row, col, this.tileData[off]);
+				}
+				++off;
+			}
 		}
 	}
+}
 
 
-	export type TMXLayerSet = { [name: string]: TMXLayer; };
-	export type TMXObjectGroupSet = { [name: string]: TMXObjectGroup; };
-
-
-	export class TMXData {
-		layers: TMXLayerSet = {};
-		objectGroups: TMXObjectGroupSet = {};
-
-		private width_ = 0;
-		private height_ = 0;
-
-		get width() { return this.width_; }
-		get height() { return this.height_; }
-
-		load(filePath: string): Promise<TMXData> {
-			return loadFile(filePath, {
-				tryBreakCache: true,
-				mimeType: "application/xml",
-				responseType: FileLoadType.Document
-			}).then(
-				(dataXML: XMLDocument) => {
-					const tileDoc = dataXML.childNodes[0];
-
-					for (const attr of [].slice.call(tileDoc.attributes, 0)) {
-						if (attr.nodeName == "width") {
-							this.width_ = parseInt(attr.textContent || "0");
-						}
-						if (attr.nodeName == "height") {
-							this.height_ = parseInt(attr.textContent || "0");
-						}
-					}
-
-					for (let ix = 0; ix < tileDoc.childNodes.length; ++ix) { // tslint:disable-line:prefer-for-of
-						const node = tileDoc.childNodes[ix];
-						let nameAttr: Attr;
-						let name: string | null;
-
-						if (node.nodeName == "layer") {
-							nameAttr = node.attributes.getNamedItem("name");
-							name = nameAttr && nameAttr.textContent;
-							if (name) {
-								this.layers[name] = new TMXLayer(node);
-							}
-						}
-						else if (node.nodeName == "objectgroup") {
-							nameAttr = node.attributes.getNamedItem("name");
-							name = nameAttr && nameAttr.textContent;
-							if (name) {
-								this.objectGroups[name] = new TMXObjectGroup(node);
-							}
-						}
-					}
-
-					return this;
-				}
-			);
-		};
+export class TMXObjectGroup {
+	constructor(_groupNode: Node) {
+		// TODO: TBI
 	}
+}
 
-} // ns sd.asset
+
+export type TMXLayerSet = { [name: string]: TMXLayer; };
+export type TMXObjectGroupSet = { [name: string]: TMXObjectGroup; };
+
+
+export class TMXData {
+	layers: TMXLayerSet = {};
+	objectGroups: TMXObjectGroupSet = {};
+
+	private width_ = 0;
+	private height_ = 0;
+
+	get width() { return this.width_; }
+	get height() { return this.height_; }
+
+	load(filePath: string): Promise<TMXData> {
+		return loadFile(filePath, {
+			tryBreakCache: true,
+			mimeType: "application/xml",
+			responseType: FileLoadType.Document
+		}).then(
+			(dataXML: XMLDocument) => {
+				const tileDoc = dataXML.childNodes[0];
+
+				for (const attr of [].slice.call(tileDoc.attributes, 0)) {
+					if (attr.nodeName == "width") {
+						this.width_ = parseInt(attr.textContent || "0");
+					}
+					if (attr.nodeName == "height") {
+						this.height_ = parseInt(attr.textContent || "0");
+					}
+				}
+
+				for (let ix = 0; ix < tileDoc.childNodes.length; ++ix) { // tslint:disable-line:prefer-for-of
+					const node = tileDoc.childNodes[ix];
+					let nameAttr: Attr;
+					let name: string | null;
+
+					if (node.nodeName == "layer") {
+						nameAttr = node.attributes.getNamedItem("name");
+						name = nameAttr && nameAttr.textContent;
+						if (name) {
+							this.layers[name] = new TMXLayer(node);
+						}
+					}
+					else if (node.nodeName == "objectgroup") {
+						nameAttr = node.attributes.getNamedItem("name");
+						name = nameAttr && nameAttr.textContent;
+						if (name) {
+							this.objectGroups[name] = new TMXObjectGroup(node);
+						}
+					}
+				}
+
+				return this;
+			}
+		);
+	};
+}
