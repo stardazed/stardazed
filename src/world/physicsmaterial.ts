c62f7d8e8437d385a6d1d1569e465dfcf788a909
Arthur Langereis <puurtuur@me.com>
2016-11-23 16:26:21 +0100

    refactor(world): modularise physicsmaterial

--- src/world/physicsmaterial.ts
+++ src/world/physicsmaterial.ts
@@ -1,70 +1,69 @@
-// physicsmaterial - Physics Material asset
+// world/physicsmaterial - Physics Material asset
 // Part of Stardazed TX
 // (c) 2016 by Arthur Langereis - @zenmumbler
 // https://github.com/stardazed/stardazed-tx
 
-namespace sd.world {
+import { assert, cloneStruct } from "core/util";
+import { Instance, InstanceArrayView } from "world/instance";
 
-	export type PhysicsMaterialRef = Instance<PhysicsMaterialManager>;
-	export type PhysicsMaterialArrayView = InstanceArrayView<PhysicsMaterialManager>;
+export type PhysicsMaterialRef = Instance<PhysicsMaterialManager>;
+export type PhysicsMaterialArrayView = InstanceArrayView<PhysicsMaterialManager>;
 
 
-	export class PhysicsMaterialData {
-		friction = 0;    // 0..1
-		restitution = 0; // 0..1
+export class PhysicsMaterialData {
+	friction = 0;    // 0..1
+	restitution = 0; // 0..1
+}
+
+
+export class PhysicsMaterialManager {
+	private instanceData_: (PhysicsMaterialData | null)[] = [null];
+	private freed_ = new Set<PhysicsMaterialRef>();
+
+	create(desc: PhysicsMaterialData): PhysicsMaterialRef {
+		let nextRef: PhysicsMaterialRef = 0;
+		if (this.freed_.size > 0) {
+			nextRef = this.freed_.values().next().value!;
+			this.freed_.delete(nextRef);
+		}
+		else {
+			nextRef = this.instanceData_.length;
+		}
+
+		this.instanceData_[<number>nextRef] = cloneStruct(desc);
+		return nextRef;
 	}
 
 
-	export class PhysicsMaterialManager {
-		private instanceData_: (PhysicsMaterialData | null)[] = [null];
-		private freed_ = new Set<PhysicsMaterialRef>();
-
-		create(desc: PhysicsMaterialData): PhysicsMaterialRef {
-			let nextRef: PhysicsMaterialRef = 0;
-			if (this.freed_.size > 0) {
-				nextRef = this.freed_.values().next().value!;
-				this.freed_.delete(nextRef);
-			}
-			else {
-				nextRef = this.instanceData_.length;
-			}
-
-			this.instanceData_[<number>nextRef] = cloneStruct(desc);
-			return nextRef;
+	destroy(ref: PhysicsMaterialRef) {
+		if (! this.valid(ref)) {
+			return;
 		}
 
-
-		destroy(ref: PhysicsMaterialRef) {
-			if (! this.valid(ref)) {
-				return;
-			}
-
-			const index = <number>ref;
-			if (index == this.instanceData_.length - 1) {
-				this.instanceData_.length = index;
-			}
-			else {
-				this.instanceData_[index] = null;
-				this.freed_.add(ref);
-			}
+		const index = <number>ref;
+		if (index == this.instanceData_.length - 1) {
+			this.instanceData_.length = index;
 		}
-
-
-		get count() {
-			return this.instanceData_.length - 1 - this.freed_.size;
-		}
-
-
-		valid(ref: PhysicsMaterialRef) {
-			const index = <number>ref;
-			return (index < this.instanceData_.length) && (this.instanceData_[index] != null);
-		}
-
-
-		item(ref: PhysicsMaterialRef): PhysicsMaterialData {
-			assert(this.valid(ref));
-			return this.instanceData_[<number>ref]!; // the above assert validates the index
+		else {
+			this.instanceData_[index] = null;
+			this.freed_.add(ref);
 		}
 	}
 
-} // ns sd.world
+
+	get count() {
+		return this.instanceData_.length - 1 - this.freed_.size;
+	}
+
+
+	valid(ref: PhysicsMaterialRef) {
+		const index = <number>ref;
+		return (index < this.instanceData_.length) && (this.instanceData_[index] != null);
+	}
+
+
+	item(ref: PhysicsMaterialRef): PhysicsMaterialData {
+		assert(this.valid(ref));
+		return this.instanceData_[<number>ref]!; // the above assert validates the index
+	}
+}
