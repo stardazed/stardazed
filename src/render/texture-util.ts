8676d6455332d720f773b2598e6e3643ce036a7f
Arthur Langereis <puurtuur@me.com>
2016-11-23 18:15:37 +0100

    refactor(render): modularise texture-util

--- src/render/texture-util.ts
+++ src/render/texture-util.ts
@@ -3,69 +3,69 @@
 // (c) 2015-2016 by Arthur Langereis - @zenmumbler
 // https://github.com/stardazed/stardazed-tx
 
-namespace sd.render {
+import { assert } from "core/util";
+import { RenderContext } from "render/rendercontext";
+import { Texture, useMipMaps, makeTexDesc2DFromImageSource, makeTexDescCubeFromImageSources } from "render/texture";
 
-	export function loadSimpleTexture(rc: RenderContext, filePath: string, mipmaps = false): Promise<render.Texture> {
-		return new Promise<render.Texture>(function(resolve, reject) {
-			const image = new Image();
+export function loadSimpleTexture(rc: RenderContext, filePath: string, mipmaps = false): Promise<Texture> {
+	return new Promise<Texture>(function(resolve, reject) {
+		const image = new Image();
 
-			image.onload = function() {
-				const td = render.makeTexDesc2DFromImageSource(image, render.useMipMaps(mipmaps));
-				const texture = new render.Texture(rc, td);
-				resolve(texture);
-			};
-			image.onerror = function() {
-				assert(false, `Image ${filePath} does not exist`);
-				reject();
-			};
+		image.onload = function() {
+			const td = makeTexDesc2DFromImageSource(image, useMipMaps(mipmaps));
+			const texture = new Texture(rc, td);
+			resolve(texture);
+		};
+		image.onerror = function() {
+			assert(false, `Image ${filePath} does not exist`);
+			reject();
+		};
 
-			image.src = filePath;
-		});
-	}
-
-
-	export function loadCubeTexture(rc: RenderContext, filePaths: string[]): Promise<render.Texture> {
-		assert(filePaths.length == 6, "must have 6 paths for cube tex");
-
-		return new Promise<render.Texture>(function(resolve, reject) {
-			const images: HTMLImageElement[] = [];
-			let loaded = 0;
-
-			for (let k = 0; k < 6; ++k) {
-				(function(face: number) {
-					const image = new Image();
-
-					image.onload = function() {
-						images[face] = image;
-						++loaded;
-
-						if (loaded == 6) {
-							const td = render.makeTexDescCubeFromImageSources(images);
-							const texture = new render.Texture(rc, td);
-							resolve(texture);
-						}
-					};
-					image.onerror = function() {
-						assert(false, `Image ${filePaths[face]} does not exist`);
-						reject();
-					};
-
-					image.src = filePaths[face];
-				} (k));
-			}
-		});
-	}
+		image.src = filePath;
+	});
+}
 
 
-	export function makeCubeMapPaths(basePath: string, extension: string): string[] {
-		return [
-			basePath + `posx${extension}`,
-			basePath + `negx${extension}`,
-			basePath + `posy${extension}`,
-			basePath + `negy${extension}`,
-			basePath + `posz${extension}`,
-			basePath + `negz${extension}`
-		];
-	}
+export function loadCubeTexture(rc: RenderContext, filePaths: string[]): Promise<Texture> {
+	assert(filePaths.length == 6, "must have 6 paths for cube tex");
 
-} // ns sd.render
+	return new Promise<Texture>(function(resolve, reject) {
+		const images: HTMLImageElement[] = [];
+		let loaded = 0;
+
+		for (let k = 0; k < 6; ++k) {
+			(function(face: number) {
+				const image = new Image();
+
+				image.onload = function() {
+					images[face] = image;
+					++loaded;
+
+					if (loaded == 6) {
+						const td = makeTexDescCubeFromImageSources(images);
+						const texture = new Texture(rc, td);
+						resolve(texture);
+					}
+				};
+				image.onerror = function() {
+					assert(false, `Image ${filePaths[face]} does not exist`);
+					reject();
+				};
+
+				image.src = filePaths[face];
+			} (k));
+		}
+	});
+}
+
+
+export function makeCubeMapPaths(basePath: string, extension: string): string[] {
+	return [
+		basePath + `posx${extension}`,
+		basePath + `negx${extension}`,
+		basePath + `posy${extension}`,
+		basePath + `negy${extension}`,
+		basePath + `posz${extension}`,
+		basePath + `negz${extension}`
+	];
+}
