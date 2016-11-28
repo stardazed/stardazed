4927f7f4807748cd01444c1e1c5aa26c28708d6b
Arthur Langereis <puurtuur@me.com>
2016-11-28 12:08:33 +0100

    chore: test gulp task to combine all .d.ts files

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,43 @@
 // (c) 2016 by Arthur Langereis - @zenmumbler
 // https://github.com/stardazed/stardazed-tx
 
+const fs = require("fs");
+const path = require("path");
+
 const gulp = require("gulp");
 const concat = require("gulp-concat");
+const rename = require("gulp-rename");
+const wrap = require("gulp-wrap");
 const rollup = require("rollup-stream");
 const source = require("vinyl-source-stream");
-const path = require("path");
+const merge = require("merge-stream");
+
+const declarationsPath = "build/decls";
+
+function getFolders(dir) {
+	return fs.readdirSync(dir)
+		.filter(function(file) {
+			return fs.statSync(path.join(dir, file)).isDirectory();
+		});
+}
+
+gulp.task("decls", function() {
+	const folders = getFolders(declarationsPath);
+
+	const tasks = folders.map(function(folder) {
+		return gulp.src(path.join(declarationsPath, folder, "/*.d.ts"))
+			.pipe(concat(folder + ".d.ts"))
+			.pipe(wrap("declare module <%= folder %> {\n<%= contents %>}\n", { folder: folder }))
+			.pipe(gulp.dest(declarationsPath)) 
+	});
+
+   // group all modules into one declaration
+	const root = gulp.src(path.join(declarationsPath, "/*.d.ts"))
+		.pipe(concat("stardazed-txx.d.ts"))
+		.pipe(gulp.dest("dist"));
+
+	return merge(tasks, root);
+});
 
 // bundle main site code
 gulp.task("default", function() {
