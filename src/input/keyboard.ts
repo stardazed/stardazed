a9ea15d9855af131dc2c7175f58aa7a2b19ac915
Arthur Langereis <puurtuur@me.com>
2016-11-29 23:43:41 +0100

    refactor(input): input/input -> input/keyboard

--- src/input/keyboard.ts
+++ src/input/keyboard.ts
@@ -1,4 +1,4 @@
-// input - keyboard, mouse, controller
+// input/keyboard - keyboard input handling
 // Part of Stardazed TX
 // (c) 2015-2016 by Arthur Langereis - @zenmumbler
 // https://github.com/stardazed/stardazed-tx
