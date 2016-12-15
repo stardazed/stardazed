a595e60b52e01c0ffe1768af3ebb232e7d285176
Arthur Langereis <puurtuur@me.com>
2016-12-15 12:00:25 +0100

    remove old minify.sh

--- dist/minify.sh
+++
@@ -1,3 +0,0 @@
-sed 's/\/\*\!/\/** @license/' stardazed-tx.js > sdtx-temp.js
-closure-compiler --language_in ECMASCRIPT5_STRICT --language_out ECMASCRIPT5_STRICT --env BROWSER --js_output_file stardazed-tx-min.js ../ext/veclib.js ../ext/inflate.js sdtx-temp.js
-rm sdtx-temp.js
