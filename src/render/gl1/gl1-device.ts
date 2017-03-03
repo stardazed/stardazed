d037c80db6f1baad078bf5e132d6fb8ba0b9fe6c
Arthur Langereis <puurtuur@me.com>
2017-03-03 18:18:16 +0100

    wip: GL1RenderDevice

---
+++ src/render/gl1/gl1-device.ts
@@ -0,0 +1,14 @@
+// render/gl1/device - WebGL1 implementation of RenderDevice
+// Part of Stardazed
+// (c) 2015-2017 by Arthur Langereis - @zenmumbler
+// https://github.com/stardazed/stardazed
+
+namespace sd.render {
+
+	export class GL1RenderDevice implements RenderDevice {
+		dispatch(rrcb: RenderResourceCommandBuffer) {
+			
+		}
+	}
+
+} // ns sd.render.gl1
