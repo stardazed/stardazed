315da9cb1c2a62c34642038e89754187eaac61b1
Arthur Langereis <puurtuur@me.com>
2017-03-14 14:25:42 +0100

    test: vertexattr test scaffold

---
+++ test/tests/test-mesh-vertexattribute.ts
@@ -0,0 +1,11 @@
+// Part of Stardazed
+// (c) 2015-2017 by Arthur Langereis - @zenmumbler
+// https://github.com/stardazed/stardazed
+
+group("meshdata.vertexAttribute", () => {
+	const { VertexField } = sd.meshdata;
+
+	test("elementCount", () => {
+		const { vertexFieldElementCount } = sd.meshdata;
+	});
+});
