bd187dc36e1bc39ed1d55140489a04edc4e518c5
Arthur Langereis <puurtuur@me.com>
2017-03-20 12:11:06 +0100

    fix(mesh): edge cases for prim element calc

--- src/mesh/indexbuffer.ts
+++ src/mesh/indexbuffer.ts
@@ -1,4 +1,4 @@
-// meshdata/indexbuffer - indexbuffer and triangleview for indexbuffer
+// meshdata/indexbuffer - indexbuffer
 // Part of Stardazed
 // (c) 2015-2017 by Arthur Langereis - @zenmumbler
 // https://github.com/stardazed/stardazed
@@ -81,17 +81,19 @@ namespace sd.meshdata {
 
 
 	export function elementCountForPrimitiveCount(primitiveType: PrimitiveType, primitiveCount: number) {
+		assert(primitiveCount >= 0);
+
 		switch (primitiveType) {
 			case PrimitiveType.Point:
 				return primitiveCount;
 			case PrimitiveType.Line:
 				return primitiveCount * 2;
 			case PrimitiveType.LineStrip:
-				return primitiveCount + 1;
+				return primitiveCount > 0 ? primitiveCount + 1 : 0;
 			case PrimitiveType.Triangle:
 				return primitiveCount * 3;
 			case PrimitiveType.TriangleStrip:
-				return primitiveCount + 2;
+				return primitiveCount > 0 ? primitiveCount + 2 : 0;
 
 			default:
 				assert(false, "Unknown primitive type");
@@ -101,17 +103,19 @@ namespace sd.meshdata {
 
 
 	export function primitiveCountForElementCount(primitiveType: PrimitiveType, elementCount: number) {
+		assert(elementCount >= 0);
+
 		switch (primitiveType) {
 			case PrimitiveType.Point:
 				return elementCount;
 			case PrimitiveType.Line:
 				return (elementCount / 2) | 0;
 			case PrimitiveType.LineStrip:
-				return elementCount - 1;
+				return elementCount > 0 ? elementCount - 1 : 0;
 			case PrimitiveType.Triangle:
 				return (elementCount / 3) | 0;
 			case PrimitiveType.TriangleStrip:
-				return elementCount - 2;
+				return elementCount > 0 ? elementCount - 2 : 0;
 
 			default:
 				assert(false, "Unknown primitive type");
