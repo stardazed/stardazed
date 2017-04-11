165946b637ec1aaca81950963252069fa8ee5849
Arthur Langereis <puurtuur@me.com>
2017-04-11 21:56:43 +0200

    feat(entity): the return of Light (sans shadows)

--- src/entity/light.ts
+++ src/entity/light.ts
@@ -1,22 +1,56 @@
-// world/light - Light component
-// Part of Stardazed TX
+// entity/light - Light component
+// Part of Stardazed
 // (c) 2015-2017 by Arthur Langereis - @zenmumbler
-// https://github.com/stardazed/stardazed-tx
+// https://github.com/stardazed/stardazed
 
-namespace sd.world {
+namespace sd.entity {
 
-	export type LightInstance = Instance<LightManager>;
-	export type LightRange = InstanceRange<LightManager>;
-	export type LightSet = InstanceSet<LightManager>;
-	export type LightIterator = InstanceIterator<LightManager>;
-	export type LightArrayView = InstanceArrayView<LightManager>;
-
-	export interface ShadowView {
-		light: LightInstance;
-		lightProjection: ProjectionSetup;
-		shadowFBO: render.FrameBuffer;
-		filteredTexture?: render.Texture;
+	export const enum LightType {
+		None,
+		Directional,
+		Point,
+		Spot
 	}
+
+	// export const enum ShadowType {
+	// 	None,
+	// 	Hard,
+	// 	Soft
+	// }
+
+	// export const enum ShadowQuality {
+	// 	Auto
+	// }
+
+	export interface Light {
+		type: LightType;
+
+		colour: Float3;
+		intensity: number;
+
+		range?: number;  // m   (point/spot only)
+		cutoff?: number; // rad (spot only)
+
+		// shadowType?: ShadowType;
+		// shadowQuality?: ShadowQuality;
+		// shadowStrength?: number;  // 0..1
+		// shadowBias?: number;      // 0.001..0.1
+	}
+
+	// ----
+
+	export type LightInstance = Instance<LightComponent>;
+	export type LightRange = InstanceRange<LightComponent>;
+	export type LightSet = InstanceSet<LightComponent>;
+	export type LightIterator = InstanceIterator<LightComponent>;
+	export type LightArrayView = InstanceArrayView<LightComponent>;
+
+	// export interface ShadowView {
+	// 	light: LightInstance;
+	// 	lightProjection: math.ProjectionSetup;
+	// 	shadowFBO: render.FrameBuffer;
+	// 	filteredTexture?: render.Texture;
+	// }
 
 
 	// This setup allows for a viewport > 5K (5120x2880)
@@ -39,29 +73,32 @@ namespace sd.world {
 	}
 
 
-	export class LightManager implements Component<LightManager> {
+	export class LightComponent implements Component<LightComponent> {
 		private instanceData_: container.FixedMultiArray;
 		private entityBase_: EntityArrayView;
 		private transformBase_: TransformArrayView;
 		private enabledBase_: Uint8Array;
-		private shadowTypeBase_: ConstEnumArrayView<asset.ShadowType>;
-		private shadowQualityBase_: ConstEnumArrayView<asset.ShadowQuality>;
+		// private shadowTypeBase_: ConstEnumArrayView<ShadowType>;
+		// private shadowQualityBase_: ConstEnumArrayView<ShadowQuality>;
 
 		private lightData_: container.FixedMultiArray;
 		private globalLightData_: Float32Array;
 		private tileLightIndexes_: Float32Array;
 		private lightGrid_: Float32Array;
 		private lutTexture_: render.Texture;
+		private lutTilesWide_ = 0;
+		private lutTilesHigh_ = 0;
+		private lutParam_ = new Float32Array(2);
 		private count_: number;
 
 		private gridRowSpans_: LightGridSpan[][];
 
 		private nullVec3_ = new Float32Array(3); // used to convert directions to rotations
 
-		private shadowFBO_: render.FrameBuffer | null = null;
+		// private shadowFBO_: render.FrameBuffer | null = null;
 
 
-		constructor(private rc: render.RenderContext, private transformMgr_: Transform) {
+		constructor(private transformMgr_: TransformComponent, resources: render.RenderResourceCommandBuffer) {
 			this.count_ = 0;
 
 			// instance info
@@ -76,8 +113,8 @@ namespace sd.world {
 			this.entityBase_ = this.instanceData_.indexedFieldView(0);
 			this.transformBase_ = this.instanceData_.indexedFieldView(1);
 			this.enabledBase_ = this.instanceData_.indexedFieldView(2);
-			this.shadowTypeBase_ = this.instanceData_.indexedFieldView(3);
-			this.shadowQualityBase_ = this.instanceData_.indexedFieldView(4);
+			// this.shadowTypeBase_ = this.instanceData_.indexedFieldView(3);
+			// this.shadowQualityBase_ = this.instanceData_.indexedFieldView(4);
 
 			// grid creation
 			this.gridRowSpans_ = [];
@@ -93,21 +130,21 @@ namespace sd.world {
 			this.tileLightIndexes_ = this.lightData_.indexedFieldView(1);
 			this.lightGrid_ = this.lightData_.indexedFieldView(2);
 
-			const lutDesc = render.makeTexDesc2DFloatLUT(new Float32Array(this.lightData_.data), LUT_WIDTH, LUT_HEIGHT);
-			this.lutTexture_ = new render.Texture(rc, lutDesc);
+			this.lutTexture_ = render.makeTex2DFloatLUT(new Float32Array(this.lightData_.data), LUT_WIDTH, LUT_HEIGHT);
+			resources.alloc(this.lutTexture_);
 
 			vec3.set(this.nullVec3_, 1, 0, 0);
 		}
 
 
-		create(entity: Entity, desc: asset.Light): LightInstance {
+		create(entity: Entity, desc: Light): LightInstance {
 			// do we have any room left?
 			assert(this.count_ < MAX_LIGHTS, "light storage exhausted");
 			this.count_ += 1;
 			const instance = this.count_;
 
 			// validate parameters
-			assert(desc.type != asset.LightType.None);
+			assert(desc.type != LightType.None);
 
 			// linking
 			this.entityBase_[instance] = entity;
@@ -117,8 +154,8 @@ namespace sd.world {
 			this.enabledBase_[instance] = 1;
 
 			// non-shader shadow data (all optional)
-			this.shadowTypeBase_[instance] = desc.shadowType || asset.ShadowType.None;
-			this.shadowQualityBase_[instance] = desc.shadowQuality || asset.ShadowQuality.Auto;
+			// this.shadowTypeBase_[instance] = desc.shadowType || ShadowType.None;
+			// this.shadowQualityBase_[instance] = desc.shadowQuality || ShadowQuality.Auto;
 
 			// write global light data
 			const gldV4Index = instance * 5;
@@ -132,7 +169,7 @@ namespace sd.world {
 			// pixel3: direction[3], cutoff
 			container.setIndexedVec4(this.globalLightData_, gldV4Index + 3, [0, 0, 0, Math.cos(desc.cutoff || 0)]);
 			// pixel4: shadowStrength, shadowBias, 0, 0
-			container.setIndexedVec4(this.globalLightData_, gldV4Index + 4, [desc.shadowStrength || 1.0, desc.shadowBias || 0.002, 0, 0]);
+			// container.setIndexedVec4(this.globalLightData_, gldV4Index + 4, [desc.shadowStrength || 1.0, desc.shadowBias || 0.002, 0, 0]);
 
 			return instance;
 		}
@@ -156,7 +193,7 @@ namespace sd.world {
 		}
 
 		all(): LightRange {
-			return new InstanceLinearRange<LightManager>(1, this.count);
+			return new InstanceLinearRange<LightComponent>(1, this.count);
 		}
 
 		// [AL] temporary
@@ -220,10 +257,10 @@ namespace sd.world {
 		}
 
 
-		private updateLightGrid(range: LightRange, projection: ProjectionSetup, viewport: render.Viewport) {
-			const vpHeight = this.rc.gl.drawingBufferHeight;
-			const tilesWide = this.lutTilesWide;
-			const tilesHigh = this.lutTilesHigh;
+		private updateLightGrid(range: LightRange, projection: math.ProjectionSetup, viewport: render.Viewport) {
+			const vpHeight = this.lutParam_[1];
+			const tilesWide = this.lutTilesWide_;
+			const tilesHigh = this.lutTilesHigh_;
 
 			// reset grid row light index table
 			for (let row = 0; row < tilesHigh; ++row) {
@@ -244,7 +281,7 @@ namespace sd.world {
 				const lix = iter.current as number;
 				const lightType = this.type(lix);
 
-				if (lightType === asset.LightType.Point) {
+				if (lightType === LightType.Point) {
 					// calculate screen space bounds based on a simple point and radius cube
 					const lcpos = this.positionCameraSpace(lix);
 					const radius = this.range(lix);
@@ -310,7 +347,13 @@ namespace sd.world {
 		}
 
 
-		prepareLightsForRender(range: LightRange, proj: ProjectionSetup, viewport: render.Viewport) {
+		prepareLightsForRender(range: LightRange, proj: math.ProjectionSetup, targetDim: image.PixelDimensions, viewport: render.Viewport) {
+			// update lut dimensions
+			this.lutTilesWide_ = Math.ceil(targetDim.width / TILE_DIMENSION);
+			this.lutTilesHigh_ = Math.ceil(targetDim.height / TILE_DIMENSION);
+			this.lutParam_[0] = this.lutTilesWide_;
+			this.lutParam_[1] = targetDim.height;
+
 			const viewNormalMatrix = mat3.normalFromMat4([], proj.viewMatrix);
 
 			// calculate world and camera space vectors of each light
@@ -326,7 +369,7 @@ namespace sd.world {
 				const type = this.type(lix);
 				const transform = this.transformBase_[lix];
 
-				if (type != asset.LightType.Directional) {
+				if (type != LightType.Directional) {
 					const lightPos_world = this.transformMgr_.worldPosition(transform); // tslint:disable-line:variable-name
 					const lightPos_cam = vec3.transformMat4([], lightPos_world, proj.viewMatrix); // tslint:disable-line:variable-name
 
@@ -340,7 +383,7 @@ namespace sd.world {
 					this.globalLightData_[posWorldOffset + 1] = lightPos_world[1];
 					this.globalLightData_[posWorldOffset + 2] = lightPos_world[2];
 				}
-				if (type != asset.LightType.Point) {
+				if (type != LightType.Point) {
 					const rotMat = mat3.normalFromMat4([], this.transformMgr_.worldMatrix(transform));
 					const lightDir_world = vec3.transformMat3([], this.nullVec3_, rotMat); // tslint:disable-line:variable-name
 					const lightDir_cam = vec3.transformMat3([], lightDir_world, viewNormalMatrix); // tslint:disable-line:variable-name
@@ -359,11 +402,13 @@ namespace sd.world {
 			const gllRowsUsed = Math.ceil(highestLightIndex / LUT_WIDTH);
 			const indexRowsUsed = Math.ceil(indexPixelsUsed / LUT_WIDTH);
 
-			this.lutTexture_.bind();
-			this.rc.gl.texSubImage2D(this.lutTexture_.target, 0, 0, 0, LUT_WIDTH, gllRowsUsed, this.rc.gl.RGBA, this.rc.gl.FLOAT, this.globalLightData_);
-			this.rc.gl.texSubImage2D(this.lutTexture_.target, 0, 0, LUT_LIGHTDATA_ROWS, LUT_WIDTH, indexRowsUsed, this.rc.gl.RGBA, this.rc.gl.FLOAT, this.tileLightIndexes_);
-			this.rc.gl.texSubImage2D(this.lutTexture_.target, 0, 0, LUT_LIGHTDATA_ROWS + LUT_INDEXLIST_ROWS, LUT_WIDTH, gridRowsUsed, this.rc.gl.RGBA, this.rc.gl.FLOAT, this.lightGrid_);
-			this.lutTexture_.unbind();
+			// resource updates
+			const cmd = new render.RenderCommandBuffer();
+			// TODO: should use slice to only pass subarray of data actually being sent?
+			cmd.textureWrite(this.lutTexture_, image.makePixelCoordinate(0, 0), image.makePixelDimensions(LUT_WIDTH, gllRowsUsed), this.globalLightData_);
+			cmd.textureWrite(this.lutTexture_, image.makePixelCoordinate(0, LUT_LIGHTDATA_ROWS), image.makePixelDimensions(LUT_WIDTH, indexRowsUsed), this.tileLightIndexes_);
+			cmd.textureWrite(this.lutTexture_, image.makePixelCoordinate(0, LUT_LIGHTDATA_ROWS + LUT_INDEXLIST_ROWS), image.makePixelDimensions(LUT_WIDTH, gridRowsUsed), this.lightGrid_);
+			return cmd;
 		}
 
 
@@ -371,19 +416,9 @@ namespace sd.world {
 			return this.lutTexture_;
 		}
 
-		private get lutTilesWide() {
-			return Math.ceil(this.rc.gl.drawingBufferWidth / TILE_DIMENSION);
-		}
-
-		private get lutTilesHigh() {
-			return Math.ceil(this.rc.gl.drawingBufferHeight / TILE_DIMENSION);
-		}
-
 		get lutParam() {
 			// gridWidth, vpHeight
-			return new Float32Array([
-				this.lutTilesWide, this.rc.gl.drawingBufferHeight
-			]);
+			return this.lutParam_;
 		}
 
 
@@ -445,63 +480,61 @@ namespace sd.world {
 			return this.globalLightData_.slice(posCamOffset, posCamOffset + 3);
 		}
 
-		projectionSetupForLight(inst: LightInstance, viewportWidth: number, viewportHeight: number, nearZ: number): ProjectionSetup | null {
-			const transform = this.transformBase_[<number>inst];
-			const worldPos = this.transformMgr_.worldPosition(transform);
-			const worldDirection = this.direction(inst);
-			const worldTarget = vec3.add([], worldPos, worldDirection);
+		// projectionSetupForLight(inst: LightInstance, viewportWidth: number, viewportHeight: number, nearZ: number): math.ProjectionSetup | null {
+		// 	const transform = this.transformBase_[<number>inst];
+		// 	const worldPos = this.transformMgr_.worldPosition(transform);
+		// 	const worldDirection = this.direction(inst);
+		// 	const worldTarget = vec3.add([], worldPos, worldDirection);
 
-			let viewMatrix: Float4x4;
-			let projectionMatrix: Float4x4;
+		// 	let viewMatrix: Float4x4;
+		// 	let projectionMatrix: Float4x4;
 
-			const type = this.type(inst);
-			if (type == asset.LightType.Spot) {
-				const farZ = this.range(inst);
-				const fov = this.cutoff(inst) * 2; // cutoff is half-angle
-				viewMatrix = mat4.lookAt(new Float32Array(16), worldPos, worldTarget, [0, 1, 0]); // FIXME: this can likely be done cheaper
-				projectionMatrix = mat4.perspective(new Float32Array(16), fov, viewportWidth / viewportHeight, nearZ, farZ);
-				// TODO: cache this matrix?
-			}
-			else if (type == asset.LightType.Directional) {
-				viewMatrix = mat4.lookAt(new Float32Array(16), [0, 0, 0], worldDirection, [0, 1, 0]); // FIXME: this can likely be done cheaper
-				projectionMatrix = mat4.ortho(new Float32Array(16), -40, 40, -40, 40, -40, 40);
-			}
-			else {
-				return null;
-			}
+		// 	const type = this.type(inst);
+		// 	if (type == LightType.Spot) {
+		// 		const farZ = this.range(inst);
+		// 		const fov = this.cutoff(inst) * 2; // cutoff is half-angle
+		// 		viewMatrix = mat4.lookAt(new Float32Array(16), worldPos, worldTarget, [0, 1, 0]); // FIXME: this can likely be done cheaper
+		// 		projectionMatrix = mat4.perspective(new Float32Array(16), fov, viewportWidth / viewportHeight, nearZ, farZ);
+		// 		// TODO: cache this matrix?
+		// 	}
+		// 	else if (type == LightType.Directional) {
+		// 		viewMatrix = mat4.lookAt(new Float32Array(16), [0, 0, 0], worldDirection, [0, 1, 0]); // FIXME: this can likely be done cheaper
+		// 		projectionMatrix = mat4.ortho(new Float32Array(16), -40, 40, -40, 40, -40, 40);
+		// 	}
+		// 	else {
+		// 		return null;
+		// 	}
 
-			return {
-				projectionMatrix: projectionMatrix,
-				viewMatrix: viewMatrix
-			};
-		}
-
-
-		private shadowFrameBufferOfQuality(rc: render.RenderContext, _quality: asset.ShadowQuality) {
-			// TODO: each shadow quality level of shadows will have a dedicated, reusable FBO
-			if (! this.shadowFBO_) {
-				this.shadowFBO_ = render.makeShadowMapFrameBuffer(rc, 1024);
-			}
-
-			return this.shadowFBO_;
-		}
+		// 	return {
+		// 		projectionMatrix: projectionMatrix,
+		// 		viewMatrix: viewMatrix
+		// 	};
+		// }
 
 
-		shadowViewForLight(rc: render.RenderContext, inst: LightInstance, nearZ: number): ShadowView | null {
-			const fbo = this.shadowFrameBufferOfQuality(rc, this.shadowQualityBase_[<number>inst]);
-			const projection = this.projectionSetupForLight(inst, fbo.width, fbo.height, nearZ);
+		// private shadowFrameBufferOfQuality(rc: render.RenderDevice, _quality: ShadowQuality) {
+		// 	// TODO: each shadow quality level of shadows will have a dedicated, reusable FBO
+		// 	if (! this.shadowFBO_) {
+		// 		this.shadowFBO_ = render.makeShadowMapFrameBuffer(rc, 1024);
+		// 	}
+		// 	return this.shadowFBO_;
+		// }
 
-			return projection && {
-				light: inst,
-				lightProjection: projection,
-				shadowFBO: fbo
-			};
-		}
+
+		// shadowViewForLight(rc: render.RenderDevice, inst: LightInstance, nearZ: number): ShadowView | null {
+		// 	const fbo = this.shadowFrameBufferOfQuality(rc, this.shadowQualityBase_[<number>inst]);
+		// 	const projection = this.projectionSetupForLight(inst, fbo.width, fbo.height, nearZ);
+		// 	return projection && {
+		// 		light: inst,
+		// 		lightProjection: projection,
+		// 		shadowFBO: fbo
+		// 	};
+		// }
 
 
 		// -- internal properties
 
-		type(inst: LightInstance): asset.LightType {
+		type(inst: LightInstance): LightType {
 			const offset = ((inst as number) * 20) + 3;
 			return this.globalLightData_[offset];
 		}
@@ -556,44 +589,44 @@ namespace sd.world {
 
 		// -- shadow data
 
-		shadowType(inst: LightInstance): asset.ShadowType {
-			return this.shadowTypeBase_[<number>inst];
-		}
+		// shadowType(inst: LightInstance): ShadowType {
+		// 	return this.shadowTypeBase_[<number>inst];
+		// }
 
-		setShadowType(inst: LightInstance, newType: asset.ShadowType) {
-			this.shadowTypeBase_[<number>inst] = newType;
-		}
-
-
-		shadowQuality(inst: LightInstance): asset.ShadowQuality {
-			return this.shadowQualityBase_[<number>inst];
-		}
-
-		setShadowQuality(inst: LightInstance, newQuality: asset.ShadowQuality) {
-			this.shadowQualityBase_[<number>inst] = newQuality;
-		}
+		// setShadowType(inst: LightInstance, newType: ShadowType) {
+		// 	this.shadowTypeBase_[<number>inst] = newType;
+		// }
 
 
-		shadowStrength(inst: LightInstance): number {
-			const offset = ((inst as number) * 20) + 16;
-			return this.globalLightData_[offset];
-		}
+		// shadowQuality(inst: LightInstance): ShadowQuality {
+		// 	return this.shadowQualityBase_[<number>inst];
+		// }
 
-		setShadowStrength(inst: LightInstance, newStrength: number) {
-			const offset = ((inst as number) * 20) + 16;
-			this.globalLightData_[offset] = newStrength;
-		}
+		// setShadowQuality(inst: LightInstance, newQuality: ShadowQuality) {
+		// 	this.shadowQualityBase_[<number>inst] = newQuality;
+		// }
 
 
-		shadowBias(inst: LightInstance): number {
-			const offset = ((inst as number) * 20) + 17;
-			return this.globalLightData_[offset];
-		}
+		// shadowStrength(inst: LightInstance): number {
+		// 	const offset = ((inst as number) * 20) + 16;
+		// 	return this.globalLightData_[offset];
+		// }
 
-		setShadowBias(inst: LightInstance, newBias: number) {
-			const offset = ((inst as number) * 20) + 17;
-			this.globalLightData_[offset] = newBias;
-		}
+		// setShadowStrength(inst: LightInstance, newStrength: number) {
+		// 	const offset = ((inst as number) * 20) + 16;
+		// 	this.globalLightData_[offset] = newStrength;
+		// }
+
+
+		// shadowBias(inst: LightInstance): number {
+		// 	const offset = ((inst as number) * 20) + 17;
+		// 	return this.globalLightData_[offset];
+		// }
+
+		// setShadowBias(inst: LightInstance, newBias: number) {
+		// 	const offset = ((inst as number) * 20) + 17;
+		// 	this.globalLightData_[offset] = newBias;
+		// }
 	}
 
 } // ns sd.world
