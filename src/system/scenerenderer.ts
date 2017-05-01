c0fdefc8b607a7f51cf1ba0ea349f179511031fe
Arthur Langereis <puurtuur@me.com>
2017-05-01 09:58:34 +0200

    wip(scene): start with scene build

---
+++ src/system/scenerenderer.ts
@@ -0,0 +1,12 @@
+// system/scenerenderer - the default scene rendering system
+// Part of Stardazed
+// (c) 2015-2017 by Arthur Langereis - @zenmumbler
+// https://github.com/stardazed/stardazed
+
+namespace sd {
+
+	export interface Scene {
+		
+	}
+
+} // ns sd
