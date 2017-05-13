644bd1632f915e767f8bc2d80a0cb5fbe08531f2
Arthur Langereis <puurtuur@me.com>
2017-05-14 00:07:09 +0200

    ref(render): move ReusableResourceArray to tools

--- src/render/gl1/gl1-device.ts
+++ src/render/gl1/gl1-device.ts
@@ -19,63 +19,6 @@ namespace sd.render.gl1 {
 				return rd.gl.NONE;
 		}
 	}
-
-	// ----
-
-	function encodeResourceHandle(type: ResourceType, index: number) {
-		return (type << 24) | index;
-	}
-
-	function decodeResourceHandle(handle: number) {
-		const index = handle & 0x00FFFFFF;
-		const type = (handle >> 24) as ResourceType;
-		return { type, index };
-	}
-
-	export class ReusableResourceArray<C extends RenderResourceBase, R> {
-		readonly resources: (R | undefined)[] = [];
-		private freedIndexes_: number[] = [];
-		private nextIndex_ = 1;
-
-		constructor(public readonly resourceType: ResourceType) {}
-
-		insert(clientResource: C, resource: R) {
-			let index: number;
-			if (this.freedIndexes_.length) {
-				index = this.freedIndexes_.pop()!;
-			}
-			else {
-				index = this.nextIndex_;
-				this.nextIndex_ += 1;
-			}
-
-			this.resources[index] = resource;
-			clientResource.renderResourceHandle = encodeResourceHandle(this.resourceType, index);
-			return index;
-		}
-
-		find(clientResource: C): R | undefined {
-			const { index } = decodeResourceHandle(clientResource.renderResourceHandle);
-			return this.resources[index];
-		}
-
-		findMultiple(clientResources: C[]): (R | undefined)[] {
-			return clientResources.map(cr => {
-				const { index } = decodeResourceHandle(cr.renderResourceHandle);
-				return this.resources[index];
-			});
-		}
-
-		remove(clientResource: C) {
-			const { index } = decodeResourceHandle(clientResource.renderResourceHandle!);
-			clientResource.renderResourceHandle = 0;
-
-			this.resources[index] = undefined;
-			this.freedIndexes_.push(index);
-			return index;
-		}
-	}
-
 	// ----
 
 	export class GL1RenderDevice implements RenderDevice {
---
+++ src/render/gl1/gl1-tools.ts
@@ -0,0 +1,82 @@
+// render/gl1/tools - WebGL1 device utility types
+// Part of Stardazed
+// (c) 2015-2017 by Arthur Langereis - @zenmumbler
+// https://github.com/stardazed/stardazed
+
+namespace sd.render.gl1 {
+
+	export function encodeResourceHandle(type: ResourceType, index: number) {
+		return (type << 24) | index;
+	}
+
+	export function decodeResourceHandle(handle: number) {
+		const index = handle & 0x00FFFFFF;
+		const type = (handle >> 24) as ResourceType;
+		return { type, index };
+	}
+
+	export class ReusableResourceArray<C extends RenderResourceBase, R> {
+		readonly resources: (R | undefined)[] = [];
+		private freedIndexes_: number[] = [];
+		private nextIndex_ = 1;
+
+		constructor(public readonly resourceType: ResourceType) {}
+
+		insert(clientResource: C, resource: R) {
+			let index: number;
+			if (this.freedIndexes_.length) {
+				index = this.freedIndexes_.pop()!;
+			}
+			else {
+				index = this.nextIndex_;
+				this.nextIndex_ += 1;
+			}
+
+			this.resources[index] = resource;
+			clientResource.renderResourceHandle = encodeResourceHandle(this.resourceType, index);
+			return index;
+		}
+
+		find(clientResource: C): R | undefined {
+			const { index } = decodeResourceHandle(clientResource.renderResourceHandle);
+			return this.resources[index];
+		}
+
+		findMultiple(clientResources: C[]): (R | undefined)[] {
+			return clientResources.map(cr => {
+				const { index } = decodeResourceHandle(cr.renderResourceHandle);
+				return this.resources[index];
+			});
+		}
+
+		remove(clientResource: C) {
+			const { index } = decodeResourceHandle(clientResource.renderResourceHandle!);
+			clientResource.renderResourceHandle = 0;
+
+			this.resources[index] = undefined;
+			this.freedIndexes_.push(index);
+			return index;
+		}
+	}
+
+
+	// ----
+
+
+	export class GL1ResourceCommandBuffer implements RenderResourceCommandBuffer {
+		private readonly allocList_: RenderResource[] = [];
+		private readonly freeList_: RenderResource[] = [];
+
+		alloc(resource: RenderResource) {
+			this.allocList_.push(resource);
+		}
+
+		free(resource: RenderResource) {
+			this.freeList_.push(resource);
+		}
+
+		get allocList() { return this.allocList_; }
+		get freeList() { return this.freeList_; }
+	}
+
+} // ns sd.render.gl1
