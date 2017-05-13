8b8ef8e126e356e7f7cc023ba8180e4582b1a759
Arthur Langereis <puurtuur@me.com>
2017-05-14 00:19:25 +0200

    feat(render): RenderPass basic implementation

--- src/render/renderpass.ts
+++ src/render/renderpass.ts
@@ -10,7 +10,6 @@ namespace sd.render {
 		CounterClockwise
 	}
 
-
 	export const enum ClearMask {
 		None         = 0,
 		Colour       = 1,
@@ -20,7 +19,6 @@ namespace sd.render {
 		DepthStencil = Depth | Stencil,
 		All          = Colour | Depth | Stencil
 	}
-
 
 	export interface ScissorRect {
 		originX: number;
@@ -37,7 +35,6 @@ namespace sd.render {
 			height: 32768
 		};
 	}
-
 
 	export interface Viewport {
 		originX: number;
@@ -59,7 +56,6 @@ namespace sd.render {
 		};
 	}
 
-
 	export interface RenderJob {
 		pipeline: Pipeline;
 		mesh: meshdata.MeshData;
@@ -71,20 +67,146 @@ namespace sd.render {
 		constants: any[];
 	}
 
-	export interface RenderPass {
-		readonly frameBuffer: FrameBuffer | null;
-		readonly clearMask: ClearMask;
 
-		// state management
-		setScissor(rect: ScissorRect): void;
-		setViewport(viewport: Viewport): void;
-		setFrontFace(winding: FrontFaceWinding): void;
+	// ---- initial implementation for RenderCommand: use discriminated unions
 
-		// resource updates
-		textureWrite(texture: Texture, offset: image.PixelCoordinate, dim: image.PixelDimensions, data: ReadonlyTypedArray): void;
+	export const enum RenderCommandType {
+		None,
+		StartFrame,
+		Scissor,
+		Viewport,
+		FrontFace,
+		TextureWrite,
+		RenderJob
+	}
 
-		// drawing
-		render(job: RenderJob, normalizedDepth: number): void;
+	export interface StartFrameCommand {
+		type: RenderCommandType.StartFrame;
+		sortKey: number;
+		frameBufferHandle: number;
+		clearMask: ClearMask;
+	}
+
+	export interface ScissorCommand {
+		type: RenderCommandType.Scissor;
+		sortKey: number;
+		originX: number;
+		originY: number;
+		width: number;
+		height: number;
+	}
+
+	export interface ViewportCommand {
+		type: RenderCommandType.Viewport;
+		sortKey: number;
+		originX: number;
+		originY: number;
+		width: number;
+		height: number;
+		nearZ: number;
+		farZ: number;
+	}
+
+	export interface FrontFaceCommand {
+		type: RenderCommandType.FrontFace;
+		sortKey: number;
+		frontFace: FrontFaceWinding;
+	}
+
+	export interface TextureWriteCommand {
+		type: RenderCommandType.TextureWrite;
+		sortKey: number;
+		textureHandle: number;
+		x: number;
+		y: number;
+		width: number;
+		height: number;
+		pixels: ReadonlyTypedArray;
+	}
+
+	export interface RenderJobCommand {
+		type: RenderCommandType.RenderJob;
+		sortKey: number;
+
+		pipeline: Pipeline;
+		meshHandle: number;
+		primitiveType: meshdata.PrimitiveType;
+		fromElement: number;
+		elementCount: number;
+
+		// shader properties
+		textureHandles: number[];
+		samplerHandles: number[];
+		constants: any[];
+	}
+
+	export type RenderCommand = StartFrameCommand | ScissorCommand | ViewportCommand | FrontFaceCommand | TextureWriteCommand | RenderJobCommand;
+
+
+	export class RenderPass {
+		readonly commands: RenderCommand[] = [];
+
+		setFrameBuffer(fb: FrameBuffer | null, clearMask: ClearMask) {
+			this.commands.push({
+				type: RenderCommandType.StartFrame,
+				sortKey: 0,
+				frameBufferHandle: fb ? fb.renderResourceHandle : 0,
+				clearMask
+			});
+		}
+
+		setScissor(rect: ScissorRect) {
+			this.commands.push({
+				type: RenderCommandType.Scissor,
+				sortKey: 0,
+				...rect
+			});
+		}
+
+		setViewport(viewport: Viewport) {
+			this.commands.push({
+				type: RenderCommandType.Viewport,
+				sortKey: 0,
+				...viewport
+			});
+		}
+
+		setFrontFace(winding: FrontFaceWinding) {
+			this.commands.push({
+				type: RenderCommandType.FrontFace,
+				sortKey: 0,
+				frontFace: winding
+			});
+
+		}
+
+		textureWrite(texture: Texture, offset: image.PixelCoordinate, dim: image.PixelDimensions, pixels: ReadonlyTypedArray) {
+			this.commands.push({
+				type: RenderCommandType.TextureWrite,
+				sortKey: 0,
+				textureHandle: texture.renderResourceHandle,
+				x: offset.x,
+				y: offset.y,
+				width: dim.width,
+				height: dim.height,
+				pixels
+			});
+		}
+
+		render(job: RenderJob, _normalizedDepth: number) {
+			this.commands.push({
+				type: RenderCommandType.RenderJob,
+				sortKey: 0,
+				pipeline: job.pipeline,
+				meshHandle: job.mesh.renderResourceHandle,
+				primitiveType: job.primGroup.type,
+				fromElement: job.primGroup.fromElement,
+				elementCount: job.primGroup.elementCount,
+				textureHandles: job.textures.map(t => t.renderResourceHandle),
+				samplerHandles: job.samplers.map(s => s.renderResourceHandle),
+				constants: job.constants
+			});
+		}
 	}
 
 } // ns sd.render
