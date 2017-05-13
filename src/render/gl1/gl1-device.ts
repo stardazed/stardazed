a130ff259909ce94ff2cddfa8edbca297062f636
Arthur Langereis <puurtuur@me.com>
2017-05-14 00:30:06 +0200

    ref(render): remove wip render stuff

--- src/render/gl1/gl1-device.ts
+++ src/render/gl1/gl1-device.ts
@@ -133,47 +133,6 @@ namespace sd.render.gl1 {
 		}
 
 
-		generateStandardShader(options: StandardShaderOptions) {
-			return makeStandardShader(options);
-		}
-
-		// TEMPORARY
-		render(proj: Float32Array, view: Float32Array, mesh: meshdata.MeshData, shader: Shader) {
-			const gl = this.gl;
-
-			const meshVAOMap = this.meshes_.find(mesh)!;
-			let vao = meshVAOMap.get(shader);
-			if (! vao) {
-				vao = gl1CreateVAOForAttrBinding(this, mesh, shader.vertexFunction.in);
-				meshVAOMap.set(shader, vao);
-			}
-			this.extVAO.bindVertexArrayOES(vao);
-
-			const prog = this.shaders_.find(shader)!;
-			gl.useProgram(prog);
-			gl.enable(gl.DEPTH_TEST);
-			gl.depthFunc(gl.LEQUAL);
-			
-			const model = mat4.create();
-			const mv = view;
-			const mvp = mat4.multiply(new Float32Array(16), proj, view);
-			const norm = mat3.normalFromMat4(mat3.create(), mvp);
-
-			gl.uniformMatrix4fv(gl.getUniformLocation(prog, shader.vertexFunction.constantBlocks![0].constants[0].name)!, false, model);
-			gl.uniformMatrix4fv(gl.getUniformLocation(prog, shader.vertexFunction.constantBlocks![0].constants[1].name)!, false, mv);
-			gl.uniformMatrix4fv(gl.getUniformLocation(prog, shader.vertexFunction.constantBlocks![0].constants[2].name)!, false, mvp);
-			gl.uniformMatrix3fv(gl.getUniformLocation(prog, shader.vertexFunction.constantBlocks![0].constants[3].name)!, false, norm);
-
-			gl.uniform4fv(gl.getUniformLocation(prog, shader.fragmentFunction.constantBlocks![0].constants[1].name)!, vec4.fromValues(.6, .1, .1, 1));
-			gl.uniform4fv(gl.getUniformLocation(prog, shader.fragmentFunction.constantBlocks![0].constants[3].name)!, vec4.fromValues(.7, 0, 0, 0));
-
-			const sub = mesh.subMeshes[0];
-			gl.drawElements(gl.TRIANGLES, sub.elementCount, glTypeForIndexElementType(this, mesh.indexBuffer!.indexElementType), 0);
-
-			this.extVAO.bindVertexArrayOES(null);
-		}
-		// TEMPORARY
-
 
 		dispatch(_rcb: RenderCommandBuffer | RenderCommandBuffer[]) {
 		}
