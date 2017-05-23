398f7383be4259af3096e8732358eb99295b4509
Arthur Langereis <puurtuur@me.com>
2017-05-23 13:16:11 +0200

    ref(gl1): replace conversion func to Map lookup

--- src/render/gl1/gl1-mesh.ts
+++ src/render/gl1/gl1-mesh.ts
@@ -19,63 +19,50 @@ namespace sd.render.gl1 {
 	}
 	*/
 
-	function gl1TypeForVertexField(rd: GL1RenderDevice, vf: meshdata.VertexField) {
-		switch (vf) {
-			case meshdata.VertexField.Float:
-			case meshdata.VertexField.Floatx2:
-			case meshdata.VertexField.Floatx3:
-			case meshdata.VertexField.Floatx4:
-				return rd.gl.FLOAT;
+	const gl1TypeForVertexField = new Map<meshdata.VertexField, number>([
+		[meshdata.VertexField.Float, GLConst.FLOAT],
+		[meshdata.VertexField.Floatx2, GLConst.FLOAT],
+		[meshdata.VertexField.Floatx3, GLConst.FLOAT],
+		[meshdata.VertexField.Floatx4, GLConst.FLOAT],
 
-			case meshdata.VertexField.UInt32:
-			case meshdata.VertexField.UInt32x2:
-			case meshdata.VertexField.UInt32x3:
-			case meshdata.VertexField.UInt32x4:
-				return rd.gl.UNSIGNED_INT;
+		[meshdata.VertexField.UInt32, GLConst.UNSIGNED_INT],
+		[meshdata.VertexField.UInt32x2, GLConst.UNSIGNED_INT],
+		[meshdata.VertexField.UInt32x3, GLConst.UNSIGNED_INT],
+		[meshdata.VertexField.UInt32x4, GLConst.UNSIGNED_INT],
 
-			case meshdata.VertexField.SInt32:
-			case meshdata.VertexField.SInt32x2:
-			case meshdata.VertexField.SInt32x3:
-			case meshdata.VertexField.SInt32x4:
-				return rd.gl.INT;
+		[meshdata.VertexField.SInt32, GLConst.INT],
+		[meshdata.VertexField.SInt32x2, GLConst.INT],
+		[meshdata.VertexField.SInt32x3, GLConst.INT],
+		[meshdata.VertexField.SInt32x4, GLConst.INT],
 
-			case meshdata.VertexField.UInt16x2:
-			case meshdata.VertexField.Norm_UInt16x2:
-			case meshdata.VertexField.UInt16x3:
-			case meshdata.VertexField.Norm_UInt16x3:
-			case meshdata.VertexField.UInt16x4:
-			case meshdata.VertexField.Norm_UInt16x4:
-				return rd.gl.UNSIGNED_SHORT;
+		[meshdata.VertexField.UInt16x2, GLConst.UNSIGNED_SHORT],
+		[meshdata.VertexField.UInt16x3, GLConst.UNSIGNED_SHORT],
+		[meshdata.VertexField.UInt16x4, GLConst.UNSIGNED_SHORT],
+		[meshdata.VertexField.Norm_UInt16x2, GLConst.UNSIGNED_SHORT],
+		[meshdata.VertexField.Norm_UInt16x3, GLConst.UNSIGNED_SHORT],
+		[meshdata.VertexField.Norm_UInt16x4, GLConst.UNSIGNED_SHORT],
 
-			case meshdata.VertexField.SInt16x2:
-			case meshdata.VertexField.Norm_SInt16x2:
-			case meshdata.VertexField.SInt16x3:
-			case meshdata.VertexField.Norm_SInt16x3:
-			case meshdata.VertexField.SInt16x4:
-			case meshdata.VertexField.Norm_SInt16x4:
-				return rd.gl.SHORT;
+		[meshdata.VertexField.SInt16x2, GLConst.SHORT],
+		[meshdata.VertexField.SInt16x3, GLConst.SHORT],
+		[meshdata.VertexField.SInt16x4, GLConst.SHORT],
+		[meshdata.VertexField.Norm_SInt16x2, GLConst.SHORT],
+		[meshdata.VertexField.Norm_SInt16x3, GLConst.SHORT],
+		[meshdata.VertexField.Norm_SInt16x4, GLConst.SHORT],
 
-			case meshdata.VertexField.UInt8x2:
-			case meshdata.VertexField.Norm_UInt8x2:
-			case meshdata.VertexField.UInt8x3:
-			case meshdata.VertexField.Norm_UInt8x3:
-			case meshdata.VertexField.UInt8x4:
-			case meshdata.VertexField.Norm_UInt8x4:
-				return rd.gl.UNSIGNED_BYTE;
+		[meshdata.VertexField.UInt8x2, GLConst.UNSIGNED_BYTE],
+		[meshdata.VertexField.UInt8x3, GLConst.UNSIGNED_BYTE],
+		[meshdata.VertexField.UInt8x4, GLConst.UNSIGNED_BYTE],
+		[meshdata.VertexField.Norm_UInt8x2, GLConst.UNSIGNED_BYTE],
+		[meshdata.VertexField.Norm_UInt8x3, GLConst.UNSIGNED_BYTE],
+		[meshdata.VertexField.Norm_UInt8x4, GLConst.UNSIGNED_BYTE],
 
-			case meshdata.VertexField.SInt8x2:
-			case meshdata.VertexField.Norm_SInt8x2:
-			case meshdata.VertexField.SInt8x3:
-			case meshdata.VertexField.Norm_SInt8x3:
-			case meshdata.VertexField.SInt8x4:
-			case meshdata.VertexField.Norm_SInt8x4:
-				return rd.gl.BYTE;
-
-			default:
-				assert(false, "Invalid mesh.VertexField");
-				return rd.gl.NONE;
-		}
-	}
+		[meshdata.VertexField.SInt8x2, GLConst.BYTE],
+		[meshdata.VertexField.SInt8x3, GLConst.BYTE],
+		[meshdata.VertexField.SInt8x4, GLConst.BYTE],
+		[meshdata.VertexField.Norm_SInt8x2, GLConst.BYTE],
+		[meshdata.VertexField.Norm_SInt8x3, GLConst.BYTE],
+		[meshdata.VertexField.Norm_SInt8x4, GLConst.BYTE],
+	]);
 
 	const shaderRoleToAttributeRole: { [rr: string]: meshdata.VertexAttributeRole } = {
 		position: meshdata.VertexAttributeRole.Position,
@@ -161,7 +148,7 @@ namespace sd.render.gl1 {
 					gl.enableVertexAttribArray(attr.index);
 
 					const elementCount = meshdata.vertexFieldElementCount(meshAttr.field);
-					const glElementType = gl1TypeForVertexField(rd, meshAttr.field);
+					const glElementType = gl1TypeForVertexField.get(meshAttr.field)! | 0;
 					const normalized = meshdata.vertexFieldIsNormalized(meshAttr.field);
 					gl.vertexAttribPointer(attr.index, elementCount, glElementType, normalized, mesh.bufferStrides[boundBufferIndex], meshAttr.offset);
 				}
