65ea46a028e5491afdca563788f322f9bbf128ff
Arthur Langereis <puurtuur@me.com>
2017-06-05 15:17:17 +0200

    feat(core): add makeLUT helper

--- src/core/util.ts
+++ src/core/util.ts
@@ -5,12 +5,10 @@
 
 /// <reference path="./numeric.ts" />
 
-
 interface Console {
 	// Safari-specific debugging extension
 	takeHeapSnapshot(): void;
 }
-
 
 namespace sd {
 
@@ -22,8 +20,11 @@ namespace sd {
 	}
 
 
-	// Deep clone an object. Use only for simple struct types.
-	export function cloneStructDeep<T>(object: T): T {
+	/**
+	 * Deep clone an object. Use only for simple struct types.
+	 * @param object The object to clone
+	 */
+	export function cloneStructDeep<T extends object>(object: T): T {
 		const copy: any = {};
 		Object.getOwnPropertyNames(object).forEach(name => {
 			if (typeof (object as any)[name] === "object") {
@@ -34,6 +35,20 @@ namespace sd {
 			}
 		});
 		return copy as T;
+	}
+
+
+	/**
+	 * Create an immutable object that acts as a lookup table with numerical keys, such as (const) enum values.
+	 * @param keyVals Alternating key, value pairs
+	 */
+	export function makeLUT<A extends number, B>(...keyVals: (A | B)[]): { readonly [k: number]: Readonly<B>; } {
+		const lut: { [k: number]: B; } = {};
+		const count = keyVals.length;
+		for (let i = 0; i < count; i += 2) {
+			lut[keyVals[i] as number] = keyVals[i + 1] as B;
+		}
+		return Object.freeze(lut);
 	}
 
 } // ns sd
