7533642f53b06af5b4224b7c3d7f4e2f7dca819c
Arthur Langereis <puurtuur@me.com>
2017-06-19 14:58:21 +0200

    docs: add framebuffer doc comments

--- src/render/framebuffer.ts
+++ src/render/framebuffer.ts
@@ -5,23 +5,68 @@
 
 namespace sd.render {
 
+	/**
+	 * A render resource referencing a frame buffer aka render target.
+	 * A FrameBuffer can have multiple colour attachment, an optional
+	 * depth attachments and an optional stencil attachment.
+	 */
 	export interface FrameBuffer extends RenderResourceBase {
+		/**
+		 * Width in pixels for all attachments
+		 * @readonly
+		 */
 		readonly width: number;
+		/**
+		 * Height in pixels for all attachments
+		 * @readonly
+		 */
 		readonly height: number;
 
+		/**
+		 * Array of colour attachments, may be empty.
+		 * @readonly
+		 */
 		readonly colourAttachments: ReadonlyArray<AttachmentDescriptor>;
+		/**
+		 * Optional depth attachment.
+		 * @readonly
+		 */
 		readonly depthAttachment?: AttachmentDescriptor;
+		/**
+		 * Optional stencil attachment.
+		 * @readonly
+		 */
 		readonly stencilAttachment?: AttachmentDescriptor;
 	}
 
 	// ----
 
+	/**
+	 * Describes the properties of a [[FrameBuffer]] attachment.
+	 */
 	export interface AttachmentDescriptor {
+		/**
+		 * The texture resource that will act as the attachment buffer.
+		 */
 		texture: Texture;
-		level: number; // mipmap
-		layer: number | CubeMapFace; // TexCube only: 0..5
+		/**
+		 * The mipmap level of [[texture]] to use.
+		 * Not supported when using the WebGL 1 renderer.
+		 */
+		level: number;
+		/**
+		 * Either the [[CubeMapFace]] in case of a cube texture or the layer in case of a 2D Array texture. 0 otherwise.
+		 * Not supported when using the WebGL 1 renderer.
+		 */
+		layer: number | CubeMapFace;
 	}
 
+	/**
+	 * Utility function to create an [[AttachmentDescriptor]] object.
+	 * @param texture The [[Texture]] to bind.
+	 * @param level Optional: mipmap level of texture to bind.
+	 * @param layer Optional: cube map face or array layer to bind.
+	 */
 	export function makeAttachmentDescriptor(texture: Texture, level?: number, layer?: number | CubeMapFace): AttachmentDescriptor {
 		return {
 			texture,
@@ -30,12 +75,19 @@ namespace sd.render {
 		};
 	}
 
+	/**
+	 * Object describing a [[FrameBuffer]]. Used by the [[makeFrameBuffer]] function.
+	 */
 	export interface FrameBufferDescriptor {
 		colourAttachments: AttachmentDescriptor[];
 		depthAttachment?: AttachmentDescriptor;
 		stencilAttachment?: AttachmentDescriptor;
 	}
 
+	/**
+	 * Makes a [[FrameBuffer]] render resource using a descriptor.
+	 * @param desc A [[FrameBufferDescriptor]] describing the [[FrameBuffer]] resource to make.
+	 */
 	export function makeFrameBuffer(desc: FrameBufferDescriptor): FrameBuffer {
 		assert(desc.colourAttachments.length > 0, "FrameBuffer must specify at least one colour attachment.");
 		const { width, height } = desc.colourAttachments[0].texture.dim;
@@ -51,27 +103,49 @@ namespace sd.render {
 
 	// ----
 
-	// This structure facilitates easy creation of all required
-	// textures for a FrameBuffer in case they need to be allocated anyway
-	// The implementation is free to allocate the textures as fit for the
-	// platform (2D array, multiple 2D textures, etc.) so no assumptions should
-	// be made about the type or organization of the textures.
-
+	/**
+	 * This structure facilitates easy creation of all required
+	 * textures for a FrameBuffer in case they need to be allocated anyway.
+	 * The implementation is free to allocate the textures as fit for the
+	 * platform (2D array, multiple 2D textures, etc.) so no assumptions should
+	 * be made about the type or organization of the textures.
+	 */
 	export interface FrameBufferAllocationDescriptor {
-		// all textures in a FrameBuffer have the same dimensions
+		/**
+		 * Width in pixels for all attachments
+		 */
 		width: number;
+		/**
+		 * Height in pixels for all attachments
+		 */
 		height: number;
 
-		// format of the colour attachments
+		/**
+		 * [[PixelFormat]] of all the colour attachments
+		 */
 		colours: image.PixelFormat[];
 
-		// The implementation may create a combined depth/stencil texture if it
-		// fits the profile of the provided texture formats, or you can make it
-		// explicit by setting both to the same DepthStencil PixelFormat.
+		/**
+		 * Optional depth attachment [[PixelFormat]], None means no attachment.
+		 * The implementation may create a combined depth/stencil texture if it
+		 * fits the profile of the provided texture formats, or you can make it
+		 * explicit by setting both to the same DepthStencil PixelFormat.
+		 */
 		depth: image.PixelFormat.None | image.PixelFormat.Depth16I | image.PixelFormat.Depth24I | image.PixelFormat.Depth24_Stencil8;
+		/**
+		 * Optional stencil attachment [[PixelFormat]], None means no attachment.
+		 * The implementation may create a combined depth/stencil texture if it
+		 * fits the profile of the provided texture formats, or you can make it
+		 * explicit by setting both to the same DepthStencil PixelFormat.
+		 */
 		stencil: image.PixelFormat.None | image.PixelFormat.Stencil8 | image.PixelFormat.Depth24_Stencil8;
 	}
 
+	/**
+	 * Utility function to make an empty [[FrameBufferAllocationDescriptor]] object.
+	 * @param width Required width of the frame buffer.
+	 * @param height Required height of the frame buffer.
+	 */
 	export function makeFrameBufferAllocationDescriptor(width: number, height: number): FrameBufferAllocationDescriptor {
 		return {
 			width, height,
@@ -81,6 +155,11 @@ namespace sd.render {
 		};
 	}
 
+	/**
+	 * Make a full [[FrameBufferDescriptor]] by allocating the required attachments
+	 * 
+	 * @param desc Descriptor of required resources for the frame buffer.
+	 */
 	export function allocateTexturesForFrameBuffer(desc: FrameBufferAllocationDescriptor): FrameBufferDescriptor {
 		const { width, height } = desc;
 
@@ -140,6 +219,11 @@ namespace sd.render {
 		return fbDesc;
 	}
 
+	/**
+	 * Using a [[FrameBufferAllocationDescriptor]], allocate all needed attachments and create a [[FrameBuffer]].
+	 * @param fbad The descriptor of the [[FrameBuffer]] to allocate
+	 * @returns The fully allocated [[FrameBuffer]]
+	 */
 	export function allocateFrameBuffer(fbad: FrameBufferAllocationDescriptor): FrameBuffer {
 		const fbd = allocateTexturesForFrameBuffer(fbad);
 		return makeFrameBuffer(fbd);
