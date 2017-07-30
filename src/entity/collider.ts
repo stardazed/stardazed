d8d7f26b4a9262dbd62ddb11a0d2ee03499251d6
Arthur Langereis <puurtuur@me.com>
2017-07-30 11:59:50 +0200

    ref(phys): define construction objs for Colliders

--- src/entity/collider.ts
+++ src/entity/collider.ts
@@ -5,14 +5,92 @@
 
 namespace sd.entity {
 
-	export const enum ColliderShape {
+	export const enum ColliderShapeType {
 		Box,
 		Sphere,
 		Capsule,
+		Cylinder,
+		Cone,
 		Plane,
-		ConvexMesh,
-		ConcaveMesh
+		ConvexHull,
+		Mesh,
+		HeightField
 	}
+
+	export interface BoxShape {
+		type: ColliderShapeType.Box;
+		halfExtents: Float3;
+		margin?: number;
+		scale?: ConstFloat3;
+	}
+
+	export interface SphereShape {
+		type: ColliderShapeType.Sphere;
+		radius: number;
+		margin?: number;
+		scale?: ConstFloat3;
+	}
+
+	export interface CapsuleShape {
+		type: ColliderShapeType.Capsule;
+		radius: number;
+		height: number;
+		orientation: Ammo.AxisIndex;
+		margin?: number;
+		scale?: ConstFloat3;
+	}
+
+	export interface CylinderShape {
+		type: ColliderShapeType.Cylinder;
+		halfExtents: ConstFloat3;
+		orientation: Ammo.AxisIndex;
+		margin?: number;
+		scale?: ConstFloat3;
+	}
+
+	export interface ConeShape {
+		type: ColliderShapeType.Cone;
+		radius: number;
+		height: number;
+		orientation: Ammo.AxisIndex;
+		scale?: ConstFloat3;
+	}
+
+	export interface PlaneShape {
+		type: ColliderShapeType.Plane;
+		planeNormal: ConstFloat3;
+		planeConstant: number;
+	}
+
+	export interface ConvexHullShape {
+		type: ColliderShapeType.ConvexHull;
+		pointCount: number;
+		points: ArrayOfConstNumber; // vec3s laid out linearly
+	}
+
+	export interface MeshShape {
+		type: ColliderShapeType.Mesh;
+		mesh: meshdata.MeshData;
+		convex: boolean;
+		convexMargin?: number;
+		scale?: ConstFloat3;
+	}
+
+	export interface HeightFieldShape {
+		// constructor(heightStickWidth: number, heightStickLength: number, heightfieldData: VoidPtr, heightScale: number, minHeight: number, maxHeight: number, upAxis: AxisIndex, hdt: PHY_ScalarType, flipQuadEdges: boolean);
+		type: ColliderShapeType.HeightField;
+		gridWidth: number;
+		gridDepth: number;
+		minHeight: number;
+		maxHeight: number;
+		heightScale?: number;
+		orientation?: Ammo.AxisIndex;
+		scale?: ConstFloat3;
+
+		// TODO: finish later
+	}
+
+	export type ColliderShape = BoxShape | SphereShape | CapsuleShape | CylinderShape | ConeShape | PlaneShape | ConvexHullShape | MeshShape | HeightFieldShape;
 
 	export const enum ColliderType {
 		Trigger,
@@ -20,10 +98,10 @@ namespace sd.entity {
 	}
 
 	export interface Collider {
-		
+		shape: ColliderShape;
 
 		// rigidbody
-		mass: number;
+		mass: number; // 0 means fixed static object
 		linearDrag: number;
 		angularDrag: number;
 		kinematic: boolean;
@@ -51,8 +129,8 @@ namespace sd.entity {
 			this.world_.setGravity( new Ammo.btVector3(0, -9.8, 0));
 		}
 
-		create(entity: Entity): ColliderInstance {
-			return instance;
+		create(_entity: Entity, _collider: Collider): ColliderInstance {
+			return 0;
 		}
 
 		destroy(_inst: ColliderInstance) {
@@ -65,7 +143,7 @@ namespace sd.entity {
 			}
 		}
 
-		get count() { return this.instanceData_.count; }
+		get count() { return 0; }
 
 		valid(inst: ColliderInstance) {
 			return inst <= this.count;
