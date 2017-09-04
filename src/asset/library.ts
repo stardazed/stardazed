52daa166038e52ae2f02e51e7851a82e2519dbb6
Arthur Langereis <puurtuur@me.com>
2017-09-04 12:20:14 +0200

    wip(asset): older asset management files

---
+++ src/asset/library.ts
@@ -0,0 +1,12 @@
+// asset/library - 
+// Part of Stardazed
+// (c) 2015-2017 by Arthur Langereis - @zenmumbler
+// https://github.com/stardazed/stardazed
+
+namespace sd.asset {
+
+	export class AssetLibrary {
+		
+	}
+
+} // ns sd.asset
