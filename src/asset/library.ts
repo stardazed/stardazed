8232264b3c29f124da144b6af6fae34cc45410a5
Arthur Langereis <puurtuur@me.com>
2017-09-10 19:00:13 +0200

    chore(asset): library description

--- src/asset/library.ts
+++ src/asset/library.ts
@@ -1,4 +1,4 @@
-// asset/library - it's-a back!
+// asset/library - extensible asset loader and caching class
 // Part of Stardazed
 // (c) 2015-2017 by Arthur Langereis - @zenmumbler
 // https://github.com/stardazed/stardazed
