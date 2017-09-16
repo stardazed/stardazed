a53388a1c639cf0c28cd63cda043e399a30dc3cd
Arthur Langereis <puurtuur@me.com>
2017-09-16 21:04:54 +0200

    feat(asset): add material parser w/o texture resolve

---
+++ src/asset/parser/material.ts
@@ -0,0 +1,326 @@
+// asset/parser/material - standard material assets
+// Part of Stardazed
+// (c) 2015-2017 by Arthur Langereis - @zenmumbler
+// https://github.com/stardazed/stardazed
+
+/// <reference path="../library.ts" />
+
+namespace sd.asset {
+
+	export namespace parser {
+
+		export interface MaterialColourMetadata {
+			type: "diffuse" | "diffuseSpecular" | "pbrMetallic" | "pbrSpecular";
+
+			baseColour: Float3;
+			specularFactor: Float3;
+			specularExponent: number;
+			metallic: number;
+			roughness: number;
+
+			colourTexture: RawAsset<TextureAssetMetadata>;
+			metallicTexture: RawAsset<TextureAssetMetadata>;
+			roughnessTexture: RawAsset<TextureAssetMetadata>;
+			specularTexture: RawAsset<TextureAssetMetadata>;
+		}
+
+		export interface MaterialAssetMetadata {
+			colour: Partial<MaterialColourMetadata>;
+
+			doubleSided: boolean;
+			uvScale: Float2;
+			uvOffset: Float2;
+
+			alphaCoverage: "ignore" | "mask" | "transparent";
+			alphaFactor: number;
+			alphaCutoff: number;
+
+			emissiveFactor: Float3;
+			heightRange: number;
+
+			alphaTexture: RawAsset<TextureAssetMetadata>;
+			emissiveTexture: RawAsset<TextureAssetMetadata>;
+			normalTexture: RawAsset<TextureAssetMetadata>;
+			heightTexture: RawAsset<TextureAssetMetadata>;
+			ambientOcclusionTexture: RawAsset<TextureAssetMetadata>;
+		}
+
+		// ----------------
+
+		type RawTextureRole = "colour" | "metallic" | "roughness" | "specular" | "alpha" | "emissive" | "normal" | "height" | "ao";
+
+		class RawTextures {
+			roles: RawTextureRole[] = [];
+			rawAssets: RawAsset<TextureAssetMetadata>[] = [];
+
+			add(role: RawTextureRole, tex: RawAsset<TextureAssetMetadata> | undefined) {
+				if (tex) {
+					this.roles.push(role);
+					this.rawAssets.push(tex);
+				}
+			}
+		}
+
+		// ----------------
+
+		/**
+		 * Try to interpret the passed value as a float array.
+		 * Returns undefined in case of failure.
+		 * @param f3 A value presumed to be a valid Float3
+		 * @param len The required length of the vector
+		 */
+		const parseFloatArray = (fx: any, len: number) => {
+			let res: Float32Array | undefined;
+			if (Array.isArray(fx) && fx.length === len) {
+				res = new Float32Array(fx);
+				if (fx.some(f => isNaN(f))) {
+					res = undefined;
+				}
+			}
+			return res;
+		};
+
+		const parseFloatValue = (f: any) => {
+			const res = parseFloat(f);
+			if (typeof res === "number" && !isNaN(res)) {
+				return res;
+			}
+			return undefined;
+		};
+
+		const getBaseColour = (colour: Partial<MaterialColourMetadata>, def: Float3) => {
+			if (colour.baseColour !== void 0) {
+				const f3 = parseFloatArray(colour.baseColour, 3);
+				if (f3 && f3.every(f => f >= 0)) {
+					return f3;
+				}
+				console.warn(`Material parser: ignoring invalid baseColour`, colour.baseColour);
+			}
+			return def;
+		};
+
+		const getSpecularFactor = (colour: Partial<MaterialColourMetadata>, def: Float3) => {
+			if (colour.specularFactor !== void 0) {
+				const f3 = parseFloatArray(colour.specularFactor, 3);
+				if (f3 && f3.every(f => f >= 0)) {
+					return f3;
+				}
+				console.warn(`Material parser: ignoring invalid specularFactor`, colour.specularFactor);
+			}
+			return def;
+		};
+
+		const getSpecularExponent = (colour: Partial<MaterialColourMetadata>, def: number) => {
+			if (colour.specularExponent !== void 0) {
+				const f = parseFloatValue(colour.specularExponent);
+				if (f !== void 0 && f > 0 && f < 256) {
+					return f;
+				}
+				console.warn(`Material parser: ignoring invalid specularExponent`, colour.specularExponent);
+			}
+			return def;
+		};
+
+		const getMetallicFactor = (colour: Partial<MaterialColourMetadata>, def: number) => {
+			if (colour.metallic !== void 0) {
+				const f = parseFloatValue(colour.metallic);
+				if (f !== void 0 && f >= 0 && f <= 1) {
+					return f;
+				}
+				console.warn(`Material parser: ignoring invalid metallicFactor`, colour.metallic);
+			}
+			return def;
+		};
+
+		const getRoughnessFactor = (colour: Partial<MaterialColourMetadata>, def: number) => {
+			if (colour.roughness !== void 0) {
+				const f = parseFloatValue(colour.roughness);
+				if (f !== void 0 && f >= 0) {
+					return f;
+				}
+				console.warn(`Material parser: ignoring invalid roughnessFactor`, colour.roughness);
+			}
+			return def;
+		};
+
+		const getAlphaCoverage = (meta: Partial<MaterialAssetMetadata>, def: AlphaCoverage) => {
+			switch (meta.alphaCoverage) {
+				case "ignore": return AlphaCoverage.Ignore;
+				case "mask": return AlphaCoverage.Mask;
+				case "transparent": return AlphaCoverage.Transparent;
+				case void 0: return def;
+			}
+			console.warn(`Material parser: ignoring invalid alphaCoverage`, meta.alphaCoverage);
+			return def;
+		};
+
+		const getAlphaCutoff = (meta: Partial<MaterialAssetMetadata>, def: number) => {
+			if (meta.alphaCutoff !== void 0) {
+				const f = parseFloatValue(meta.alphaCutoff);
+				if (f !== void 0 && f >= 0 && f <= 1) {
+					return f;
+				}
+				console.warn(`Material parser: ignoring invalid alphaCutoff`, meta.alphaCutoff);
+			}
+			return def;
+		};
+
+		const getAlphaFactor = (meta: Partial<MaterialAssetMetadata>, def: number) => {
+			if (meta.alphaFactor !== void 0) {
+				const f = parseFloatValue(meta.alphaFactor);
+				if (f !== void 0 && f >= 0) {
+					return f;
+				}
+				console.warn(`Material parser: ignoring invalid alphaFactor`, meta.alphaFactor);
+			}
+			return def;
+		};
+
+		const getHeightRange = (meta: Partial<MaterialAssetMetadata>, def: number) => {
+			if (meta.heightRange !== void 0) {
+				const f = parseFloatValue(meta.heightRange);
+				if (f !== void 0) {
+					return f;
+				}
+				console.warn(`Material parser: ignoring invalid heightRange`, meta.heightRange);
+			}
+			return def;
+		};
+
+		const getEmissiveFactor = (meta: Partial<MaterialAssetMetadata>, def: Float3) => {
+			if (meta.emissiveFactor !== void 0) {
+				const f3 = parseFloatArray(meta.emissiveFactor, 3);
+				if (f3 && f3.every(f => f >= 0)) {
+					return f3;
+				}
+				console.warn(`Material parser: ignoring invalid emissiveFactor`, meta.emissiveFactor);
+			}
+			return def;
+		};
+
+		const getDoubleSided = (meta: Partial<MaterialAssetMetadata>, def: boolean) => {
+			if (meta.doubleSided !== void 0) {
+				if (meta.doubleSided === true || meta.doubleSided === false) {
+					return meta.doubleSided;
+				}
+				console.warn(`Material parser: ignoring invalid doubleSided`, meta.doubleSided);
+			}
+			return def;
+		};
+
+		const getUVScale = (meta: Partial<MaterialAssetMetadata>, def: Float2) => {
+			if (meta.uvScale !== void 0) {
+				const f2 = parseFloatArray(meta.uvScale, 2);
+				if (f2) {
+					return f2;
+				}
+				console.warn(`Material parser: ignoring invalid uvScale`, meta.uvScale);
+			}
+			return def;
+		};
+
+		const getUVOffset = (meta: Partial<MaterialAssetMetadata>, def: Float2) => {
+			if (meta.uvOffset !== void 0) {
+				const f2 = parseFloatArray(meta.uvOffset, 2);
+				if (f2) {
+					return f2;
+				}
+				console.warn(`Material parser: ignoring invalid uvOffset`, meta.uvOffset);
+			}
+			return def;
+		};
+
+		// ------------
+
+		function getDiffuseResponseData(meta: Partial<MaterialAssetMetadata>, rawTex: RawTextures) {
+			const response = makeDiffuseResponse();
+			const colour = meta.colour!;
+
+			response.baseColour = getBaseColour(colour, response.baseColour);
+			rawTex.add("colour", colour.colourTexture);
+
+			return response;
+		}
+
+		function getDiffuseSpecularResponseData(meta: Partial<MaterialAssetMetadata>, rawTex: RawTextures) {
+			const response = makeDiffuseSpecularResponse(getDiffuseResponseData(meta, rawTex));
+			const colour = meta.colour!;
+			
+			response.specularFactor = getSpecularFactor(colour, response.specularFactor);
+			response.specularExponent = getSpecularExponent(colour, response.specularExponent);
+			rawTex.add("specular", colour.specularTexture);
+
+			return response;
+		}
+
+		function getPBRMetallicResponseData(meta: Partial<MaterialAssetMetadata>, rawTex: RawTextures) {
+			const response = makePBRMetallicResponse(getDiffuseResponseData(meta, rawTex));
+			const colour = meta.colour!;
+
+			response.metallic = getMetallicFactor(colour, response.metallic);
+			response.roughness = getRoughnessFactor(colour, response.roughness);
+			rawTex.add("metallic", colour.metallicTexture);
+			rawTex.add("roughness", colour.roughnessTexture);
+
+			return response;
+		}
+
+		function getPBRSpecularResponseData(meta: Partial<MaterialAssetMetadata>, rawTex: RawTextures) {
+			const response = makePBRSpecularResponse(getDiffuseResponseData(meta, rawTex));
+			const colour = meta.colour!;
+
+			response.specularFactor = getSpecularFactor(colour, response.specularFactor);
+			response.roughness = getRoughnessFactor(colour, response.roughness);
+			rawTex.add("specular", colour.specularTexture);
+			rawTex.add("roughness", colour.roughnessTexture);
+
+			return response;
+		}
+
+		/**
+		 * Create a standard Material.
+		 * @param resource The source data to be parsed
+		 */
+		export function* parseMaterial(resource: RawAsset<MaterialAssetMetadata>) {
+			const textures = new RawTextures();
+			const meta = resource.metadata;
+
+			let colour: ColourResponse;
+			const colourType = meta.colour && meta.colour.type;
+			switch (colourType) {
+				case "diffuse": colour = getDiffuseResponseData(meta, textures); break;
+				case "diffuseSpecular": colour = getDiffuseSpecularResponseData(meta, textures); break;
+				case "pbrMetallic": colour = getPBRMetallicResponseData(meta, textures); break;
+				case "pbrSpecular": colour = getPBRSpecularResponseData(meta, textures); break;
+				default:
+					throw new Error("Material parser: missing or invalid colour type.");
+			}
+
+			const mat = makeMaterial(resource.name, colour);
+			mat.alphaCoverage = getAlphaCoverage(meta, mat.alphaCoverage);
+			mat.alphaCutoff = getAlphaCutoff(meta, mat.alphaCutoff);
+			mat.alphaFactor = getAlphaFactor(meta, mat.alphaFactor);
+			mat.heightRange = getHeightRange(meta, mat.heightRange);
+			mat.emissiveFactor = getEmissiveFactor(meta, mat.emissiveFactor);
+			mat.doubleSided = getDoubleSided(meta, mat.doubleSided);
+			mat.uvScale = getUVScale(meta, mat.uvScale);
+			mat.uvOffset = getUVOffset(meta, mat.uvOffset);
+
+			textures.add("alpha", meta.alphaTexture);
+			textures.add("normal", meta.normalTexture);
+			textures.add("height", meta.heightTexture);
+			textures.add("ao", meta.ambientOcclusionTexture);
+			textures.add("emissive", meta.emissiveTexture);
+			
+			return mat;
+		}
+
+	} // ns parser
+
+	export interface Library {
+		loadMaterial(ra: parser.RawAsset): Promise<Material>;
+		materialByName(name: string): Material | undefined;
+	}
+	registerAssetLoaderParser("material", parser.parseMaterial);
+
+} // ns sd.asset
