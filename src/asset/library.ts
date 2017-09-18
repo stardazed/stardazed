d0b612c06cfa1821e5a517c344beacdb56234629
Arthur Langereis <puurtuur@me.com>
2017-09-18 15:49:34 +0200

    ref(asset): v3 library is but a simple shell

--- src/asset/library.ts
+++ src/asset/library.ts
@@ -1,146 +1,39 @@
-// asset/library - extensible asset loader and caching class
+// asset/library - extensible asset processor
 // Part of Stardazed
 // (c) 2015-2017 by Arthur Langereis - @zenmumbler
 // https://github.com/stardazed/stardazed
 
-/// <reference path="./parsers.ts" />
-
 namespace sd.asset {
 
-	export type LoaderParser = (sa: parser.RawAsset) => Promise<any> | Iterator<any>;
-
-	export class LibraryBase {
-		private loader_: loader.Loader;
-		private loaderParserFuncs_: { [kind: string]: LoaderParser | undefined; } = {};
-
-		constructor(loaderInfo: loader.LoaderInfo) {
-			this.loader_ = loader.makeLoader(loaderInfo);
-		}
-
-		protected registerLoaderParser(forKind: string, lp: LoaderParser) {
-			this.loaderParserFuncs_[forKind] = lp.bind(this);
-		}
-
-		protected loadData<Metadata extends object>(ra: parser.RawAsset<Metadata>): Promise<parser.RawAsset<Metadata>> {
-			if (ra.dataPath !== void 0 && ra.dataBlob === void 0) {
-				if (ra.mimeType === undefined) {
-					ra.mimeType = parser.mimeTypeForFileExtension(io.fileExtensionOfURL(ra.dataPath));
-				}
+	export interface Asset<AssetItem = any, Metadata extends object = any> {
+		kind?: string;
+		id?: string;
+		name?: string;
+		metadata?: Partial<Metadata>;
+		item?: AssetItem;
+	}
 	
-				return this.loader_(ra.dataPath, ra.mimeType).then(
-					blob => {
-						ra.dataBlob = blob;
-						return ra;
-					}
-				);
-			}
-			else {
-				return Promise.resolve(ra);
-			}
-		}
-
-		protected async processParser(res: Promise<any> | Iterator<any>): Promise<any> {
-			if (res instanceof Promise) {
-				return res.then(internal => {
-					if (isIterator(internal)) {
-						return this.processParser(internal);
-					}
-					else {
-						return internal;
-					}
-				});
-			}
-			else {
-				let subAssets: any[] | any | undefined; // evals to just "any", but explicitly specified here for doc-purposes
-				do {
-					const itr = res.next(subAssets);
-					if (itr.done) {
-						return itr.value;
-					}
-					else {
-						const ras: parser.RawAsset | parser.RawAsset[] = itr.value;
-						if (Array.isArray(ras)) {
-							assert(ras.every(sa => parser.isRawAsset(sa)), "Library: Iterator AssetParser must yield only (arrays of) RawAssets");
-							subAssets = await this.loadAssetFile(ras);
-						}
-						else {
-							assert(parser.isRawAsset(ras), "Library: Iterator AssetParser must yield only (arrays of) RawAssets");
-							subAssets = await this.loadAny(ras);
-						}
-						
-					}
-				} while (true);
-			}
-		}
-
-		protected preprocessRawAsset(ra: parser.RawAsset) {
-			return this.loadData(ra);
-		}
-
-		loadAny(ra: parser.RawAsset) {
-			return this.preprocessRawAsset(ra).then(pra => {
-				const loaderParser = this.loaderParserFuncs_[pra.kind];
-				if (loaderParser) {
-					return this.processParser(loaderParser(pra));
-				}
-				return Promise.reject(new Error(`No registered parser for asset kind: ${pra.kind}, requested path: ${pra.dataPath}`));
-			});
-		}
-
-		loadAssetFile(assets: parser.RawAsset[]) {
-			return Promise.all(assets.map(sa => this.loadAny(sa)));
-		}
+	export type AssetProcessor = (ra: Asset) => Promise<Asset>;
+	export type LibraryPlugin = (lib: AssetLibrary) => void;
+	
+	export interface AssetLibrary {
+		process: AssetProcessor;
 	}
 
-	export type LibraryExtension = (Base: Constructor<LibraryBase>) => Constructor<LibraryBase>;
-	const mixins: LibraryExtension[] = [];
+	export const makeLibrary = (plugins: LibraryPlugin[]) => {
+		const library: AssetLibrary = {
+			// initial processor simply resolves to the input
+			process: (asset: Asset) => Promise.resolve(asset)
+		};
 
-	export const addLibraryExtension = (mixin: LibraryExtension) => {
-		mixins.push(mixin);
-	};
-
-	export const registerAssetLoaderParser = <A, M extends object>(kind: string, assetParser: parser.AssetParser<A, M>) => {
-		const cacheArrayName = `${kind}s_`;
-		const loadFuncName = `load${capitalize(kind)}`;
-		const lookupFuncName = `${kind}ByName`;
-
-		const LoaderMixin = <T extends Constructor<LibraryBase>>(Lib: T) =>
-			class extends Lib {
-				constructor(...args: any[]) {
-					super(...args);
-					(this as any)[cacheArrayName] = new Map<string, A>();
-					this.registerLoaderParser(kind, (this as any)[loadFuncName]);
-				}
-
-				[loadFuncName](ra: parser.RawAsset) {
-					return this.preprocessRawAsset(ra)
-						.then(resource => this.processParser(assetParser(resource)))
-						.then(asset => {
-							(this as any)[cacheArrayName].set(ra.name, asset);
-							return asset;
-						});
-				}
-
-				[lookupFuncName](name: string) {
-					return (this as any)[cacheArrayName].get(name);
-				}
-			};
-
-		addLibraryExtension(LoaderMixin);
-	};
-
-	export interface Library {
-		// generic load-parse methods
-		loadAny(sa: parser.RawAsset): Promise<any>;
-		loadAssetFile(assets: parser.RawAsset[]): Promise<any[]>;
-	}
-
-	export const makeLibrary = (rootLoader: loader.LoaderInfo): Library => {
-		let Lib = LibraryBase;
-		for (const m of mixins) {
-			Lib = m(Lib);
+		// allow plugins to extend the processor
+		for (const plugin of plugins) {
+			plugin(library);
 		}
-		return (new Lib(rootLoader)) as any as Library;
+		return library;
 	};
 
+	// plugins = [generator, dataloader, importer, dependencies, parser]
+	export const makeDefaultLibrary = () => makeLibrary([]);
+	
 } // ns sd.asset
