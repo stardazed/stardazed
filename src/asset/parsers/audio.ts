106cc9234be2e94cc0863ace1ebad3aef8726002
Arthur Langereis <puurtuur@me.com>
2017-09-18 18:07:44 +0200

    ref(asset): further simplify audio parser

--- src/asset/parsers/audio.ts
+++ src/asset/parsers/audio.ts
@@ -5,49 +5,43 @@
 
 /// <reference path="../library.ts" />
 
-namespace sd.asset {
+namespace sd.asset.parser {
 
-	export namespace parser {
-		export interface AudioAssetMetadata {
-		}
+	/**
+	 * Create an AudioBuffer for an asset blob
+	 * @param resource The source data to be parsed
+	 */
+	export const parseAudio: AssetProcessor = (asset: Asset<AudioBuffer, {}>) =>
+		getArrayBuffer(asset).then(
+			data => new Promise<Asset>((resolve, reject) => {
+				audio.sharedAudioContext().decodeAudioData(
+					data,
+					audioData => {
+						asset.item = audioData;
+						resolve(asset);
+					},
+					err => {
+						reject(`Invalid audio data, error: ${err}`);
+					}
+				);
+			})
+		);
 
-		/**
-		 * Create an AudioBuffer for an asset blob
-		 * @param resource The source data to be parsed
-		 */
-		export const parseAudio: AssetProcessor = (asset: Asset<AudioBuffer, AudioAssetMetadata>) =>
-			getArrayBuffer(asset).then(
-				data => new Promise<Asset>((resolve, reject) => {
-					audio.sharedAudioContext().decodeAudioData(
-						data,
-						audioData => {
-							asset.item = audioData;
-							resolve(asset);
-						},
-						err => {
-							reject(`Invalid audio data, error: ${err}`);
-						}
-					);
-				})
-			);
+	registerFileExtension("mp3", "audio/mpeg");
+	registerFileExtension("m4a", "audio/mp4");
+	registerFileExtension("mp4", "audio/mp4");
+	registerFileExtension("ogg", "audio/ogg");
+	registerFileExtension("wav", "audio/vnd.wav");
+	registerFileExtension("aif", "audio/x-aiff");
+	registerFileExtension("aifc", "audio/x-aiff");
+	registerFileExtension("aiff", "audio/x-aiff");
 
-		registerFileExtension("mp3", "audio/mpeg");
-		registerFileExtension("m4a", "audio/mp4");
-		registerFileExtension("mp4", "audio/mp4");
-		registerFileExtension("ogg", "audio/ogg");
-		registerFileExtension("wav", "audio/vnd.wav");
-		registerFileExtension("aif", "audio/x-aiff");
-		registerFileExtension("aifc", "audio/x-aiff");
-		registerFileExtension("aiff", "audio/x-aiff");
+	mapMimeTypeToAssetKind("audio/mpeg", "audio");
+	mapMimeTypeToAssetKind("audio/mp4", "audio");
+	mapMimeTypeToAssetKind("audio/ogg", "audio");
+	mapMimeTypeToAssetKind("audio/vnd.wav", "audio");
+	mapMimeTypeToAssetKind("audio/x-aiff", "audio");
 
-		mapMimeTypeToAssetKind("audio/mpeg", "audio");
-		mapMimeTypeToAssetKind("audio/mp4", "audio");
-		mapMimeTypeToAssetKind("audio/ogg", "audio");
-		mapMimeTypeToAssetKind("audio/vnd.wav", "audio");
-		mapMimeTypeToAssetKind("audio/x-aiff", "audio");
+	registerParser("audio", parseAudio);
 
-		registerParser("audio", parseAudio);
-	
-	} // ns parser
-
-} // ns sd.asset
+} // ns sd.asset.parser
