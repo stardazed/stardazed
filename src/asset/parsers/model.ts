d4be24e9479b8f482287383a1b6db32309408438
Arthur Langereis <puurtuur@me.com>
2017-09-28 01:09:52 +0200

    ref(asset): a Model is now a mesh and materials

--- src/asset/parsers/model.ts
+++ src/asset/parsers/model.ts
@@ -7,23 +7,10 @@
 
 namespace sd.asset {
 
-	export const makeTransform = (): entity.Transform => ({
-		position: [0, 0, 0],
-		rotation: [0, 0, 0, 1],
-		scale: [1, 1, 1]
-	});
-
 	export interface Model {
-		transform: entity.Transform;
-		// renderable
-		mesh?: meshdata.MeshData;
+		mesh: meshdata.MeshData;
 		materials: Material[];
 	}
-
-	export const makeModel = (): Model => ({
-		transform: makeTransform(),
-		materials: []
-	});
 
 	export interface CacheAccess {
 		(kind: "model", name: string): Model;
@@ -49,57 +36,57 @@ namespace sd.asset {
 		};
 
 		export const parseModel: AssetProcessor = (asset: Asset<Model, ModelAssetMetadata>) =>
-			new Promise<Asset<Model, ModelAssetMetadata>>((resolve, _reject) => {
-				const model = makeModel();
-
+			new Promise<Asset<Model, ModelAssetMetadata>>((resolve, reject) => {
 				if (asset.dependencies) {
 					const meshAsset = getMeshDependency(asset.dependencies);
-					if (meshAsset) {
-						model.mesh = meshAsset.item;
+					if (! meshAsset) {
+						return reject(`Model parser: a mesh dependency named "mesh" must be provided.`);
 					}
 
+					const model: Model = {
+						mesh: meshAsset.item!,
+						materials: []
+					};
+					asset.item = model;
+					
 					for (const depName of Object.getOwnPropertyNames(asset.dependencies)) {
 						const dep = asset.dependencies[depName];
 						if (dep !== void 0) {
 							if (dep.kind === "mesh") {
 								if (depName !== "mesh") {
-									console.warn(`Model parser: only mesh dependencies named "mesh" are considered, ignoring ${depName}`, dep);
+									console.warn(`Model parser: only one mesh dependency named "mesh" is considered, ignoring ${depName}`, dep);
 								}
 							}
 							else if (dep.kind === "material") {
-								if (meshAsset) {
-									if (typeof dep.item === "object" && dep.item !== null) { // FIXME: add isMaterial tester fn
-										if (asset.metadata && asset.metadata.materialIndexMap) {
-											const meshMatIndex = asset.metadata.materialIndexMap[depName];
-											if (typeof meshMatIndex === "number" && meshMatIndex >= 0) {
-												model.materials[meshMatIndex] = dep.item;
-											}
-											else {
-												console.warn(`Model parser: material named ${depName} not found in material index map, ignoring`, asset);
-											}
+								if (typeof dep.item === "object" && dep.item !== null) { // FIXME: add isMaterial tester fn
+									if (asset.metadata && asset.metadata.materialIndexMap) {
+										const meshMatIndex = asset.metadata.materialIndexMap[depName];
+										if (typeof meshMatIndex === "number" && meshMatIndex >= 0) {
+											model.materials[meshMatIndex] = dep.item;
 										}
 										else {
-											console.warn(`Model parser: no material index map was specified, materials will be in arbitrary order`, asset);
-											model.materials.push(dep.item);
+											console.warn(`Model parser: material named ${depName} not found in material index map, ignoring`, asset);
 										}
 									}
 									else {
-										console.warn(`Model parser: material dependency was not loaded`, dep);
+										console.warn(`Model parser: no material index map was specified, materials will be in arbitrary order`, asset);
+										model.materials.push(dep.item);
 									}
 								}
 								else {
-									console.warn(`Model parser: model with material dependencies does not have a mesh`, asset);
-									model.materials.push(dep.item);
+									console.warn(`Model parser: material dependency was not loaded`, dep);
 								}
 							}
 							else {
-								console.warn(`Model parser: ignoring incompatible dependency`, dep);
+								console.warn(`Model parser: ignoring non-mesh, non-material dependency`, dep);
 							}
 						}
 					}
 				}
+				else {
+					return reject(`Model parser: a model must have dependencies specified.`);
+				}
 
-				asset.item = model;
 				resolve(asset);
 			});
 
