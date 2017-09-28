4266586e4a87a24c95dda5a603958d575667fcea
Arthur Langereis <puurtuur@me.com>
2017-09-28 15:21:33 +0200

    feat(asset): mesh asset generator with box implementation

--- src/asset/generators/mesh-gen.ts
+++ src/asset/generators/mesh-gen.ts
@@ -1,8 +1,56 @@
-// asset/generators/mesh - Wavefront MTL material file importer
+// asset/generators/mesh - Mesh asset generator
 // Part of Stardazed
 // (c) 2015-2017 by Arthur Langereis - @zenmumbler
 // https://github.com/stardazed/stardazed
 
 namespace sd.asset.generator {
+
+	interface MeshGenConfig {
+		shape: string;
+	}
+
+	export const meshGenerator: AssetGenerator = (config: Partial<MeshGenConfig>) =>
+		new Promise<Asset>((resolve, reject) => {
+			let md: Promise<meshdata.MeshData>;
+			switch (config.shape) {
+				case "box":
+					md = boxMeshGenerator(config);
+					break;
+				default:
+					return reject(`Mesh generator: unknown or missing shape "${config.shape}"`);
+			}
+
+			resolve(md.then(meshData => ({
+				kind: "mesh",
+				item: meshData
+			})));
+		});
+
+	registerGenerator("mesh", meshGenerator);
+
+	// ----
+
+	interface BoxMeshGenConfig extends MeshGenConfig {
+		extents: number[];
+	}
+
+	/**
+	 * @internal
+	 * @param config box mesh configuration
+	 */
+	const boxMeshGenerator = (config: Partial<BoxMeshGenConfig>) =>
+		new Promise<meshdata.MeshData>((resolve, reject) => {
+			const { extents } = config;
+			if (Array.isArray(extents) && extents.length === 3 && extents.every(v => typeof v === "number")) {
+				resolve(meshdata.gen.generate(new meshdata.gen.Box({
+					width: extents[0],
+					height: extents[1],
+					depth: extents[2]
+				})));
+			}
+			else {
+				reject(`Box mesh generator: config.extents must be an array of numbers of length 3`);
+			}
+		});
 
 } // ns sd.asset.generator
