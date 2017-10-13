7a26e7b094a8d891b49cf1cd79fde713e50094ed
Arthur Langereis <puurtuur@me.com>
2017-10-13 16:38:13 +0200

    ref(core): remove TypedArray catch-all type and replace with union

--- src/core/array.ts
+++ src/core/array.ts
@@ -33,66 +33,13 @@ if (! ArrayBuffer.transfer) {
 
 namespace sd {
 
-	export interface TypedArrayBase {
-		readonly BYTES_PER_ELEMENT: number;
+	export type TypedArrayConstructor =
+		Uint8ArrayConstructor | Uint8ClampedArrayConstructor | Uint16ArrayConstructor | Uint32ArrayConstructor |
+		Int8ArrayConstructor | Int16ArrayConstructor | Int32ArrayConstructor |
+		Float32ArrayConstructor | Float64ArrayConstructor;
 
-		readonly buffer: ArrayBuffer | SharedArrayBuffer;
-		readonly byteLength: number;
-		readonly byteOffset: number;
-		readonly length: number;
+	export type TypedArray = Uint8Array | Uint8ClampedArray | Uint16Array | Uint32Array | Int8Array | Int16Array | Int32Array | Float32Array | Float64Array;
 
-		subarray(begin: number, end?: number): this;
-		slice(start?: number, end?: number): this;
-
-		every(callbackfn: (value: number, index: number, array: this) => boolean, thisArg?: any): boolean;
-		filter(callbackfn: (value: number, index: number, array: this) => any, thisArg?: any): this;
-		find(predicate: (value: number, index: number, obj: number[]) => boolean, thisArg?: any): number | undefined;
-		findIndex(predicate: (value: number, index: number, obj: number[]) => boolean, thisArg?: any): number;
-		forEach(callbackfn: (value: number, index: number, array: this) => void, thisArg?: any): void;
-		indexOf(searchElement: number, fromIndex?: number): number;
-		join(separator?: string): string;
-		lastIndexOf(searchElement: number, fromIndex?: number): number;
-
-		map(callbackfn: (value: number, index: number, array: this) => number, thisArg?: any): this;
-		reduce(callbackfn: (previousValue: number, currentValue: number, currentIndex: number, array: this) => number, initialValue?: number): number;
-		reduce<U>(callbackfn: (previousValue: U, currentValue: number, currentIndex: number, array: this) => U, initialValue: U): U;
-		reduceRight(callbackfn: (previousValue: number, currentValue: number, currentIndex: number, array: this) => number, initialValue?: number): number;
-		reduceRight<U>(callbackfn: (previousValue: U, currentValue: number, currentIndex: number, array: this) => U, initialValue: U): U;
-		some(callbackfn: (value: number, index: number, array: this) => boolean, thisArg?: any): boolean;
-
-		toLocaleString(): string;
-		toString(): string;
-
-		// es2015.iterable extensions
-		[Symbol.iterator](): IterableIterator<number>;
-		entries(): IterableIterator<[number, number]>;
-		keys(): IterableIterator<number>;
-		values(): IterableIterator<number>;
-
-		// es2016.includes extensions
-		[Symbol.toStringTag]: any;
-		includes(searchElement: number, fromIndex?: number): boolean;
-	}
-
-	export interface TypedArray extends TypedArrayBase {
-		[index: number]: number;
-
-		set(arrayOrElements: ArrayLike<number>, offset?: number): void;
-
-		copyWithin(target: number, start: number, end?: number): this;
-		fill(value: number, start?: number, end?: number): this;
-
-		reverse(): this;
-		sort(compareFn?: (a: number, b: number) => number): this;
-	}
-
-	export interface ReadonlyTypedArray extends TypedArrayBase {
-		readonly [index: number]: number;
-	}
-
-	export interface TypedArrayConstructor {
-		new (lengthOrSource: number | Iterable<number> | ArrayLike<number>): TypedArray;
-		new (buffer: ArrayBuffer | SharedArrayBuffer, byteOffset?: number, length?: number): TypedArray;
 	// helper types for enums stored in int arrays
 	export interface ConstEnumArray8View<T extends number> extends Uint8Array {
 		[index: number]: T;
@@ -101,7 +48,6 @@ namespace sd {
 	export interface ConstEnumArray32View<T extends number> extends Int32Array {
 		[index: number]: T;
 	}
-
 
 	// special purpose or generic array interfaces used very frequently
 	export interface MutableArrayLike<T> {
