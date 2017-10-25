4924dde39a96c5ef2d1547b8c31c0d1c831634d5
Arthur Langereis <puurtuur@me.com>
2017-10-25 19:31:37 +0200

    ref(asset): convert image-builtin to importer

---
+++ src/asset/importers/image-builtin.ts
@@ -0,0 +1,83 @@
+// asset/importer/image-builtin - Browser built-in image importer
+// Part of Stardazed
+// (c) 2015-2017 by Arthur Langereis - @zenmumbler
+// https://github.com/stardazed/stardazed
+
+/// <reference path="../importer.ts" />
+
+namespace sd.asset.importer {
+
+	function importBuiltInImage(data: Blob, _uri: string) {
+		const blobURL = URL.createObjectURL(data);
+
+		return new Promise<image.PixelDataProvider>((resolve, reject) => {
+			const builtin = new Image();
+			builtin.onload = () => {
+				resolve(new HTMLImageDataProvider(builtin));
+			};
+			builtin.onerror = () => {
+				reject(`The image at '${blobURL}' is not supported`);
+			};
+
+			// Always enable CORS as GL will not allow tainted data to be loaded so if it fails
+			// we can't use the image anyway and enabling it for local resources does no harm.
+			builtin.crossOrigin = "anonymous";
+			builtin.src = blobURL;
+		}).then(provider => {
+			URL.revokeObjectURL(blobURL);
+			return {
+				image: {
+					kind: "image",
+					item: provider
+				}
+			};
+		});
+	}
+
+	registerImporter(importBuiltInImage, "image/bmp", ["bm", "bmp"]);
+	registerImporter(importBuiltInImage, "image/jpeg", ["jpg", "jpe", "jpeg"]);
+	registerImporter(importBuiltInImage, "image/png", "png");
+
+
+	export class HTMLImageDataProvider implements image.PixelDataProvider {
+		private colourSpace_: image.ColourSpace;
+		private pixelFormat_: image.PixelFormat;
+		readonly dim: image.PixelDimensions;
+		readonly mipMapCount = 1;
+
+		constructor(private image_: HTMLImageElement) {
+			this.colourSpace = image.ColourSpace.sRGB;
+			this.dim = image.makePixelDimensions(image_.width, image_.height);
+		}
+
+		get colourSpace() {
+			return this.colourSpace_;
+		}
+
+		set colourSpace(ncs: image.ColourSpace) {
+			if (this.colourSpace_ !== ncs) {
+				this.colourSpace_ = ncs;
+				this.pixelFormat_ = (ncs === image.ColourSpace.sRGB) ? image.PixelFormat.SRGB8_Alpha8 : image.PixelFormat.RGBA8;	
+			}
+		}
+
+		get pixelFormat() {
+			return this.pixelFormat_;
+		}
+
+
+		pixelBufferForLevel(level: number): image.PixelBuffer | undefined {
+			if (level !== 0) {
+				return undefined;
+			}
+
+			return {
+				colourSpace: this.colourSpace,
+				pixelFormat: this.pixelFormat,
+				dim: { ...this.dim },
+				data: this.image_
+			};
+		}
+	}
+
+} // ns sd.asset.importer
