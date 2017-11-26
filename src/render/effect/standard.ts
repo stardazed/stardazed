5c8141d0594e5614a1e2166382eca7647a8f8538
Arthur Langereis <puurtuur@me.com>
2017-11-26 16:01:08 +0100

    wip(render): disable fog for now

--- src/render/effect/standard.ts
+++ src/render/effect/standard.ts
@@ -68,7 +68,7 @@ namespace sd.render.effect {
 			vec3 totalLight = totalDynamicLightContributionTiledForward(si, mi);
 			totalLight += vec3(0.015, 0.01, 0.02);
 
-			totalLight = applyDepthFog(totalLight * mi.albedo.rgb, length(vertexPos_cam));
+			// totalLight = applyDepthFog(totalLight * mi.albedo.rgb, length(vertexPos_cam));
 
 			gl_FragColor = vec4(linearToSRGB(totalLight), 1.0);
 			`
