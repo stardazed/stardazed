a5348926abb12f3020e4196dd4bf5791de7a50c5
Arthur Langereis <puurtuur@me.com>
2018-01-02 15:44:16 -0700

    ref(control): add input file
    device aggregate input

---
+++ src/control/input.ts
@@ -0,0 +1,9 @@
+// control/input - multi-device input mapping and reading
+// Part of Stardazed
+// (c) 2015-2017 by Arthur Langereis - @zenmumbler
+// https://github.com/stardazed/stardazed
+
+namespace sd.control {
+
+
+} // ns sd.control
