8965fdfe71fbd97acd9f4da51d6decf684233450
Arthur Langereis <puurtuur@me.com>
2018-01-13 12:16:31 +0100

    wip(control): some axis defs

--- src/control/input.ts
+++ src/control/input.ts
@@ -10,5 +10,64 @@ namespace sd.control {
 		halfTransitionCount: number;
 	}
 
+	export const enum InputDeviceType {
+		Keyboard,
+		Mouse,
+		Controller
+	}
+
+	export interface KeyboardInput {
+		type: InputDeviceType.Keyboard;
+		key: Key;
+	}
+
+	export const enum MouseControl {
+		Button0,
+		Button1,
+		Button2,
+		Button3,
+		Button4,
+		Button5,
+		Button6,
+		Button7,
+
+		Button8,
+		Button9,
+		Button10,
+		Button11,
+		Button12,
+		Button13,
+		Button14,
+		Button15,
+
+		MotionHorizontal,
+		MotionVertical,
+
+		WheelHorizontal,
+		WheelVertical
+	}
+
+	export interface MouseInput {
+		type: InputDeviceType.Mouse;
+		axis: MouseControl;
+	}
+
+	export type DeviceInput = KeyboardInput | MouseInput;
+
+	export interface AxisDescriptor {
+		enabled: boolean;
+		invert: boolean;
+
+		sensitivity: number;
+		gravity: number;
+		snap: boolean;
+
+		positive: DeviceInput;
+		negative: DeviceInput;
+	}
+
+	export interface Input {
+		
+	}
 
 } // ns sd.control
