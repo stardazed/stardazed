f2c3198405af9260cde6bff9fbdda24e8e61647c
Arthur Langereis <puurtuur@me.com>
2018-05-27 18:47:30 +0200

    ref(asset): skeleton asset type

--- packages/sd-asset/src/asset.ts
+++ packages/sd-asset/src/asset.ts
@@ -5,6 +5,18 @@
  * https://github.com/stardazed/stardazed
  */
 
-export default function() {
-
+export interface Asset<AssetItem = any> {
+	/**
+	 * A string indicating the type of the asset.
+	 * Should be a single, lowercase word, e.g.: mesh, model, texture
+	 */
+	kind?: string;
+	/**
+	 * A descriptive label for this asset.
+	 */
+	name?: string;
+	/**
+	 * The generated or fully processed asset data.
+	 */
+	item?: AssetItem;
 }
