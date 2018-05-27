b3693e41d2a68c4851ea2d001eba298072aaccb1
Arthur Langereis <puurtuur@me.com>
2018-05-27 19:35:53 +0200

    chore(packages): use sd npm org - asset

--- packages/asset/src/index.ts
+++ packages/asset/src/index.ts
@@ -1,5 +1,5 @@
 /**
- * sd-asset - asset management core
+ * @stardazed/asset - asset management
  * Part of Stardazed
  * (c) 2015-Present by Arthur Langereis - @zenmumbler
  * https://github.com/stardazed/stardazed
