2742b50f067e79e824f7adc680a87e67978a6681
Arthur Langereis <puurtuur@me.com>
2018-05-27 19:33:38 +0200

    chore(packages): use sd npm org - image

--- packages/image/src/index.ts
+++ packages/image/src/index.ts
@@ -1,5 +1,5 @@
 /**
- * sd-image - image (meta)data streaming and parsing
+ * @stardazed/image - image (meta)data representation
  * Part of Stardazed
  * (c) 2015-Present by Arthur Langereis - @zenmumbler
  * https://github.com/stardazed/stardazed
