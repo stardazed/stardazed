733c560ee9c05543888d39bc835173412d4c233a
Arthur Langereis <puurtuur@me.com>
2018-05-29 22:59:41 +0200

    ref(geometry-data): move view helpers to index

--- packages/geometry-data/src/triangle-view.ts
+++ packages/geometry-data/src/triangle-view.ts
@@ -43,41 +43,4 @@ export interface TriangleView {
 }
 
 
-export function makeTriangleViewForIndexBuffer(ib: IndexBuffer): TriangleView {
-	return new IndexBufferTriangleView(ib);
-}
-
-export function makeTriangleViewForGeometry(geom: Geometry): TriangleView | undefined {
-	const allTrianglePrimitives = geom.subMeshes.every(sm => sm.type === PrimitiveType.Triangle);
-	if (! allTrianglePrimitives) {
-		console.warn("triangleViewForMesh, cannot create TriangleView as not all submeshes are of Triangle type", geom);
-		return undefined;
-	}
-
-	if (geom.indexBuffer) {
-		return new IndexBufferTriangleView(geom.indexBuffer);
-	}
-
-	const elementCount = geom.subMeshes.map(sm => sm.elementCount).reduce((sum, count) => sum + count, 0);
-	return new DirectTriangleView(elementCount);
-}
-
-
-export function makeTriangleViewForSubMesh(geom: Geometry, subMeshIndex: number): TriangleView | undefined {
-	const subMesh = geom.subMeshes[subMeshIndex];
-	if (! subMesh) {
-		console.warn("triangleViewForSubMesh, invalid submesh index", geom, subMeshIndex);
-		return undefined;
-	}
-	if (subMesh.type !== PrimitiveType.Triangle) {
-		console.warn("triangleViewForSubMesh, incompatible submesh type", geom, subMeshIndex);
-		return undefined;
-	}
-	const fromTriangle = (subMesh.fromElement / 3) | 0;
-	const toTriangle = ((subMesh.elementCount / 3) | 0) + fromTriangle;
-
-	if (geom.indexBuffer) {
-		return new IndexBufferTriangleView(geom.indexBuffer, fromTriangle, toTriangle);
-	}
-	return new DirectTriangleView(subMesh.elementCount, fromTriangle, toTriangle);
 }
