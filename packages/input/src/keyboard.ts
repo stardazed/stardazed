1186e2bb239aff22a304f7a9025780f3fd67eeae
Arthur Langereis <puurtuur@me.com>
2018-06-16 19:07:12 +0200

    ref(input): complete basic package conversion

--- packages/input/src/keyboard.ts
+++ packages/input/src/keyboard.ts
@@ -1,137 +1,139 @@
-// control/keyboard - keyboard input handling
-// Part of Stardazed
-// (c) 2015-2018 by Arthur Langereis - @zenmumbler
-// https://github.com/stardazed/stardazed
+/**
+ * input/keyboard - keyboard input handling
+ * Part of Stardazed
+ * (c) 2015-Present by Arthur Langereis - @zenmumbler
+ * https://github.com/stardazed/stardazed
+ */
 
-namespace sd.control {
+import { UInt8, Double } from "@stardazed/core";
+import { FixedMultiArray, MABField, fill } from "@stardazed/container";
+import { ButtonState } from "./common";
 
-	export enum Key {
-		NONE = 0,
+export enum Key {
+	NONE = 0,
 
-		UP = 38,
-		DOWN = 40,
-		LEFT = 37,
-		RIGHT = 39,
+	UP = 38,
+	DOWN = 40,
+	LEFT = 37,
+	RIGHT = 39,
 
-		BACKSPACE = 8,
-		TAB = 9,
-		RETURN = 13,
-		ESC = 27,
-		SPACE = 32,
+	BACKSPACE = 8,
+	TAB = 9,
+	RETURN = 13,
+	ESC = 27,
+	SPACE = 32,
 
-		PAGEUP = 33,
-		PAGEDOWN = 34,
-		HOME = 36,
-		END = 35,
-		DELETE = 46,
+	PAGEUP = 33,
+	PAGEDOWN = 34,
+	HOME = 36,
+	END = 35,
+	DELETE = 46,
 
-		// charCode equals keyCode for A-Z
-		A = "A".charCodeAt(0), B = "B".charCodeAt(0), C = "C".charCodeAt(0), D = "D".charCodeAt(0),
-		E = "E".charCodeAt(0), F = "F".charCodeAt(0), G = "G".charCodeAt(0), H = "H".charCodeAt(0),
-		I = "I".charCodeAt(0), J = "J".charCodeAt(0), K = "K".charCodeAt(0), L = "L".charCodeAt(0),
-		M = "M".charCodeAt(0), N = "N".charCodeAt(0), O = "O".charCodeAt(0), P = "P".charCodeAt(0),
-		Q = "Q".charCodeAt(0), R = "R".charCodeAt(0), S = "S".charCodeAt(0), T = "T".charCodeAt(0),
-		U = "U".charCodeAt(0), V = "V".charCodeAt(0), W = "W".charCodeAt(0), X = "X".charCodeAt(0),
-		Y = "Y".charCodeAt(0), Z = "Z".charCodeAt(0)
-	}
-
-
-	export interface Keyboard {
-		keyState(kc: Key): ButtonState;
-		down(kc: Key): boolean;
-		pressed(kc: Key): boolean;
-		released(kc: Key): boolean;
-		halfTransitions(kc: Key): number;
-
-		reset(): void;
-		resetPerFrameData(): void;
-	}
+	// charCode equals keyCode for A-Z
+	A = "A".charCodeAt(0), B = "B".charCodeAt(0), C = "C".charCodeAt(0), D = "D".charCodeAt(0),
+	E = "E".charCodeAt(0), F = "F".charCodeAt(0), G = "G".charCodeAt(0), H = "H".charCodeAt(0),
+	I = "I".charCodeAt(0), J = "J".charCodeAt(0), K = "K".charCodeAt(0), L = "L".charCodeAt(0),
+	M = "M".charCodeAt(0), N = "N".charCodeAt(0), O = "O".charCodeAt(0), P = "P".charCodeAt(0),
+	Q = "Q".charCodeAt(0), R = "R".charCodeAt(0), S = "S".charCodeAt(0), T = "T".charCodeAt(0),
+	U = "U".charCodeAt(0), V = "V".charCodeAt(0), W = "W".charCodeAt(0), X = "X".charCodeAt(0),
+	Y = "Y".charCodeAt(0), Z = "Z".charCodeAt(0),
+}
 
 
-	class KeyboardImpl implements Keyboard {
-		private keyData_: container.FixedMultiArray;
-		private downBase_: Uint8Array;
-		private halfTransBase_: Uint8Array;
-		private lastEventBase_: Float64Array;
+export interface Keyboard {
+	keyState(kc: Key): ButtonState;
+	down(kc: Key): boolean;
+	pressed(kc: Key): boolean;
+	released(kc: Key): boolean;
+	halfTransitions(kc: Key): number;
 
-		constructor() {
-			const fields: container.MABField[] = [
-				{ type: UInt8, count: 1 },  // down
-				{ type: UInt8, count: 1 },  // halfTransitionCount
-				{ type: Double, count: 1 }, // lastEvent
-			];
-			this.keyData_ = new container.FixedMultiArray(128, fields);
-			this.downBase_ = this.keyData_.indexedFieldView(0) as Uint8Array;
-			this.halfTransBase_ = this.keyData_.indexedFieldView(1) as Uint8Array;
-			this.lastEventBase_ = this.keyData_.indexedFieldView(2) as Float64Array;
+	reset(): void;
+	resetPerFrameData(): void;
+}
 
-			// The extra check in the key handlers for the timeStamp was added
-			// after I encountered a rare, but frequently enough occuring bug
-			// where, when a key is pressed for a longer time so that repeat
-			// keydown events are fired, _very_ occasionally the last keydown
-			// would be fired with the same timeStamp as the keyup event but
-			// the event handler for that last down event was fired AFTER the
-			// keyup event handler, causing the key to appear to be "stuck".
-			window.addEventListener("keydown", evt => {
-				const lastEvent = this.lastEventBase_[evt.keyCode];
-				const wasDown = this.downBase_[evt.keyCode];
 
-				if (lastEvent < evt.timeStamp) {
-					if (! wasDown) { // ignore key repeat events
-						this.downBase_[evt.keyCode] = 1;
-						++this.halfTransBase_[evt.keyCode];
-					}
-					this.lastEventBase_[evt.keyCode] = evt.timeStamp;
+class KeyboardImpl implements Keyboard {
+	private keyData_: FixedMultiArray;
+	private downBase_: Uint8Array;
+	private halfTransBase_: Uint8Array;
+	private lastEventBase_: Float64Array;
+
+	constructor() {
+		const fields: MABField[] = [
+			{ type: UInt8, count: 1 },  // down
+			{ type: UInt8, count: 1 },  // halfTransitionCount
+			{ type: Double, count: 1 }, // lastEvent
+		];
+		this.keyData_ = new FixedMultiArray(128, fields);
+		this.downBase_ = this.keyData_.indexedFieldView(0) as Uint8Array;
+		this.halfTransBase_ = this.keyData_.indexedFieldView(1) as Uint8Array;
+		this.lastEventBase_ = this.keyData_.indexedFieldView(2) as Float64Array;
+
+		// The extra check in the key handlers for the timeStamp was added
+		// after I encountered a rare, but frequently enough occuring bug
+		// where, when a key is pressed for a longer time so that repeat
+		// keydown events are fired, _very_ occasionally the last keydown
+		// would be fired with the same timeStamp as the keyup event but
+		// the event handler for that last down event was fired AFTER the
+		// keyup event handler, causing the key to appear to be "stuck".
+		window.addEventListener("keydown", evt => {
+			const lastEvent = this.lastEventBase_[evt.keyCode];
+			const wasDown = this.downBase_[evt.keyCode];
+
+			if (lastEvent < evt.timeStamp) {
+				if (! wasDown) { // ignore key repeat events
+					this.downBase_[evt.keyCode] = 1;
+					++this.halfTransBase_[evt.keyCode];
 				}
-
-				if (! evt.metaKey) {
-					evt.preventDefault();
-				}
-			}, true);
-
-			window.addEventListener("keyup", evt => {
-				this.downBase_[evt.keyCode] = 0;
-				++this.halfTransBase_[evt.keyCode];
 				this.lastEventBase_[evt.keyCode] = evt.timeStamp;
+			}
 
+			if (! evt.metaKey) {
 				evt.preventDefault();
-			}, true);
-		}
+			}
+		}, true);
 
-		keyState(kc: Key): ButtonState {
-			return {
-				down: !!this.downBase_[kc],
-				halfTransitionCount: this.halfTransBase_[kc]
-			};
-		}
+		window.addEventListener("keyup", evt => {
+			this.downBase_[evt.keyCode] = 0;
+			++this.halfTransBase_[evt.keyCode];
+			this.lastEventBase_[evt.keyCode] = evt.timeStamp;
 
-		down(kc: Key): boolean {
-			return !!this.downBase_[kc];
-		}
-
-		halfTransitions(kc: Key): number {
-			return this.halfTransBase_[kc];
-		}
-
-		pressed(kc: Key): boolean {
-			return this.downBase_[kc] ? (this.halfTransBase_[kc] > 0) : false;
-		}
-
-		released(kc: Key): boolean {
-			return !this.downBase_[kc] ? (this.halfTransBase_[kc] > 0) : false;
-		}
-
-		reset() {
-			this.keyData_.clear();
-		}
-
-		resetPerFrameData() {
-			container.fill(this.halfTransBase_, 0, this.halfTransBase_.length);
-		}
+			evt.preventDefault();
+		}, true);
 	}
 
+	keyState(kc: Key): ButtonState {
+		return {
+			down: !!this.downBase_[kc],
+			halfTransitionCount: this.halfTransBase_[kc]
+		};
+	}
 
-	// -- exported devices
-	export const keyboard: Keyboard = new KeyboardImpl();
+	down(kc: Key): boolean {
+		return !!this.downBase_[kc];
+	}
 
-} // ns sd.control
+	halfTransitions(kc: Key): number {
+		return this.halfTransBase_[kc];
+	}
+
+	pressed(kc: Key): boolean {
+		return this.downBase_[kc] ? (this.halfTransBase_[kc] > 0) : false;
+	}
+
+	released(kc: Key): boolean {
+		return !this.downBase_[kc] ? (this.halfTransBase_[kc] > 0) : false;
+	}
+
+	reset() {
+		this.keyData_.clear();
+	}
+
+	resetPerFrameData() {
+		fill(this.halfTransBase_, 0, this.halfTransBase_.length);
+	}
+}
+
+
+// -- exported devices
+export const keyboard: Keyboard = new KeyboardImpl();
--- packages/input/src/mouse.ts
+++ packages/input/src/mouse.ts
@@ -1,155 +1,185 @@
-// control/mouse - mouse input and capture handling
-// Part of Stardazed
-// (c) 2015-2018 by Arthur Langereis - @zenmumbler
-// https://github.com/stardazed/stardazed
+/**
+ * input/mouse - mouse input and capture handling
+ * Part of Stardazed
+ * (c) 2015-Present by Arthur Langereis - @zenmumbler
+ * https://github.com/stardazed/stardazed
+ */
 
-namespace sd.control {
+import { UInt8, Float2 } from "@stardazed/core";
+import { FixedMultiArray, MABField, fill } from "@stardazed/container";
+import { vec2 } from "@stardazed/math";
+import { ButtonState } from "./common";
 
-	export interface Mouse {
-		buttonState(index: number): ButtonState;
-		down(index: number): boolean;
-		pressed(index: number): boolean;
-		released(index: number): boolean;
-		halfTransitions(index: number): number;
+// not used yet
+export const enum MouseControl {
+	Button0,
+	Button1,
+	Button2,
+	Button3,
+	Button4,
+	Button5,
+	Button6,
+	Button7,
 
-		reset(): void;
-		resetPerFrameData(): void;
+	Button8,
+	Button9,
+	Button10,
+	Button11,
+	Button12,
+	Button13,
+	Button14,
+	Button15,
 
-		lock(): void;
-		unlock(): void;
+	MotionHorizontal,
+	MotionVertical,
 
-		readonly position: Float2;
-		readonly positionDelta: Float2;
-		readonly wheelDelta: Float2;
+	WheelHorizontal,
+	WheelVertical
+}
+
+export interface Mouse {
+	buttonState(index: number): ButtonState;
+	down(index: number): boolean;
+	pressed(index: number): boolean;
+	released(index: number): boolean;
+	halfTransitions(index: number): number;
+
+	reset(): void;
+	resetPerFrameData(): void;
+
+	lock(): void;
+	unlock(): void;
+
+	readonly position: Float2;
+	readonly positionDelta: Float2;
+	readonly wheelDelta: Float2;
+}
+
+class MouseImpl implements Mouse {
+	private buttonData_: FixedMultiArray;
+	private downBase_: Uint8Array;
+	private halfTransBase_: Uint8Array;
+	private readonly position_: number[];
+	private readonly positionDelta_: number[];
+	private readonly wheelDelta_: number[];
+
+	constructor() {
+		const fields: MABField[] = [
+			{ type: UInt8, count: 1 },  // down
+			{ type: UInt8, count: 1 },  // halfTransitionCount
+		];
+		this.buttonData_ = new FixedMultiArray(16, fields);
+		this.downBase_ = this.buttonData_.indexedFieldView(0) as Uint8Array;
+		this.halfTransBase_ = this.buttonData_.indexedFieldView(1) as Uint8Array;
+
+		this.position_ = [0, 0];
+		this.positionDelta_ = [0, 0];
+		this.wheelDelta_ = [0, 0];
+
+		window.addEventListener("wheel", evt => {
+			this.wheelDelta_[0] = evt.deltaX;
+			this.wheelDelta_[1] = evt.deltaY;
+			evt.preventDefault();
+		}, true);
+	
+		window.addEventListener("mousedown", evt => {
+			this.downBase_[evt.button] = 1;
+			++this.halfTransBase_[evt.button];
+			evt.preventDefault();
+		}, true);
+	
+		window.addEventListener("mouseup", evt => {
+			this.downBase_[evt.button] = 0;
+			++this.halfTransBase_[evt.button];
+			evt.preventDefault();
+		}, true);
+	
+		window.addEventListener("contextmenu", evt => {
+			// disable context menu to allow for 2nd-clicks to
+			// be handled by the game.
+			evt.preventDefault();
+		}, true);
+
+		window.addEventListener("mousemove", evt => {
+			if (document.pointerLockElement) {
+				this.positionDelta_[0] = evt.movementX;
+				this.positionDelta_[1] = evt.movementY;
+			}
+			else {
+				this.positionDelta_[0] = evt.clientX - this.position_[0];
+				this.positionDelta_[1] = evt.clientY - this.position_[1];
+				this.position_[0] = evt.clientX;
+				this.position_[1] = evt.clientY;
+			}
+		}, true);
+
+		document.addEventListener("pointerlockchange", _evt => {
+			// integrate into mouse capturing logic
+		});
+
+		document.addEventListener("pointerlockerror", _evt => {
+			// integrate into mouse capturing logic
+		});
 	}
 
-	class MouseImpl implements Mouse {
-		private buttonData_: container.FixedMultiArray;
-		private downBase_: Uint8Array;
-		private halfTransBase_: Uint8Array;
-		private readonly position_: number[];
-		private readonly positionDelta_: number[];
-		private readonly wheelDelta_: number[];
-
-		constructor() {
-			const fields: container.MABField[] = [
-				{ type: UInt8, count: 1 },  // down
-				{ type: UInt8, count: 1 },  // halfTransitionCount
-			];
-			this.buttonData_ = new container.FixedMultiArray(16, fields);
-			this.downBase_ = this.buttonData_.indexedFieldView(0) as Uint8Array;
-			this.halfTransBase_ = this.buttonData_.indexedFieldView(1) as Uint8Array;
-
-			this.position_ = [0, 0];
-			this.positionDelta_ = [0, 0];
-			this.wheelDelta_ = [0, 0];
-
-			window.addEventListener("wheel", evt => {
-				this.wheelDelta_[0] = evt.deltaX;
-				this.wheelDelta_[1] = evt.deltaY;
-				evt.preventDefault();
-			}, true);
-		
-			window.addEventListener("mousedown", evt => {
-				this.downBase_[evt.button] = 1;
-				++this.halfTransBase_[evt.button];
-				evt.preventDefault();
-			}, true);
-		
-			window.addEventListener("mouseup", evt => {
-				this.downBase_[evt.button] = 0;
-				++this.halfTransBase_[evt.button];
-				evt.preventDefault();
-			}, true);
-		
-			window.addEventListener("contextmenu", evt => {
-				// disable context menu to allow for 2nd-clicks to
-				// be handled by the game.
-				evt.preventDefault();
-			}, true);
-
-			window.addEventListener("mousemove", evt => {
-				if (document.pointerLockElement) {
-					this.positionDelta_[0] = evt.movementX;
-					this.positionDelta_[1] = evt.movementY;
-				}
-				else {
-					this.positionDelta_[0] = evt.clientX - this.position_[0];
-					this.positionDelta_[1] = evt.clientY - this.position_[1];
-					this.position_[0] = evt.clientX;
-					this.position_[1] = evt.clientY;
-				}
-			}, true);
-
-			document.addEventListener("pointerlockchange", _evt => {
-				// integrate into mouse capturing logic
-			});
-
-			document.addEventListener("pointerlockerror", _evt => {
-				// integrate into mouse capturing logic
-			});
-		}
-
-		buttonState(index: number): ButtonState {
-			return {
-				down: !!this.downBase_[index],
-				halfTransitionCount: this.halfTransBase_[index]
-			};
-		}
-
-		down(index: number) {
-			return !!this.downBase_[index];
-		}
-
-		pressed(index: number) {
-			return this.downBase_[index] ? (this.halfTransBase_[index] > 0) : false;
-		}
-
-		released(index: number): boolean {
-			return !this.downBase_[index] ? (this.halfTransBase_[index] > 0) : false;
-		}
-
-		halfTransitions(index: number) {
-			return this.halfTransBase_[index];
-		}
-
-		reset() {
-			this.buttonData_.clear();
-			vec2.set(this.position_, 0, 0);
-			vec2.set(this.positionDelta_, 0, 0);
-			vec2.set(this.wheelDelta_, 0, 0);
-		}
-
-		resetPerFrameData() {
-			container.fill(this.halfTransBase_, 0, this.halfTransBase_.length);
-			this.positionDelta_[0] = 0;
-			this.positionDelta_[1] = 0;
-			this.wheelDelta_[0] = 0;
-			this.wheelDelta_[1] = 0;
-		}
-
-		lock() {
-			document.querySelector("canvas")!.requestPointerLock();
-		}
-
-		unlock() {
-			document.exitPointerLock();
-		}
-
-		get position() {
-			return this.position_.slice();
-		}
-
-		get positionDelta() {
-			return this.positionDelta_.slice();
-		}
-
-		get wheelDelta() {
-			return this.wheelDelta_.slice();
-		}
+	buttonState(index: number): ButtonState {
+		return {
+			down: !!this.downBase_[index],
+			halfTransitionCount: this.halfTransBase_[index]
+		};
 	}
 
-	// -- exported devices
-	export const mouse: Mouse = new MouseImpl();
+	down(index: number) {
+		return !!this.downBase_[index];
+	}
 
-} // ns sd.control
+	pressed(index: number) {
+		return this.downBase_[index] ? (this.halfTransBase_[index] > 0) : false;
+	}
+
+	released(index: number): boolean {
+		return !this.downBase_[index] ? (this.halfTransBase_[index] > 0) : false;
+	}
+
+	halfTransitions(index: number) {
+		return this.halfTransBase_[index];
+	}
+
+	reset() {
+		this.buttonData_.clear();
+		vec2.set(this.position_, 0, 0);
+		vec2.set(this.positionDelta_, 0, 0);
+		vec2.set(this.wheelDelta_, 0, 0);
+	}
+
+	resetPerFrameData() {
+		fill(this.halfTransBase_, 0, this.halfTransBase_.length);
+		this.positionDelta_[0] = 0;
+		this.positionDelta_[1] = 0;
+		this.wheelDelta_[0] = 0;
+		this.wheelDelta_[1] = 0;
+	}
+
+	lock() {
+		document.querySelector("canvas")!.requestPointerLock();
+	}
+
+	unlock() {
+		document.exitPointerLock();
+	}
+
+	get position() {
+		return this.position_.slice();
+	}
+
+	get positionDelta() {
+		return this.positionDelta_.slice();
+	}
+
+	get wheelDelta() {
+		return this.wheelDelta_.slice();
+	}
+}
+
+// -- exported devices
+export const mouse: Mouse = new MouseImpl();
