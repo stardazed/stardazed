46f166651227b5091f304a0f9ea9dc8e9f636096
Arthur Langereis <puurtuur@me.com>
2018-07-13 00:35:17 +0200

    feat(numeric): add integer trait

--- packages/core/src/numeric.ts
+++ packages/core/src/numeric.ts
@@ -15,6 +15,7 @@ export interface NumericType {
 	readonly min: number;
 	readonly max: number;
 	readonly signed: boolean;
+	readonly integer: boolean;
 
 	readonly byteSize: number;
 	readonly arrayType: TypedArrayConstructor;
@@ -27,6 +28,7 @@ export const UInt8: NumericType = {
 	min: 0,
 	max: 255,
 	signed: false,
+	integer: true,
 	byteSize: 1,
 	arrayType: Uint8Array
 };
@@ -38,6 +40,7 @@ export const UInt8Clamped: NumericType = {
 	min: 0,
 	max: 255,
 	signed: false,
+	integer: true,
 	byteSize: 1,
 	arrayType: Uint8ClampedArray
 };
@@ -49,6 +52,7 @@ export const SInt8: NumericType = {
 	min: -128,
 	max: 127,
 	signed: true,
+	integer: true,
 	byteSize: 1,
 	arrayType: Int8Array
 };
@@ -60,6 +64,7 @@ export const UInt16: NumericType = {
 	min: 0,
 	max: 65535,
 	signed: false,
+	integer: true,
 	byteSize: 2,
 	arrayType: Uint16Array
 };
@@ -71,6 +76,7 @@ export const SInt16: NumericType = {
 	min: -32768,
 	max: 32767,
 	signed: true,
+	integer: true,
 	byteSize: 2,
 	arrayType: Int16Array
 };
@@ -82,6 +88,7 @@ export const UInt32: NumericType = {
 	min: 0,
 	max: 4294967295,
 	signed: false,
+	integer: true,
 	byteSize: 4,
 	arrayType: Uint32Array
 };
@@ -93,6 +100,7 @@ export const SInt32: NumericType = {
 	min: -2147483648,
 	max: 2147483647,
 	signed: true,
+	integer: true,
 	byteSize: 4,
 	arrayType: Int32Array
 };
@@ -104,6 +112,7 @@ export const Float: NumericType = {
 	min: -340282346638528859811704183484516925440.0,
 	max: 340282346638528859811704183484516925440.0,
 	signed: true,
+	integer: false,
 	byteSize: 4,
 	arrayType: Float32Array
 };
@@ -115,6 +124,7 @@ export const Double: NumericType = {
 	min: -179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.0,
 	max: 179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.0,
 	signed: true,
+	integer: false,
 	byteSize: 8,
 	arrayType: Float64Array
 };
