01f744e91908992958386cddf31077ec0c6fc3bd
Arthur Langereis <puurtuur@me.com>
2018-07-27 23:03:00 +0200

    chore(packages): create package core/debug

--- core/debug/src/debug.ts
+++ core/debug/src/debug.ts
@@ -1,5 +1,5 @@
 /**
- * debug/assert - assertions
+ * @stardazed/debug - debugging helpers and tools
  * Part of Stardazed
  * (c) 2015-Present by Arthur Langereis - @zenmumbler
  * https://github.com/stardazed/stardazed
--- packages/debug/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @stardazed/debug - debugging helpers and tools for Stardazed
- * Part of Stardazed
- * (c) 2015-Present by Arthur Langereis - @zenmumbler
- * https://github.com/stardazed/stardazed
- */
-
-export * from "./assert";
