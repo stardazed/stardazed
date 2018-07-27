93b915e5598ce99d35221707484de05936cbc9d4
Arthur Langereis <puurtuur@me.com>
2018-07-27 23:03:44 +0200

    chore(packages): create package core/math

--- core/math/src/math.ts
+++ core/math/src/math.ts
@@ -1,16 +1,10 @@
 /**
- * math/common - shared elements
+ * @stardazed/math - mathematical helper functions
  * Part of Stardazed
  * (c) 2015-Present by Arthur Langereis - @zenmumbler
  * https://github.com/stardazed/stardazed
  */
 
-import { NumArray, MutNumArray } from "@stardazed/core";
-
-// constants
-export const EPSILON = 0.000001;
-
-// functions
 export function clamp(n: number, min: number, max: number): number {
 	return Math.max(min, Math.min(max, n));
 }
@@ -48,9 +42,10 @@ export function isPowerOf2(n: number) {
 	return (n & (n - 1)) === 0;
 }
 
-// roundUpPowerOf2
-// return closest powerOf2 number that is >= n
-// e.g.: 15 -> 16; 16 -> 16; 17 -> 32
+/**
+ * Return closest powerOf2 number that is >= n
+ * @example 15 -> 16; 16 -> 16; 17 -> 32
+ */
 export function roundUpPowerOf2(n: number) {
 	if (n <= 0) { return 1; }
 	n = (n | 0) - 1;
@@ -62,23 +57,22 @@ export function roundUpPowerOf2(n: number) {
 	return n + 1;
 }
 
-// alignUp
-// round val up to closest alignmentPow2
+/**
+ * Round val up to closest multiple of alignmentPow2
+ * @param val number to align up
+ * @param alignmentPow2 power-of-2 alignment border that val will be rounded up towards
+ * @expects {audit} isPowerOf2(alignmentPow2)
+ */
 export function alignUp(val: number, alignmentPow2: number) {
 	return (val + alignmentPow2 - 1) & (~(alignmentPow2 - 1));
 }
 
-// alignDown
-// round val down to closest alignmentPow2
+/**
+ * Round val down to closest multiple of alignmentPow2
+ * @param val number to align up
+ * @param alignmentPow2 power-of-2 alignment border that val will be rounded down towards
+ * @expects {audit} isPowerOf2(alignmentPow2)
+ */
 export function alignDown(val: number, alignmentPow2: number) {
 	return val & (~(alignmentPow2 - 1));
 }
-
-// (strided) iteration of vecN types
-export interface VecArrayIterationOptions {
-	stride?: number;
-	offset?: number;
-	count?: number;
-}
-
-export type VecArrayIterationFunction = (out: MutNumArray, a: NumArray, ...args: any[]) => void;
