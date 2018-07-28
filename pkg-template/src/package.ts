eeabeedd85cedea35ebcae6f17b0a6183dd5bc82
Arthur Langereis <puurtuur@me.com>
2018-07-28 17:27:53 +0200

    chore(packages): template package dir
    to facility easy creation of new packages

---
+++ pkg-template/src/package.ts
@@ -0,0 +1,13 @@
+/**
+ * @stardazed/package - description
+ * Part of Stardazed
+ * (c) 2015-Present by Arthur Langereis - @zenmumbler
+ * https://github.com/stardazed/stardazed
+ */
+
+/**
+ * Does something useful
+ */
+export function stuff() {
+	console.info("Yay!");
+}
