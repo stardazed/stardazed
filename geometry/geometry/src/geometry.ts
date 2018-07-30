53102cff20fbd64c90e628e06ede27d41fea116a
Arthur Langereis <puurtuur@me.com>
2018-07-30 18:36:22 +0200

    feat(packages): add geometry/geometry package

---
+++ geometry/geometry/src/geometry.ts
@@ -0,0 +1,10 @@
+/**
+ * @stardazed/geometry - Geometry compound type
+ * Part of Stardazed
+ * (c) 2015-Present by Arthur Langereis - @zenmumbler
+ * https://github.com/stardazed/stardazed
+ */
+
+export * from "./types";
+export * from "./layout";
+export { allocateGeometry } from "./allocate";
