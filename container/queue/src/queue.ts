5a238a04f0f451a803a40e9714d345575f16d2b1
Arthur Langereis <puurtuur@me.com>
2018-07-30 13:46:19 +0200

    feat(packages): add queue package
    exports ChunkedQueue, NumericQueue and NumericDeque

---
+++ container/queue/src/queue.ts
@@ -0,0 +1,10 @@
+/**
+ * @stardazed/queue - specialized queue containers
+ * Part of Stardazed
+ * (c) 2015-Present by Arthur Langereis - @zenmumbler
+ * https://github.com/stardazed/stardazed
+ */
+
+export { ChunkedQueue } from "./chunked-queue";
+export { NumericQueue } from "./numeric-queue";
+export { NumericDeque } from "./numeric-deque";
