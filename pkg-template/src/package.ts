eefe7c3f8da1da62712ce5703fa256eba1b858cd
Arthur Langereis <puurtuur@me.com>
2018-08-11 17:25:49 +0200

    ref: put variable templates in the package template
    for scriptable creation of new packages

--- pkg-template/src/package.ts
+++ pkg-template/src/package.ts
@@ -1,5 +1,5 @@
 /**
- * @stardazed/package - description
+ * @stardazed/{{package}} - {{Description}}
  * Part of Stardazed
  * (c) 2015-Present by Arthur Langereis - @zenmumbler
  * https://github.com/stardazed/stardazed
