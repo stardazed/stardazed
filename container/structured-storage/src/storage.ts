1948d915958c7f719164fd4b01740ff20fb54a97
Arthur Langereis <puurtuur@me.com>
2018-08-23 13:33:37 +0200

    ref(structured-storage): factor out alignment calc

--- container/structured-storage/src/storage.ts
+++ container/structured-storage/src/storage.ts
@@ -5,43 +5,32 @@
  * https://github.com/stardazed/stardazed
  */
 
-import { alignUp } from "@stardazed/math";
+import { SizingAlignmentFlags, calcStorageSizeWithAlignmentFlags } from "./storage-alignment";
 
 export interface StructStorage {
 	readonly itemSizeBytes: number;
+	readonly storageAlignment: SizingAlignmentFlags;
 	capacity: number;
 	owned: boolean;
 	data: Uint8Array;
 }
 
-const CAPACITY_UNIT = 32;
-const WEBASSEMBLY_PAGE_SIZE = 64 * 1024;
-
-export function alignCapacityUp(capacity: number) {
-	return alignUp(capacity, CAPACITY_UNIT);
-}
-
-export function alignSizeBytesUpToWASMPage(sizeBytes: number) {
-	return alignUp(sizeBytes, WEBASSEMBLY_PAGE_SIZE);
-}
-
 /**
  * @param itemSizeBytes Size in bytes of each individual element in the storge
  * @param minCapacity The number of elements that _at least_ need to fit in the storage.
- * @param wasmCompatible Should the buffer be sized to be a multiple of a WASM page size
+ * @param storageAlignment Flags indicating how the storage should be aligned
  * @expects isPositiveNonZeroInteger(itemSizeBytes)
  * @expects isPositiveNonZeroInteger(minCapacity)
  */
-export function allocStorage(itemSizeBytes: number, minCapacity: number, wasmCompatible = false): StructStorage {
-	const capacity = alignCapacityUp(minCapacity);
-	const dataSizeBytes = itemSizeBytes * capacity;
-	const pageAlignedSizeBytes = wasmCompatible ? alignSizeBytesUpToWASMPage(dataSizeBytes) : dataSizeBytes;
+export function allocStorage(itemSizeBytes: number, minCapacity: number, storageAlignment: SizingAlignmentFlags): StructStorage {
+	const { capacity, sizeBytes } = calcStorageSizeWithAlignmentFlags(itemSizeBytes, minCapacity, storageAlignment);
 
 	return {
 		itemSizeBytes,
+		storageAlignment,
 		capacity,
 		owned: true,
-		data: new Uint8Array(pageAlignedSizeBytes)
+		data: new Uint8Array(sizeBytes)
 	};
 }
 
@@ -52,16 +41,16 @@ export function allocStorage(itemSizeBytes: number, minCapacity: number, wasmCom
  * @expects isPositiveNonZeroInteger(itemSizeBytes)
  * @expects isPositiveNonZeroInteger(minCapacity)
  */
-export function createStorageInBuffer(itemSizeBytes: number, minCapacity: number, buffer: Uint8Array): StructStorage {
-	const capacity = alignCapacityUp(minCapacity);
-	const dataSizeBytes = itemSizeBytes * capacity;
+export function createStorageInBuffer(itemSizeBytes: number, minCapacity: number, storageAlignment: SizingAlignmentFlags, buffer: Uint8Array): StructStorage {
+	const { capacity, sizeBytes } = calcStorageSizeWithAlignmentFlags(itemSizeBytes, minCapacity, storageAlignment);
 
-	if (dataSizeBytes > buffer.byteLength) {
+	if (sizeBytes > buffer.byteLength) {
 		throw new RangeError("Provided storage is too small");
 	}
 
 	return {
 		itemSizeBytes,
+		storageAlignment,
 		capacity,
 		owned: false,
 		data: buffer
