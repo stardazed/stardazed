f7fbd1720b80e5fa5ce9608ca45e2c39d7d024ef
Arthur Langereis <puurtuur@me.com>
2019-12-03 16:35:41 +0100

    fix: ref global types in publlished dts files

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
+import fs from "fs";
 import dts from "rollup-plugin-dts";
 
 const external = id => id.startsWith("stardazed/");
-const paths = id => id.startsWith("stardazed/") && `${id.replace("stardazed", "..")}`,
+const paths = id => id.startsWith("stardazed/") && `${id.replace("stardazed", "..")}`;
 
 function indexes(module) {
 	return [
@@ -21,11 +22,14 @@ function indexes(module) {
 				file: `dist/${module}/index.d.ts`,
 				format: "esm",
 				paths,
+				banner: `/// <reference path="../global-types.d.ts" />`
 			}],
 			plugins: [dts()],
 			external
 		}
 	];
 }
+
+fs.copyFileSync("src/global-types.d.ts", "dist/global-types.d.ts");
 
 export default ["core", "container", "vector"].map(indexes).flat();
