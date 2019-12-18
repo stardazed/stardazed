26db8eeedccfecdc70b55822236825b7de94640f
Arthur Langereis <puurtuur@me.com>
2019-12-17 20:36:43 -0700

    ref: remove object.freeze from numerics
    keeping line with the "types are law" way of creating SD, also Rollup doesn't want to shake frozen objects

--- src/core/numeric.ts
+++ src/core/numeric.ts
@@ -32,7 +32,7 @@ export interface NumericType {
 /**
  * Traits of unsigned 8-bit integer numbers.
  */
-export const UInt8: NumericType = Object.freeze({
+export const UInt8: NumericType = {
 	min: 0,
 	max: 255,
 	minSafeInt: 0,
@@ -41,12 +41,12 @@ export const UInt8: NumericType = Object.freeze({
 	integer: true,
 	byteSize: 1,
 	arrayType: Uint8Array
-});
+};
 
 /**
  * Traits of signed 8-bit integer numbers.
  */
-export const SInt8: NumericType = Object.freeze({
+export const SInt8: NumericType = {
 	min: -128,
 	max: 127,
 	minSafeInt: -128,
@@ -55,12 +55,12 @@ export const SInt8: NumericType = Object.freeze({
 	integer: true,
 	byteSize: 1,
 	arrayType: Int8Array
-});
+};
 
 /**
  * Traits of unsigned 16-bit integer numbers.
  */
-export const UInt16: NumericType = Object.freeze({
+export const UInt16: NumericType = {
 	min: 0,
 	max: 65535,
 	minSafeInt: 0,
@@ -69,12 +69,12 @@ export const UInt16: NumericType = Object.freeze({
 	integer: true,
 	byteSize: 2,
 	arrayType: Uint16Array
-});
+};
 
 /**
  * Traits of signed 16-bit integer numbers.
  */
-export const SInt16: NumericType = Object.freeze({
+export const SInt16: NumericType = {
 	min: -32768,
 	max: 32767,
 	minSafeInt: -32768,
@@ -83,12 +83,12 @@ export const SInt16: NumericType = Object.freeze({
 	integer: true,
 	byteSize: 2,
 	arrayType: Int16Array
-});
+};
 
 /**
  * Traits of unsigned 32-bit integer numbers.
  */
-export const UInt32: NumericType = Object.freeze({
+export const UInt32: NumericType = {
 	min: 0,
 	max: 4294967295,
 	minSafeInt: 0,
@@ -97,12 +97,12 @@ export const UInt32: NumericType = Object.freeze({
 	integer: true,
 	byteSize: 4,
 	arrayType: Uint32Array
-});
+};
 
 /**
  * Traits of signed 32-bit integer numbers.
  */
-export const SInt32: NumericType = Object.freeze({
+export const SInt32: NumericType = {
 	min: -2147483648,
 	max: 2147483647,
 	minSafeInt: -2147483648,
@@ -111,12 +111,12 @@ export const SInt32: NumericType = Object.freeze({
 	integer: true,
 	byteSize: 4,
 	arrayType: Int32Array
-});
+};
 
 /**
  * Traits of 32-bit floating point numbers.
  */
-export const Float: NumericType = Object.freeze({
+export const Float: NumericType = {
 	min: -340282346638528859811704183484516925440.0,
 	max: 340282346638528859811704183484516925440.0,
 	minSafeInt: -16777216, // -2^24
@@ -125,12 +125,12 @@ export const Float: NumericType = Object.freeze({
 	integer: false,
 	byteSize: 4,
 	arrayType: Float32Array
-});
+};
 
 /**
  * Traits of 64-bit floating point numbers.
  */
-export const Double: NumericType = Object.freeze({
+export const Double: NumericType = {
 	min: -179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.0,
 	max: 179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.0,
 	minSafeInt: -9007199254740991, // -2^53 + 1
@@ -139,4 +139,4 @@ export const Double: NumericType = Object.freeze({
 	integer: false,
 	byteSize: 8,
 	arrayType: Float64Array
-});
+};
