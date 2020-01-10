6eeb38399a7aa8225269c042f5e38ff21ec3b9f1
Arthur Langereis <puurtuur@me.com>
2020-01-10 20:36:24 +0100

    ref(geom): make Geometry work with new VBs

--- src/geometry/geometry.ts
+++ src/geometry/geometry.ts
@@ -6,10 +6,8 @@ https://github.com/stardazed/stardazed
 */
 
 import { alignUp } from "stardazed/core";
-import { bytesRequiredForIndexCount, createIndexBufferWithStorage, IndexBuffer, isIndexBuffer, minimumIndexElementTypeForVertexCount, PrimitiveType } from "./index-buffer";
-import { VertexAttribute, VertexBuffer, VertexBufferDesc } from "./vertex-buffer";
-
-export type GeometryLayout = ReadonlyArray<VertexBufferLayout>;
+import { bytesRequiredForIndexCount, createIndexBufferWithStorage, IndexBuffer, minimumIndexElementTypeForVertexCount, PrimitiveType } from "./index-buffer";
+import { VertexBuffer, VertexBufferDescriptor } from "./vertex-buffer";
 
 export interface PrimitiveGroup {
 	/** How the elements in this group should be interpreted to form primitives */
@@ -28,84 +26,62 @@ export interface Geometry {
 	subMeshes: PrimitiveGroup[];
 }
 
-export const isGeometry = (geom: any): geom is Geometry =>
-	geom && (typeof geom === "object") &&
-	Array.isArray(geom.layout) &&
-	Array.isArray(geom.vertexBuffers) &&
-	(geom.indexBuffer === undefined || isIndexBuffer(geom.indexBuffer)) &&
-	Array.isArray(geom.subMeshes);
-
-export function makeStandardGeometryLayout(attrLists: VertexAttribute[] | VertexAttribute[][]): GeometryLayout {
-	const layouts: VertexBufferLayout[] = [];
-
-	if (attrLists.length > 0) {
-		if (isVertexAttribute(attrLists[0])) {
-			layouts.push(makeVertexBufferLayout(attrLists as VertexAttribute[]));
-		}
-		else {
-			for (const list of attrLists) {
-				layouts.push(makeVertexBufferLayout(list as VertexAttribute[]));
-			}
-		}
-	}
-
-	return layouts;
-}
-
 export const enum BufferAlignment {
 	SubBuffer = 8
 }
 
-export interface GeometryAllocOptions {
-	vertexDescs: VertexBufferDesc[];
-	vertexCount: number;
+export interface GeometryAllocDescriptor {
+	vertexDescs: VertexBufferDescriptor[];
 	indexCount: number;
 }
 
-export function bytesNeededForGeometry(options: GeometryAllocOptions) {
+export function bytesNeededForGeometry(desc: GeometryAllocDescriptor) {
 	let totalBytes = 0;
-	for (const layout of options.layout) {
-		totalBytes += layout.sizeBytesForCount(options.vertexCount);
+	for (const vbDesc of desc.vertexDescs) {
+		totalBytes += VertexBuffer.sizeBytesRequired(vbDesc.attrs, vbDesc.valueCount);
 		totalBytes = alignUp(totalBytes, BufferAlignment.SubBuffer);
 	}
-	if (options.indexCount > 0) {
-		const elementType = minimumIndexElementTypeForVertexCount(options.vertexCount);
-		totalBytes += bytesRequiredForIndexCount(elementType, options.indexCount);
+	if (desc.indexCount > 0) {
+		const elementType = minimumIndexElementTypeForVertexCount(desc.vertexDescs[0].valueCount);
+		totalBytes += bytesRequiredForIndexCount(elementType, desc.indexCount);
 		totalBytes = alignUp(totalBytes, BufferAlignment.SubBuffer);
 	}
 	return totalBytes;
 }
 
 /**
- * @expects bytesNeededForGeometry(options) > 0
+ * @expects bytesNeededForGeometry(desc) > 0
  */
-export function allocateGeometry(options: GeometryAllocOptions): Geometry {
-	const totalBytes = bytesNeededForGeometry(options);
+export function allocateGeometry(desc: GeometryAllocDescriptor): Geometry {
+	const totalBytes = bytesNeededForGeometry(desc);
 
 	const geom: Geometry = {
-		layout: options.layout,
 		vertexBuffers: [],
 		subMeshes: [],
 	};
 	const storage = new ArrayBuffer(totalBytes);
 
 	let byteOffset = 0;
-	for (const layout of options.layout) {
-		const subSize = layout.sizeBytesForCount(options.vertexCount);
+	let vertexCount = 0;
+	for (const vbDesc of desc.vertexDescs) {
+		const subSize = VertexBuffer.sizeBytesRequired(vbDesc.attrs, vbDesc.valueCount);
 		const subStorage = new Uint8Array(storage, byteOffset, subSize);
-		const vb = new VertexBuffer(layout, options.vertexCount, subStorage);
+		const vb = new VertexBuffer(vbDesc, subStorage);
 		geom.vertexBuffers.push(vb);
 
 		byteOffset += subSize;
 		byteOffset = alignUp(byteOffset, BufferAlignment.SubBuffer);
+		if (vertexCount === 0) {
+			// first VB is assumed to have the non-instanced full vertex data
+			vertexCount = vbDesc.valueCount;
+		}
 	}
-	if (options.indexCount) {
-		const elementType = minimumIndexElementTypeForVertexCount(options.vertexCount);
-		const indexSize = bytesRequiredForIndexCount(elementType, options.indexCount);
-		const subSize = bytesRequiredForIndexCount(elementType, options.indexCount);
-		const subStorage = new Uint8Array(storage, byteOffset, subSize);
+	if (desc.indexCount) {
+		const elementType = minimumIndexElementTypeForVertexCount(vertexCount);
+		const indexSize = bytesRequiredForIndexCount(elementType, desc.indexCount);
+		const subStorage = new Uint8Array(storage, byteOffset, indexSize);
 
-		geom.indexBuffer = createIndexBufferWithStorage(elementType, options.indexCount, subStorage);
+		geom.indexBuffer = createIndexBufferWithStorage(elementType, desc.indexCount, subStorage);
 		byteOffset += indexSize;
 		byteOffset = alignUp(byteOffset, BufferAlignment.SubBuffer);
 	}
