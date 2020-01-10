e35efef701d4b27f8adf3d53b68fbc3436c09709
Arthur Langereis <puurtuur@me.com>
2020-01-10 16:42:32 +0100

    ref(geometry): reimplement VertexBuffer as AOS
    - VertexField is removed
    - constructor takes descriptor
    - added buffer-wide instancing divisor property

--- src/geometry/vertex-buffer.ts
+++ src/geometry/vertex-buffer.ts
@@ -6,208 +6,6 @@ https://github.com/stardazed/stardazed
 */
 
 import { PositionedStructField, StructField, ArrayOfStructs } from "stardazed/container";
-import { NumType, NumericType } from "stardazed/core";
-
-const { Float, SInt16, SInt32, SInt8, UInt16, UInt32, UInt8 } = NumType;
-
-/**
- * A single field in a vertex buffer with three properties:
- * element type, count and normalization
- */
-export const enum VertexField {
-	Undefined,
-
-	// integer
-	UInt8,
-	UInt8x2,
-	UInt8x3,
-	UInt8x4,
-
-	SInt8,
-	SInt8x2,
-	SInt8x3,
-	SInt8x4,
-
-	UInt16,
-	UInt16x2,
-	UInt16x3,
-	UInt16x4,
-
-	SInt16,
-	SInt16x2,
-	SInt16x3,
-	SInt16x4,
-
-	UInt32,
-	UInt32x2,
-	UInt32x3,
-	UInt32x4,
-
-	SInt32,
-	SInt32x2,
-	SInt32x3,
-	SInt32x4,
-
-	// floating point
-	Float,
-	Floatx2,
-	Floatx3,
-	Floatx4,
-
-	// normalized
-	Norm_UInt8 = 0x81,	// normalized fields have high bit set
-	Norm_UInt8x2,
-	Norm_UInt8x3,
-	Norm_UInt8x4,
-
-	Norm_SInt8,
-	Norm_SInt8x2,
-	Norm_SInt8x3,
-	Norm_SInt8x4,
-
-	Norm_UInt16,
-	Norm_UInt16x2,
-	Norm_UInt16x3,
-	Norm_UInt16x4,
-
-	Norm_SInt16,
-	Norm_SInt16x2,
-	Norm_SInt16x3,
-	Norm_SInt16x4
-}
-
-export function isValidVertexField(vf: any): vf is VertexField {
-	return (typeof vf === "number") && (
-		(vf >= VertexField.UInt8 && vf <= VertexField.Floatx4) ||
-		(vf >= VertexField.Norm_UInt8 && vf <= VertexField.Norm_SInt16x4)
-	);
-}
-
-/**
- * @expects isValidVertexField(vf)
- */
-export function vertexFieldElementCount(vf: VertexField) {
-	switch (vf) {
-		case VertexField.UInt32:
-		case VertexField.SInt32:
-		case VertexField.Float:
-			return 1;
-
-		case VertexField.UInt8x2:
-		case VertexField.Norm_UInt8x2:
-		case VertexField.SInt8x2:
-		case VertexField.Norm_SInt8x2:
-		case VertexField.UInt16x2:
-		case VertexField.Norm_UInt16x2:
-		case VertexField.SInt16x2:
-		case VertexField.Norm_SInt16x2:
-		case VertexField.UInt32x2:
-		case VertexField.SInt32x2:
-		case VertexField.Floatx2:
-			return 2;
-
-		case VertexField.UInt8x3:
-		case VertexField.Norm_UInt8x3:
-		case VertexField.SInt8x3:
-		case VertexField.Norm_SInt8x3:
-		case VertexField.UInt16x3:
-		case VertexField.Norm_UInt16x3:
-		case VertexField.SInt16x3:
-		case VertexField.Norm_SInt16x3:
-		case VertexField.UInt32x3:
-		case VertexField.SInt32x3:
-		case VertexField.Floatx3:
-			return 3;
-
-		case VertexField.UInt8x4:
-		case VertexField.Norm_UInt8x4:
-		case VertexField.SInt8x4:
-		case VertexField.Norm_SInt8x4:
-		case VertexField.UInt16x4:
-		case VertexField.Norm_UInt16x4:
-		case VertexField.SInt16x4:
-		case VertexField.Norm_SInt16x4:
-		case VertexField.UInt32x4:
-		case VertexField.SInt32x4:
-		case VertexField.Floatx4:
-			return 4;
-
-		case VertexField.Undefined:
-		default:
-			return 0;
-	}
-}
-
-export function vertexFieldNumericType(vf: VertexField): NumericType | undefined {
-	switch (vf) {
-		case VertexField.Float:
-		case VertexField.Floatx2:
-		case VertexField.Floatx3:
-		case VertexField.Floatx4:
-			return Float;
-
-		case VertexField.UInt32:
-		case VertexField.UInt32x2:
-		case VertexField.UInt32x3:
-		case VertexField.UInt32x4:
-			return UInt32;
-
-		case VertexField.SInt32:
-		case VertexField.SInt32x2:
-		case VertexField.SInt32x3:
-		case VertexField.SInt32x4:
-			return SInt32;
-
-		case VertexField.UInt16x2:
-		case VertexField.Norm_UInt16x2:
-		case VertexField.UInt16x3:
-		case VertexField.Norm_UInt16x3:
-		case VertexField.UInt16x4:
-		case VertexField.Norm_UInt16x4:
-			return UInt16;
-
-		case VertexField.SInt16x2:
-		case VertexField.Norm_SInt16x2:
-		case VertexField.SInt16x3:
-		case VertexField.Norm_SInt16x3:
-		case VertexField.SInt16x4:
-		case VertexField.Norm_SInt16x4:
-			return SInt16;
-
-		case VertexField.UInt8x2:
-		case VertexField.Norm_UInt8x2:
-		case VertexField.UInt8x3:
-		case VertexField.Norm_UInt8x3:
-		case VertexField.UInt8x4:
-		case VertexField.Norm_UInt8x4:
-			return UInt8;
-
-		case VertexField.SInt8x2:
-		case VertexField.Norm_SInt8x2:
-		case VertexField.SInt8x3:
-		case VertexField.Norm_SInt8x3:
-		case VertexField.SInt8x4:
-		case VertexField.Norm_SInt8x4:
-			return SInt8;
-
-		case VertexField.Undefined:
-		default:
-			return undefined;
-	}
-}
-
-export function vertexFieldElementSizeBytes(vf: VertexField) {
-	const nt = vertexFieldNumericType(vf);
-	return nt ? nt.byteSize : 0;
-}
-
-export function vertexFieldSizeBytes(vf: VertexField) {
-	return vertexFieldElementSizeBytes(vf) * vertexFieldElementCount(vf);
-}
-
-export function vertexFieldIsNormalized(vf: VertexField) {
-	return (vf & 0x80) !== 0;
-}
 
 /**
  * The role of a vertex attribute indicates usage purpose
@@ -235,73 +33,45 @@ export const enum VertexAttributeRole {
 	JointIndexes
 }
 
-/**
- * A VertexAttribute is an optionally instanced Field with a Role inside a VertexBuffer
- */
-export interface VertexAttribute {
-	/** The data type and element count of this attribute */
-	field: VertexField;
+interface VertexFieldProps {
+	/** Only for integer fields, is the value treated as a normalised fraction? */
+	normalized: boolean;
 	/** The role of this attribute inside the buffer */
 	role: VertexAttributeRole;
-	/** Instancing value divisor, set to 0 for non-instanced behaviour */
-	divisor: number;
-}
+};
 
-export function isVertexAttribute(va: any): va is VertexAttribute {
-	return typeof va === "object" && va !== null &&
-		typeof va.field === "number" && typeof va.role === "number";
-}
+/**
+ * A VertexAttribute is a field inside a VertexBuffer
+ */
+export type VertexAttribute = StructField<VertexFieldProps>;
 
-export type PositionedAttribute = PositionedStructField<VertexAttribute>;
+/**
+ * A PositionedAttribute is a field inside a VertexBuffer with byte-level layout information
+ */
+export type PositionedAttribute = PositionedStructField<VertexFieldProps>;
 
-
-// ---- vertex layout builder
-
-export type VertexBufferLayout = StructLayout<VertexAttribute>;
-
-export function makeVertexBufferLayout(attrList: VertexAttribute[]): VertexBufferLayout {
-	const fields = attrList.map(attr => {
-		const sf: StructField<VertexAttribute> = {
-			type: vertexFieldNumericType(attr.field)!,
-			width: vertexFieldElementCount(attr.field),
-			...attr
-		};
-		return sf;
-	});
-	return new StructLayout(fields, FieldTopology.AlignedStructs);
+/**
+ * Properties used to create a VertexBuffer
+ */
+export interface VertexBufferDesc {
+	/** The vertex fields to be included in the buffer */
+	attrs: VertexAttribute[];
+	/** The number of values of each attribute required (usually the vertex count) */
+	valueCount: number;
+	/** (optional) The instancing divisor that will apply to ALL attributes in this buffer */
+	divisor?: number;
+	/** (optional) Manually provide a place to store the vertex data, usually for compound buffers */
+	storage?: Uint8Array;
 }
 
 /**
- * A VertexBuffer is a simple structure that holds storage and metatdata
- * for a specified count of vertexes with a stride.
+ * A VertexBuffer is the client-side representation of vertex data and meta-data.
  */
-export class VertexBuffer {
-	private readonly backing_: StructuredArray<VertexAttribute>;
+export class VertexBuffer extends ArrayOfStructs<VertexFieldProps> {
+	readonly divisor: number;
 
-	/**
-	 * @expects isPositiveNonZeroInteger(vertexCount)
-	 */
-	constructor(layout: StructLayout<VertexAttribute>, vertexCount: number, storage?: Uint8Array) {
-		this.backing_ = new StructuredArray<VertexAttribute>({
-			layout,
-			capacity: vertexCount,
-			bufferView: storage
-		});
-	}
-
-	get layout() {
-		return this.backing_.layout;
-	}
-	get vertexCount() {
-		return this.backing_.storage.capacity;
-	}
-	get stride() {
-		return this.backing_.layout.totalSizeBytes;
-	}
-	get storage() {
-		return this.backing_.storage.data;
-	}
-	get sizeBytes() {
-		return this.vertexCount * this.stride;
+	constructor(desc: VertexBufferDesc) {
+		super(desc.attrs, desc.valueCount, desc.storage);
+		this.divisor = desc.divisor ?? 0;
 	}
 }
