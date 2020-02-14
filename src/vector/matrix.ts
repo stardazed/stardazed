dafa46011e0e2a770b6f6ee103526eab7ede8fa9
Arthur Langereis <puurtuur@me.com>
2020-02-14 19:09:28 +0100

    feat(vector): Matrix transformPoint method

--- src/vector/matrix.ts
+++ src/vector/matrix.ts
@@ -202,6 +202,18 @@ export class Matrix {
 		return m;
 	}
 
+	transformPoint(p: Vector3) {
+		const { x, y, z } = p;
+		const m = this.data_;
+		const w = (m[3] * x + m[7] * y + m[11] * z + m[15]) || 1.0;
+
+		return new Vector3(
+			(m[0] * x + m[4] * y + m[8] * z + m[12]) / w,
+			(m[1] * x + m[5] * y + m[9] * z + m[13]) / w,
+			(m[2] * x + m[6] * y + m[10] * z + m[14]) / w
+		);
+	}
+
 	// static operations
 
 	// static constructors
