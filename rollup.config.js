43e072ecedebc6ea05feb7d8eca2be2e5f8db4dc
zenmumbler <zen@stardazed.club>
2020-10-06 16:51:57 +0200

    feat(input): input module scaffolding

--- rollup.config.js
+++ rollup.config.js
@@ -42,5 +42,6 @@ export default [
 	"geometry",
 	"entity",
 	"render",
-	"asset"
+	"asset",
+	"input"
 ].flatMap(module);
---
+++ src/input/index.ts
@@ -0,0 +1,8 @@
+/*
+input - input device access and helpers
+Part of Stardazed
+(c) 2015-Present by @zenmumbler
+https://github.com/stardazed/stardazed
+*/
+
+export const temp = true;
