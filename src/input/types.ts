ec3d867ff834ef567403bd9e1ee5974e0fcb50cf
zenmumbler <zen@stardazed.club>
2020-10-11 20:09:48 +0200

    feat(input): common types

---
+++ src/input/types.ts
@@ -0,0 +1,12 @@
+/*
+input/types - common input types and helpers
+Part of Stardazed
+(c) 2015-Present by @zenmumbler
+https://github.com/stardazed/stardazed
+*/
+
+export interface ButtonState {
+	down: boolean;
+	transitioned: boolean;
+	value: number;
+}
