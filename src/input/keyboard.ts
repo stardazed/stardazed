dbbb29a3929f7ec23ecc5b6ba3858fa870e10b41
zenmumbler <zen@stardazed.club>
2020-10-11 20:11:12 +0200

    ref(input): simplify keyboard logic
    remove half transitions, relpace with dual down state tracking

--- src/input/keyboard.ts
+++ src/input/keyboard.ts
@@ -1,10 +1,19 @@
-/*
-input - keyboard input handling
+ /*
+input/keyboard - keyboard input handling
 Part of Stardazed
 (c) 2015-Present by @zenmumbler
 https://github.com/stardazed/stardazed
 */
 
+import { StructOfArrays } from "stardazed/container";
+
+/**
+ * An abstract key code that represents a key on a generic US QWERTY keyboard.
+ * On keyboards using different key layouts, these codes represent the key
+ * that is physically in the same location as the key on a US QWERTY layout.
+ * Because of this, you can always e.g. specify WASD as movement keys and it
+ * will map to ZQSD on an AZERTY layout keyboard.
+ */
 export const enum Key {
 	None,
 	
@@ -53,45 +62,18 @@ export const enum Key {
 };
 
 const EventCodeToKey: Record<string, Key> = {
-	KeyA: Key.A,
-	KeyB: Key.B,
-	KeyC: Key.C,
-	KeyD: Key.D,
-	KeyE: Key.E,
-	KeyF: Key.F,
-	KeyG: Key.G,
-	KeyH: Key.H,
-	KeyI: Key.I,
-	KeyJ: Key.J,
-	KeyK: Key.K,
-	KeyL: Key.L,
-	KeyM: Key.M,
-	KeyN: Key.N,
-	KeyO: Key.O,
-	KeyP: Key.P,
-	KeyQ: Key.Q,
-	KeyR: Key.R,
-	KeyS: Key.S,
-	KeyT: Key.T,
-	KeyU: Key.U,
-	KeyV: Key.V,
-	KeyW: Key.W,
-	KeyX: Key.X,
-	KeyY: Key.Y,
-	KeyZ: Key.Z,
+	KeyA: Key.A, KeyB: Key.B, KeyC: Key.C, KeyD: Key.D,
+	KeyE: Key.E, KeyF: Key.F, KeyG: Key.G, KeyH: Key.H,
+	KeyI: Key.I, KeyJ: Key.J, KeyK: Key.K, KeyL: Key.L,
+	KeyM: Key.M, KeyN: Key.N, KeyO: Key.O, KeyP: Key.P,
+	KeyQ: Key.Q, KeyR: Key.R, KeyS: Key.S, KeyT: Key.T,
+	KeyU: Key.U, KeyV: Key.V, KeyW: Key.W, KeyX: Key.X,
+	KeyY: Key.Y, KeyZ: Key.Z,
 
-	Digit0: Key.Digit0,
-	Digit1: Key.Digit1,
-	Digit2: Key.Digit2,
-	Digit3: Key.Digit3,
-	Digit4: Key.Digit4,
-	Digit5: Key.Digit5,
-	Digit6: Key.Digit6,
-	Digit7: Key.Digit7,
-	Digit8: Key.Digit8,
-	Digit9: Key.Digit9,
-	Minus: Key.Minus,
-	Equal: Key.Equals,
+	Digit0: Key.Digit0, Digit1: Key.Digit1, Digit2: Key.Digit2,
+	Digit3: Key.Digit3, Digit4: Key.Digit4, Digit5: Key.Digit5,
+	Digit6: Key.Digit6, Digit7: Key.Digit7, Digit8: Key.Digit8,
+	Digit9: Key.Digit9, Minus: Key.Minus, Equal: Key.Equals,
 
 	Backspace: Key.Backspace,
 	Tab: Key.Tab,
@@ -121,16 +103,11 @@ const EventCodeToKey: Record<string, Key> = {
 	ArrowLeft: Key.ArrowLeft,
 	ArrowRight: Key.ArrowRight,
 
-	OSLeft: Key.LeftMeta,
-	OSRight: Key.RightMeta,
-	MetaLeft: Key.LeftMeta,
-	MetaRight: Key.RightMeta,
-	ShiftLeft: Key.LeftShift,
-	ShiftRight: Key.RightShift,
-	AltLeft: Key.LeftAlt,
-	AltRight: Key.RightAlt,
-	ControlLeft: Key.LeftControl,
-	ControlRight: Key.RightControl,
+	OSLeft: Key.LeftMeta, OSRight: Key.RightMeta,
+	MetaLeft: Key.LeftMeta, MetaRight: Key.RightMeta,
+	ShiftLeft: Key.LeftShift, ShiftRight: Key.RightShift,
+	AltLeft: Key.LeftAlt, AltRight: Key.RightAlt,
+	ControlLeft: Key.LeftControl, ControlRight: Key.RightControl,
 	CapsLock: Key.CapsLock,
 	ContextMenu: Key.Context,
 
@@ -144,15 +121,9 @@ const EventCodeToKey: Record<string, Key> = {
 	NumpadDecimal: Key.NumPadDecimal,
 	NumpadComma: Key.NumPadComma, // JIS
 	Numpad0: Key.NumPad0,
-	Numpad1: Key.NumPad1,
-	Numpad2: Key.NumPad2,
-	Numpad3: Key.NumPad3,
-	Numpad4: Key.NumPad4,
-	Numpad5: Key.NumPad5,
-	Numpad6: Key.NumPad6,
-	Numpad7: Key.NumPad7,
-	Numpad8: Key.NumPad8,
-	Numpad9: Key.NumPad9,
+	Numpad1: Key.NumPad1, Numpad2: Key.NumPad2, Numpad3: Key.NumPad3,
+	Numpad4: Key.NumPad4, Numpad5: Key.NumPad5, Numpad6: Key.NumPad6,
+	Numpad7: Key.NumPad7, Numpad8: Key.NumPad8, Numpad9: Key.NumPad9,
 
 	IntlYen: Key.JISYen,
 	IntlRo: Key.JISRo,
@@ -162,126 +133,97 @@ const EventCodeToKey: Record<string, Key> = {
 	Hanja: Key.Lang2,
 	KanaMode: Key.Lang2,
 
-	F1: Key.F1,
-	F2: Key.F2,
-	F3: Key.F3,
-	F4: Key.F4,
-	F5: Key.F5,
-	F6: Key.F6,
-	F7: Key.F7,
-	F8: Key.F8,
-	F9: Key.F9,
-	F10: Key.F10,
-	F11: Key.F11,
-	F12: Key.F12,
-	F13: Key.F13,
-	F14: Key.F14,
-	F15: Key.F15,
-	F16: Key.F16,
-	F17: Key.F17,
-	F18: Key.F18,
-	F19: Key.F19,
+	F1: Key.F1, F2: Key.F2, F3: Key.F3, F4: Key.F4,
+	F5: Key.F5, F6: Key.F6, F7: Key.F7, F8: Key.F8,
+	F9: Key.F9, F10: Key.F10, F11: Key.F11, F12: Key.F12,
+	F13: Key.F13, F14: Key.F14, F15: Key.F15, F16: Key.F16,
+	F17: Key.F17, F18: Key.F18, F19: Key.F19,
 
 	PrintScreen: Key.PrintScreen, // aka F13
 	ScrollLock: Key.ScrollLock, // aka F14
 	Pause: Key.Pause // aka F15
 };
 
+/**
+ * Maximum number of keys to track, must be able to represent
+ * all Key values and be a power of 2.
+ */
+const MAX_KEYS = 128;
 
-export interface Keyboard {
-	keyState(kc: Key): ButtonState;
-	down(kc: Key): boolean;
-	pressed(kc: Key): boolean;
-	released(kc: Key): boolean;
-	halfTransitions(kc: Key): number;
-
-	reset(): void;
-	resetPerFrameData(): void;
-}
-
-
-class KeyboardImpl implements Keyboard {
-	private keyData_: container.FixedMultiArray;
+export class Keyboard {
+	private keyData_: StructOfArrays;
 	private downBase_: Uint8Array;
-	private halfTransBase_: Uint8Array;
 	private lastEventBase_: Float64Array;
+	private frameToggle_: number;
 
 	constructor() {
-		const fields: container.MABField[] = [
-			{ type: UInt8, count: 1 },  // down
-			{ type: UInt8, count: 1 },  // halfTransitionCount
-			{ type: Double, count: 1 }, // lastEvent
-		];
-		this.keyData_ = new container.FixedMultiArray(128, fields);
-		this.downBase_ = this.keyData_.indexedFieldView(0) as Uint8Array;
-		this.halfTransBase_ = this.keyData_.indexedFieldView(1) as Uint8Array;
-		this.lastEventBase_ = this.keyData_.indexedFieldView(2) as Float64Array;
+		this.keyData_ = new StructOfArrays([
+			{ type: "uint8", width: 2 },  // down
+			{ type: "double", width: 1 }, // lastEvent
+		], MAX_KEYS);
+		this.downBase_ = this.keyData_.fieldArrayView(0) as Uint8Array;
+		this.lastEventBase_ = this.keyData_.fieldArrayView(2) as Float64Array;
+		this.frameToggle_ = 0;
 
 		// The extra check in the key handlers for the timeStamp was added
-		// after I encountered a rare, but frequently enough occuring bug
+		// after I encountered a rare, but frequently enough occurring bug
 		// where, when a key is pressed for a longer time so that repeat
 		// keydown events are fired, _very_ occasionally the last keydown
 		// would be fired with the same timeStamp as the keyup event but
 		// the event handler for that last down event was fired AFTER the
 		// keyup event handler, causing the key to appear to be "stuck".
-		window.addEventListener("keydown", evt => {
-			const lastEvent = this.lastEventBase_[evt.keyCode];
-			const wasDown = this.downBase_[evt.keyCode];
+		window.addEventListener("keydown", (evt) => {
+			const key = EventCodeToKey[evt.code] || Key.None;
+			const lastEvent = this.lastEventBase_[key];
 
 			if (lastEvent < evt.timeStamp) {
-				if (! wasDown) { // ignore key repeat events
-					this.downBase_[evt.keyCode] = 1;
-					++this.halfTransBase_[evt.keyCode];
-				}
-				this.lastEventBase_[evt.keyCode] = evt.timeStamp;
+				this.downBase_[this.frameToggle_ + key] = 1;
+				this.lastEventBase_[key] = evt.timeStamp;
 			}
 
-			if (! evt.metaKey) {
-				evt.preventDefault();
-			}
+			evt.preventDefault();
 		}, true);
 
-		window.addEventListener("keyup", evt => {
-			this.downBase_[evt.keyCode] = 0;
-			++this.halfTransBase_[evt.keyCode];
-			this.lastEventBase_[evt.keyCode] = evt.timeStamp;
+		window.addEventListener("keyup", (evt) => {
+			const key = EventCodeToKey[evt.code] || Key.None;
+			this.downBase_[this.frameToggle_ + key] = 0;
+			this.lastEventBase_[key] = evt.timeStamp;
 
 			evt.preventDefault();
 		}, true);
 	}
 
-	keyState(kc: Key): ButtonState {
-		return {
-			down: !!this.downBase_[kc],
-			halfTransitionCount: this.halfTransBase_[kc]
-		};
 	}
 
-	down(kc: Key): boolean {
-		return !!this.downBase_[kc];
+	/** Is the key currently not held down, regardless of previous state? */
+	up(key: Key): boolean {
+		return this.downBase_[this.frameToggle_ + key] === 0;
 	}
 
-	halfTransitions(kc: Key): number {
-		return this.halfTransBase_[kc];
+	/** Is the key currently held down, regardless of previous state? */
+	down(key: Key): boolean {
+		return this.downBase_[this.frameToggle_ + key] === 1;
 	}
 
-	pressed(kc: Key): boolean {
-		return this.downBase_[kc] ? (this.halfTransBase_[kc] > 0) : false;
+	/** Did this key transition from up to down? */
+	pressed(key: Key): boolean {
+		// cur down = 1 and old down = 0 is the only combination that yields 1 when subtracted
+		return this.downBase_[this.frameToggle_ + key] - this.downBase_[(this.frameToggle_ ^ MAX_KEYS) + key] === 1;
 	}
 
-	released(kc: Key): boolean {
-		return !this.downBase_[kc] ? (this.halfTransBase_[kc] > 0) : false;
+	/** Did this key transition from down to up? */
+	released(key: Key): boolean {
+		// reverse calculation from pressed
+		return this.downBase_[(this.frameToggle_ ^ MAX_KEYS) + key] - this.downBase_[this.frameToggle_ + key] === 1;
 	}
 
+	/** Reset key tracking, any keys still physically held down must be released before they can be tracked again. */
 	reset() {
-		this.keyData_.clear();
+		this.keyData_.data.fill(0);
 	}
 
-	resetPerFrameData() {
-		container.fill(this.halfTransBase_, 0, this.halfTransBase_.length);
+	/** Call this after processing all inputs from this device to start tracking changes in the current frame. */
+	nextFrame() {
+		this.frameToggle_ ^= MAX_KEYS;
 	}
 }
-
-
-// -- exported devices
-export const keyboard: Keyboard = new KeyboardImpl();
